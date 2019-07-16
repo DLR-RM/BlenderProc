3f3f19e3e14d0c085bad5e6aa9ba41b4965f1afc
Dominik Winkelbauer <dominik.winkelbauer@dlr.de>
2019-07-16 15:13:04 +0200

    Implements undoing a modules actions after it had run
    This is especially useful for modules like the NormalRenderer
    
    Related: #10

--- src/renderer/NormalRenderer.py
+++ src/renderer/NormalRenderer.py
@@ -6,7 +6,7 @@ from src.renderer.Renderer import Renderer
 class NormalRenderer(Renderer):
 
     def __init__(self, config):
-        Renderer.__init__(self, config)
+        Renderer.__init__(self, config, undo_after_run=True)
 
     def run(self):
         self._configure_renderer()
@@ -17,8 +17,8 @@ class NormalRenderer(Renderer):
             data_to.materials = data_from.materials
 
         # render normals
-        self.scene.cycles.samples = self.config.get_int("samples", 100)  # to smooth the result
-        self.scene.render.layers[0].cycles.use_denoising = False
+        bpy.context.scene.cycles.samples = self.config.get_int("samples", 100)  # to smooth the result
+        bpy.context.scene.render.layers[0].cycles.use_denoising = False
         new_mat = bpy.data.materials["Normal"]
         for obj in bpy.context.scene.objects:
             if len(obj.material_slots) > 0:
