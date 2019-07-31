236aecccb49d268c3fc7be5c6b652dbcedccfb37
Youssef Zidan <youssef.zidan@dlr.de>
2019-07-31 15:46:47 +0200

    use self.output_dir and adding some docs

--- src/renderer/RgbRenderer.py
+++ src/renderer/RgbRenderer.py
@@ -11,19 +11,20 @@ class RgbRenderer(Renderer):
         Renderer.__init__(self, config)
 
     def render_depth(self):
-    	output_dir = Utility.resolve_path(self.config.get_string("output_dir"))
     	bpy.context.scene.render.use_compositing = True
     	bpy.context.scene.use_nodes = True
     	bpy.data.scenes["Scene"].render.layers["RenderLayer"].use_pass_z = True
     	tree = bpy.context.scene.node_tree
     	links = tree.links
 
+    	# Create a render layer
     	rl = tree.nodes.new('CompositorNodeRLayers')      
 
     	output_file = tree.nodes.new("CompositorNodeOutputFile")
-    	output_file.base_path = os.path.join(output_dir, "depth")
+    	output_file.base_path = os.path.join(self.output_dir, "depth")
     	output_file.format.file_format = "OPEN_EXR"
 
+    	# Feed the Z output of the render layer to the input of the file IO layer
     	links.new(rl.outputs[2], output_file.inputs['Image'])
 
 
