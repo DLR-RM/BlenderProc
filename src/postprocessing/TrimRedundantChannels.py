a15b4dfd3a2c36341496108b1c18e3998c1f24c0
Youssef Zidan <youssef.zidan@dlr.de>
2019-09-18 13:33:29 +0200

    Added some documentation

--- src/postprocessing/TrimRedundantChannels.py
+++ src/postprocessing/TrimRedundantChannels.py
@@ -8,7 +8,12 @@ class TrimRedundantChannels(Module):
         Module.__init__(self, config)
 
     def run(self, data):
+        """ Removes redundant channels, where the input has more than one channels that share exactly the same value
+        
+        :param data: The image data.
+        :return: The trimmed image data.
+        """
 
-        data = data[:,:,0]
+        data = data[:,:,0] # All channles have the same value, so just extract any single channel
 
         return data
