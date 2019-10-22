cfc349ddd8ede050515b59923aa19d10bc7b5e45
Maximilian Denninger <maximilian.denninger@dlr.de>
2019-10-22 11:56:14 +0200

    Change plyloader to load replica values as well

--- src/loader/PlyLoader.py
+++ src/loader/PlyLoader.py
@@ -13,22 +13,14 @@ class PlyLoader(Module):
         """Just imports the configured .ply file straight into blender
 
         """
-        bpy.ops.import_mesh.ply(filepath=self.config.get_string("path"))
-        rotation_file_path = self.config.get_string('rotation_file_path', '')
-        if len(rotation_file_path) > 0:
-            if os.path.exists(rotation_file_path):
-                rot_value = None
-                with open(rotation_file_path, 'r') as data:
-                    try:
-                        rot_value = float(data.read().strip())
-                    except ValueError:
-                        print("Couldn't read rotation value in {}".format(rotation_file_path))
-                if rot_value is not None:
-                    bpy.ops.transform.rotate(value=radians(rot_value), orient_axis='Z')
-
-
+        if not self.config.get_bool('is_replica_object', False):
+            bpy.ops.import_mesh.ply(filepath=self.config.get_string("path"))
+        else:
+            file_path = os.path.join(self.config.get_string('data_path'), self.config.get_string('data_set_name'), 'mesh.ply')
+            if os.path.exists(file_path):
+                bpy.ops.import_mesh.ply(filepath=file_path)
             else:
-                print("File does not exist {}:".format(rotation_file_path))
+                raise Exception("The filepath is not known: {}".format(file_path))
         if self.config.get_bool('use_ambient_occlusion', False):
             bpy.context.scene.world.light_settings.use_ambient_occlusion = True  # turn AO on
             bpy.context.scene.world.light_settings.ao_factor = 0.9  # set it to 0.5
