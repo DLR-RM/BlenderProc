292910751f92d24dab743f0f013bc8e6b1bec4f8
Maximilian Denninger <maximilian.denninger@dlr.de>
2019-10-24 17:33:03 +0200

    changed param name from K to cube_length

--- src/utility/ColorPicker.py
+++ src/utility/ColorPicker.py
@@ -8,21 +8,21 @@ import colorsys
 def get_colors(num):
     """
     Description: This function generates N equidistant rgb colors and returns num of them.
-    Basically it splits a cube of shape 256 x 256 x 256 in to N smaller cubes. Where, N = K^3
-    and K is the smallest integer for which N >= num. 
+    Basically it splits a cube of shape 256 x 256 x 256 in to N smaller cubes. Where, N = cube_length^3
+    and cube_length is the smallest integer for which N >= num. 
     num : integer
     colors: list of rgb colors, where each element is a list of size 3 for each channel of rgb
     """
-    K = 1
+    cube_length = 1
     colors = []
-    while K**3 < num: # find K bound of cubes to be made
-        K+=1
-    block_length = 256/K 
-    for r in range(K):
+    while cube_length**3 < num: # find cube_length bound of cubes to be made
+        cube_length+=1
+    block_length = 256/cube_length 
+    for r in range(cube_length):
         r_mid_point = int(round(block_length * r +  block_length/2))
-        for g in range(K):
+        for g in range(cube_length):
             g_mid_point = int(round(block_length * g +  block_length/2))
-            for b in range(K):
+            for b in range(cube_length):
                 b_mid_point = int(round(block_length * b +  block_length/2))
                 colors.append([r_mid_point,g_mid_point,b_mid_point])
     return colors[:-num]
