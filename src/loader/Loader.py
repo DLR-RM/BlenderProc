c855f3b842bede102e45dfc8deeeb82135eee31c
Dominik Winkelbauer <dominik.winkelbauer@dlr.de>
2019-10-30 16:55:35 +0100

    Adds setting the physics property in all loader modules

---
+++ src/loader/Loader.py
@@ -0,0 +1,22 @@
+from src.main.Module import Module
+
+
+class Loader(Module):
+    """
+    **Configuration**:
+
+    .. csv-table::
+       :header: "Parameter", "Description"
+
+       "enable_physics", "Determines the the physics property of all created objects. Set to ACTIVE if you want the objects to actively participate in the simulation and be influenced by e.q. gravity. Set to PASSIVE, if you want the object to be static and only act as an obstacle."
+    """
+    def __init__(self, config):
+        Module.__init__(self, config)
+
+    def _set_physics_property(self, objects):
+        """ Sets the physics custom property of all given objects according to the configuration.
+
+        :type objects: A list of objects which should retrieve the custom property.
+        """
+        for obj in objects:
+            obj["physics"] = self.config.get_string("physics", "PASSIVE")
