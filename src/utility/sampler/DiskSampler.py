6e1688f63b0c70d5cd672126e6b4bd1a9454777e
Dmitry Olefir <dmitry.olefir@dlr.de>
2019-11-06 14:15:16 +0100

    Added disk rotation

--- src/utility/sampler/DiskSampler.py
+++ src/utility/sampler/DiskSampler.py
@@ -12,7 +12,8 @@ class DiskSampler:
 
        "center", "A list of three values, describing x, y and z coordinates of the center of a 2-ball. "
        "radius", "The radius of the disk."
-       "mode", "Mode of sampling. CIRCLE - sampling from the 1-sphere, DISK - sampling from the 2-ball."
+       "mode", "Mode of sampling. CIRCLE - sampling from the 1-sphere, DISK - sampling from the 2-ball (default mode)."
+       "up_vector", "An up vector which specifies a local coordinate system of the disk."
     """
 
     @staticmethod
@@ -22,12 +23,13 @@ class DiskSampler:
         :return: A random point lying inside or on the surface of a solid sphere. Type: Mathutils vector
         """
         # Center of the disk.
-        center = np.array(config.get_list("center"))
+        center = config.get_vector3d("center")
         # Radius of the disk.
         radius = config.get_float("radius")
         # Mode of operation.
-        mode = config.get_string("mode")
-
+        mode = config.get_string("mode", "DISK")
+        # Up vector
+        up_vector = config.get_vector3d("up_vector", [0, 0, 1])
         # Sample directions
         direction = np.random.normal(size=2)
 
@@ -46,12 +48,14 @@ class DiskSampler:
         else:
             raise Exception("Unknown sampling mode: " + mode)
 
-        # Normilize
-        sampled_point = list(map(lambda x: magnitude*x/norm, direction))
-
-        # Add center (from 2d sampled_point to 3d location).
-        # Sampled point is sampled from a disk/circle that is parallel to Z axis.
-        location = mathutils.Vector(np.append(np.array(sampled_point), 0) + center)
+        # Normilize and transform to 3d
+        sampled_point = np.append(list(map(lambda x: magnitude*x/norm, direction)), 0)
+        
+        # Get Euler angles from up vector
+        euler_angles = up_vector.to_track_quat('Z', 'Y').to_euler()
+        # Get rotation
+        rot_mat = mathutils.Euler((euler_angles), 'XYZ')
+        # Get location on a rotated disk and add center
+        location = rot @ mathutils.Vector(sampled_point) + center
 
         return location
-
