21eca1b4ad5713d9080aa9565a58c34c51b36898
Mohamad Elbadrawy <mohamad.elbadrawy@dlr.de>
2020-01-27 13:02:45 +0100

    exctended obj loader

--- src/loader/ObjectLoader.py
+++ src/loader/ObjectLoader.py
@@ -15,26 +15,26 @@ class ObjectLoader(Loader):
     .. csv-table::
        :header: "Parameter", "Description"
 
-       "path", "The path to the 3D data file to load. Can be either path or paths not both."
-       "paths", "A list of paths of 3D data files to load. Can be either path or paths not both."
+       "paths", "List of paths to the .obj files to be loaded."
+       "add_properties", "List of of properties to add to the objects, or attributes to set."
+
     """
     def __init__(self, config):
         Loader.__init__(self, config)
 
     def run(self):
-        if self.config.has_param('path') and self.config.has_param('paths'):
-            raise Exception("Objectloader can not use path and paths in the same module!")
-        if self.config.has_param('path'):
-            file_path = Utility.resolve_path(self.config.get_string("path"))
-            loaded_objects = Utility.import_objects(filepath=file_path)
-        elif self.config.has_param('paths'):
-            file_paths = self.config.get_list('paths')
-            loaded_objects = []
-            for file_path in file_paths:
-                resolved_file_path = Utility.resolve_path(file_path)
-                loaded_objects.extend(Utility.import_objects(filepath=resolved_file_path))
-        else:
-            raise Exception("Loader module needs either a path or paths config value")
+        files_paths = Utility.resolve_path(self.config.get_string("paths"))
+        properties = Utility.get_dict(self.config.get_string("add_properties"))
 
-        # Set the add_properties of all imported objects
-        self._set_properties(loaded_objects)
+        for path in files_paths:
+            file_path = Utility.resolve_path(path)
+            loaded_objects = Utility.import_objects(filepath=file_path)
+            for obj in loaded_objects:
+                for key in properties.keys():
+                    if hasattr(obj, prop):
+                        obj.key = properties[key]
+                    else:
+                        obj[key] = properties[key]
+
+            # Set the physics property of all imported objects
+            self._set_physics_property(loaded_objects)
