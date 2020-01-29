7ebca33dfe0fa099d934cd8f4ed706a39f7c0067
Maximilian Denninger <maximilian.denninger@dlr.de>
2020-01-29 09:15:28 +0100

    Update material randomizer to be able to use selectors

--- src/materials/MaterialRandomizer.py
+++ src/materials/MaterialRandomizer.py
@@ -29,14 +29,20 @@ class MaterialRandomizer(Module):
 
         Module.__init__(self, config)
         self.randomization_level = self.config.get_float("randomization_level", 0.2)
-        self.randomize_textures_only = self.config.get_float("randomize_textures_only", False)
-        self.output_textures_only = self.config.get_float("output_textures_only", True)
+        self.randomize_textures_only = self.config.get_bool("randomize_textures_only", False)
+        self.output_textures_only = self.config.get_bool("output_textures_only", True)
+        self._objects_to_manipulate = None
+        if self.config.has_param('manipulated_objects'):
+            self._objects_to_manipulate = self.config.get_list('manipulated_objects')
+        self._objects_to_extract_materials_from = None
+        if self.config.has_param('objects_to_extract_mat'):
+            self._objects_to_extract_materials_from = self.config.get_list('objects_to_extract_mat')
         self.scene_materials = []
 
     def run(self):
 
         self._store_all_materials()
-        if self.scene_materials:
+        if len(self.scene_materials) > 0:
             self._randomize_materials_in_scene()
         else:
             print("Warning there are no materials, which can be switched!")
@@ -47,8 +53,15 @@ class MaterialRandomizer(Module):
         Randomizes the materials of objects in a loaded scene
         """
 
-        for obj in bpy.context.scene.objects:
-            self._randomize_material_for_obj(obj)
+        if self._objects_to_manipulate is not None:
+            objects = self._objects_to_manipulate
+        else:
+            objects = bpy.context.scene.objects
+
+        for obj in objects:
+            # check if the object really has a material slot
+            if hasattr(obj, 'material_slots'):
+                self._randomize_material_for_obj(obj)
 
     def _randomize_material_for_obj(self, obj):
 
@@ -73,8 +86,7 @@ class MaterialRandomizer(Module):
         """
 
         if np.random.uniform(0, 1) <= self.randomization_level:
-            random_index = np.random.random_integers(0, len(self.scene_materials) - 1)
-            m.material = self.scene_materials[random_index]
+            m.material = np.random.choice(self.scene_materials)
 
     def _store_all_materials(self):
 
@@ -82,7 +94,11 @@ class MaterialRandomizer(Module):
         Stores all available materials(with or without textures depending on output_textures_only) from the scene
         """
 
-        for obj in bpy.context.scene.objects:
+        if self._objects_to_extract_materials_from is not None:
+            objects = self._objects_to_extract_materials_from
+        else:
+            objects = bpy.context.scene.objects
+        for obj in objects:
             for m in obj.material_slots:
                 if self.output_textures_only:
                     # check if any texture nodes are in this material, no check if they are connected to the output
