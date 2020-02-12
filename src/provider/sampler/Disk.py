b86fb7cf3c976d6f6f8dd3510d068f3e04aad8e4
Dmitry Olefir <dmitry.olefir@dlr.de>
2020-02-12 14:12:45 +0100

    refactoring+docs(sampler): changed mode to boolean, updated docs

--- src/provider/sampler/Disk.py
+++ src/provider/sampler/Disk.py
@@ -4,7 +4,7 @@ import mathutils
 from src.main.Provider import Provider
 
 class Disk(Provider):
-    """ Samples a point on a 1-sphere (circle) or on a 2-ball (disk).
+    """ Samples a point on a 1-sphere (circle) or on a 2-ball (disk, i.e. circle + interior space).
 
     **Configuration**:
 
@@ -13,7 +13,7 @@ class Disk(Provider):
 
        "center", "Center of a disk. Type: mathutils Vector."
        "radius", "The radius of the disk. Type: float."
-       "mode", "Mode of sampling, i.e. which geometric shape to sample from.  Optional. Type: string. Available values: `circle`, 'disk'. Default value: disk."
+       "use_circle_only", "Mode of sampling, i.e. which geometric shape to sample from.  Optional. Type: boolean. Default value: False (sampling from a disk)."
        "up_vector", "An up vector which specifies a local coordinate system of the disk. Optional. Type: mathutils Vector. Default value: [0, 0, 1]"
     """
 
@@ -29,7 +29,7 @@ class Disk(Provider):
         # Radius of the disk.
         radius = self.config.get_float("radius")
         # Mode of operation.
-        mode = self.config.get_string("mode", "disk")
+        use_circle_only = self.config.get_bool("use_circle_only", False)
         # Up vector
         up_vector = self.config.get_vector3d("up_vector", [0, 0, 1])
         # Sample directions
@@ -42,13 +42,11 @@ class Disk(Provider):
         norm = np.sqrt(direction.dot(direction))
 
         # If sampling from the circle set magnitude to radius of the disk
-        if mode == "circle":
+        if use_circle_only:
             magnitude = radius
         # If sampling from a disk set it to scaled radius
-        elif mode == "disk":
-            magnitude = radius * np.sqrt(np.random.uniform())
         else:
-            raise Exception("Unknown sampling mode: " + mode)
+            magnitude = radius * np.sqrt(np.random.uniform())
 
         # Normalize and transform to 3d
         sampled_point = np.append(list(map(lambda x: magnitude*x/norm, direction)), 0)
