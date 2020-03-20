46d2041bad0109ad821f1721d278791132bb7e09
Dmitry Olefir <dmitry.olefir@dlr.de>
2020-03-20 18:32:04 +0100

    feat(module): add texture images handling

--- src/materials/MaterialManipulator.py
+++ src/materials/MaterialManipulator.py
@@ -1,4 +1,6 @@
 import bpy
+import os
+
 from src.main.Module import Module
 from src.utility.Config import Config
 from src.utility.Utility import Utility
@@ -13,14 +15,119 @@ class MaterialManipulator(Module):
 
     .. csv-table::
        :header: "Parameter", "Description"
-       "selector" "To select the desired materials, should use getter.Material (provider). Type: List of materials"
-       "mode" "There are two modes for setting attribute values, can be either 'once_for_each' or 'once_for_all', default: 'once_for_each'"
-       "color_link_to_displacement" "Links the output of the texture image to the displacement, the factor determines the strength of the displacement. Type: float"
-       "change_to_vertex_color" "Changes the material to a vertex coloring mode, the value should be the name of the vertex color layer. Type: string"
+       "selector" "Materials to be modified (selection via getter.Material Provider). Type: list."
+       "mode" "Mode of operation. Available values: 'once_for_each' (sampling the values for each selected material anew), 'once_for_all' (sampling once for all of the selected materials). Optional. Default value: 'once_for_each'. Type: string."
+       "color_link_to_displacement" "Factor that determining the strength of the displacement via linking the output of the texture image to the displacement Type: float"
+       "change_to_vertex_color" "The name of the vertex color layer, used for changing the material to a vertex coloring mode. Type: string"
+       "textures", "Texture data as {texture_type (type of the image/map, i.e. color, roughness, reflection, etc.): texture_path} pairs. Texture_type should be equal to the Shaderinput name in order to be assigned to a ShaderTexImage node that will be linked to this input. Label represents to which shader input this node is aconnected. Type: dict."
+       "textures/texture_path", "Path to a texture image. Type: string."
     """
 
     def __init__(self, config):
         Module.__init__(self, config)
+
+    def run(self):
+        set_params = {}
+        sel_objs = {}
+        for key in self.config.data.keys():
+            # if its not a selector -> to the set parameters dict
+            if key != 'selector':
+                set_params[key] = self.config.data[key]
+            else:
+                sel_objs[key] = self.config.data[key]
+        # create Config objects
+        params_conf = Config(set_params)
+        sel_conf = Config(sel_objs)
+        # invoke a Getter, get a list of entities to manipulate
+        materials = sel_conf.get_list("selector")
+
+        op_mode = self.config.get_string("mode", "once_for_each")
+
+        if op_mode == "once_for_all":
+            # get values to set if they are to be set/sampled once for all selected materials
+            params = self._get_set_params(params_conf)
+
+        for material in materials:
+            if not material.use_nodes:
+                raise Exception("This material does not use nodes -> not supported here.")
+
+            if op_mode == "once_for_each":
+                # get values to set if they are to be set/sampled anew for each selected entity
+                params = self._get_set_params(params_conf)
+
+            for key in params.keys():
+                # if an attribute with such name exists for this entity
+                if key == "color_link_to_displacement":
+                    MaterialManipulator._link_color_to_displacement_for_mat(material, params[key])
+                elif key == "change_to_vertex_color":
+                    MaterialManipulator._map_vertex_color(material, params[key])
+                elif key == "textures":
+                    loaded_textures = self._load_textures(params[key])
+                    self._set_textures(loaded_textures, material)
+                elif hasattr(material, key):
+                    # set the value
+                    setattr(material, key, params[key])
+
+                # TODO exclude global settings to raise exception if attribute not found
+                #else:
+                #    raise Exception("This attribute: {} is not there!".format(key))
+
+    def _get_set_params(self, params_conf):
+        """ Extracts actual values to set from a Config object.
+
+        :param params_conf: Object with all user-defined data. Type: Config.
+        :return: Parameters to set as {name of the parameter: it's value} pairs. Type: dict.
+        """
+        params = {}
+        for key in params_conf.data.keys():
+            result = None
+            if key == "color_link_to_displacement":
+                result = params_conf.get_float(key)
+            elif key == "change_to_vertex_color":
+                result = params_conf.get_string(key)
+            elif key == "textures":
+                result = {}
+                paths_conf = Config(params_conf.get_raw_dict(key))
+                for text_key in paths_conf.data.keys():
+                    text_path = paths_conf.get_string(text_key)
+                    result.update({text_key: text_path})
+            else:
+                result = params_conf.get_raw_value(key)
+
+            params.update({key: result})
+
+        return params
+
+    def _load_textures(self, text_paths):
+        """ Loads textures.
+
+        :param text_paths: Texture data as {texture type (image/map type, i.e. color, roughness, reflection, etc.): texture path} pairs. Type: dict.
+        :return: Loaded texture data as {texture type (image/map type, i.e. color, roughness, reflection, etc.): texture object} pairs. Type: dict.
+        """
+        loaded_textures = {}
+        for key in text_paths.keys():
+            bpy.ops.image.open(filepath=text_paths[key], directory=os.path.dirname(text_paths[key]))
+            loaded_textures.update({key: bpy.data.images.get(os.path.basename(text_paths[key]))})
+
+        return loaded_textures
+
+    def _set_textures(self, loaded_textures, material):
+        """ Creates a ShaderNodeTexImage node, assigns a loaded image to it and connects to the shader of the selected materials.
+
+        :param loaded_textures: Loaded texture data as {texture type: texture object} pairs. Type: dict.
+        :param material: Material to be modified. Type: bpy.material.
+        """
+        nodes = material.node_tree.nodes
+        links = material.node_tree.links
+        # for each Image Texture node set a texture (image) if one was loaded
+        for key in loaded_textures.keys():
+            nodes.new('ShaderNodeTexImage')
+            nodes[-1].label = key
+            node = nodes.get(nodes[-1].name)
+            a = nodes.get(nodes[-1].name).outputs['Color']
+            b = nodes["Principled BSDF"].inputs[key]
+            node.image = loaded_textures[key]
+            links.new(a, b)
 
     @staticmethod
     def _link_color_to_displacement_for_mat(material, multiply_factor):
@@ -70,48 +177,3 @@ class MaterialManipulator(Module):
         else:
             raise Exception("The material: {} has no node connected to the output, "
                             "which has as an input Base Color".format(material.name))
-
-    def run(self):
-        set_params = {}
-        sel_objs = {}
-        for key in self.config.data.keys():
-            # if its not a selector -> to the set parameters dict
-            if key != 'selector':
-                set_params[key] = self.config.data[key]
-            else:
-                sel_objs[key] = self.config.data[key]
-        # create Config objects
-        params_conf = Config(set_params)
-        sel_conf = Config(sel_objs)
-        # invoke a Getter, get a list of entities to manipulate
-        materials = sel_conf.get_list("selector")
-
-        op_mode = self.config.get_string("mode", "once_for_each")
-        for key in params_conf.data.keys():
-            result = None
-            if op_mode == "once_for_all":
-                # get raw value from the set parameters if it is to be sampled anew for each selected entity
-                result = params_conf.get_raw_value(key)
-
-            for material in materials:
-                if not material.use_nodes:
-                    raise Exception("This material does not use nodes -> not supported here.")
-
-                if op_mode == "once_for_each":
-                    # get raw value from the set parameters if it is to be sampled anew for each selected entity
-                    result = params_conf.get_raw_value(key)
-                if result is None:
-                    raise Exception("This mode is unknown: {}".format(op_mode))
-                # if an attribute with such name exists for this entity
-                if key == "color_link_to_displacement":
-                    MaterialManipulator._link_color_to_displacement_for_mat(material, result)
-                elif key == "change_to_vertex_color":
-                    MaterialManipulator._map_vertex_color(material, result)
-                elif hasattr(material, key):
-                    # set the value
-                    setattr(material, key, result)
-
-                # TODO exclude global settings to raise exception if attribute not found
-                #else:
-                #    raise Exception("This attribute: {} is not there!".format(key))
-
