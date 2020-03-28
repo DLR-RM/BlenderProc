389dc41aa8bd57ff8487741bdcc65cbf2a4b39fb
Dmitry Olefir <dmitry.olefir@dlr.de>
2020-03-29 00:00:20 +0100

    feat(sampler): add sampling from sector and arc

--- src/provider/sampler/Disk.py
+++ src/provider/sampler/Disk.py
@@ -4,17 +4,26 @@ import mathutils
 from src.main.Provider import Provider
 
 class Disk(Provider):
-    """ Samples a point on a 1-sphere (circle) or on a 2-ball (disk, i.e. circle + interior space).
+    """ Samples a point on a 1-sphere (circle), or on a 2-ball (disk, i.e. circle + interior space), or on an arc/sector
+        with an inner angle less than 180 degrees. Returns a 3d mathutils.Vector sampled point.
 
     **Configuration**:
 
     .. csv-table::
        :header:, "Parameter", "Description"
 
-       "center", "Center of a disk. Type: mathutils Vector."
+       "center", "Center of a disk. Type: mathutils.Vector."
        "radius", "The radius of the disk. Type: float."
-       "use_circle_only", "Mode of sampling, i.e. which geometric shape to sample from.  Optional. Type: boolean. Default value: False (sampling from a disk)."
-       "up_vector", "An up vector which specifies a local coordinate system of the disk. Optional. Type: mathutils Vector. Default value: [0, 0, 1]"
+       "up_vector", "An up vector which specifies a local coordinate system of the disk. Optional."
+                    "Type: mathutils.Vector. Default value: [0, 0, 0]"
+       "sample_from", "Mode of sampling. Defines the geometrical structure used for sampling, i.e. the shape to sample"
+                      "from. Type: string. Default value: "disk". Available values: "disk", "circle", "sector", "arc"."
+       "start_angle", "Start angle in degrees that is used to define a sector/arc to sample from. Must be smaller"
+                      "than end_angle. Arc's/sector's inner angle must be less or equal than 180 degrees. Type: float."
+                      "Optional. Default value: 0."
+       "end_angle", "End angle in degrees that is used to define a sector/arc to sample from. Must be bigger than"
+                    "start_angle. Arc's/sector's inner angle must be less or equal than 180 degrees. Type: float."
+                    "Optional. Default value: 180."
     """
 
     def __init__(self, config):
@@ -24,38 +33,68 @@ class Disk(Provider):
         """
         :return: A random point sampled point on a 1-sphere (circle) or on a 2-ball (disk). Type: Mathutils vector.
         """
-        # Center of the disk.
         center = self.config.get_vector3d("center")
-        # Radius of the disk.
         radius = self.config.get_float("radius")
-        # Mode of operation.
-        use_circle_only = self.config.get_bool("use_circle_only", False)
-        # Up vector
-        up_vector = self.config.get_vector3d("up_vector", [0, 0, 1])
-        # Sample directions
-        direction = np.random.normal(size=2)
+        up_vector = self.config.get_vector3d("up_vector", [0, 0, 0])
+        sample_from = self.config.get_string("sample_from", "disk")
+        # check if the mode/sampling structure is supported
+        if sample_from not in ["disk", "circle", "sector", "arc"]:
+            raise Exception("Unknown mode of operation: " + sample_from)
+        # if mode is sampling from sector or arc
+        if sample_from in ["arc", "sector"]:
+            # get start and end angles
+            start_angle = self.config.get_float("start_angle", 0)
+            end_angle = self.config.get_float("end_angle", 180)
+            # check if start and end angles comply to boundaries
+            if not all([start_angle < end_angle, abs(start_angle - end_angle) <= 180]):
+                raise Exception("Sector's/arch's start and end points are defined wrong! Boundaries to comply with:"
+                                "1. start_angle < end_angle; 2. abs(start_angle - end_angle) <= 180.")
+            # transform to 2d vectors
+            start_vec = [np.cos(np.deg2rad(start_angle)), np.sin(np.deg2rad(start_angle))]
+            end_vec = [np.cos(np.deg2rad(end_angle)), np.sin(np.deg2rad(end_angle))]
 
-        if np.count_nonzero(direction)==0:
-            direction[0] = 1e-5
-
-        # For normalization
-        norm = np.sqrt(direction.dot(direction))
-
-        # If sampling from the circle set magnitude to radius of the disk
-        if use_circle_only:
+        # if sampling from the circle or arc set magnitude to radius
+        if sample_from in ["circle", "arc"]:
             magnitude = radius
-        # If sampling from a disk set it to scaled radius
-        else:
+        # if sampling from a disk or sector set it to scaled radius
+        elif sample_from in ["disk", "sector"]:
             magnitude = radius * np.sqrt(np.random.uniform())
 
-        # Normalize and transform to 3d
-        sampled_point = np.append(list(map(lambda x: magnitude*x/norm, direction)), 0)
-        
-        # Get Euler angles from up vector
+        sampled_point = self._sample_point(magnitude)
+
+        # sample a point until it falls into the defined sector/arc
+        if sample_from in ["arc", "sector"]:
+            while not all([not self._is_clockwise(start_vec, sampled_point), self._is_clockwise(end_vec, sampled_point)]):
+                sampled_point = self._sample_point(magnitude)
+
+        # get Euler angles from up vector
         euler_angles = up_vector.to_track_quat('Z', 'Y').to_euler()
-        # Get rotation
+        # get rotation
         rot_mat = mathutils.Euler(euler_angles, 'XYZ').to_matrix()
-        # Get location on a rotated disk and add center
+        # apply rotation and add center
         location = rot_mat @ mathutils.Vector(sampled_point) + center
 
         return location
+
+    def _sample_point(self, magnitude):
+        """ Samples a 3d point from a two-dimensional normal distribution with the third dim equal to 0.
+
+        :param magnitude: Scaling factor of a radius. Type: float.
+        :return: Sampled 3d point. Type: numpy.array.
+        """
+        direction = np.random.normal(size=2)
+        if np.count_nonzero(direction) == 0:
+            direction[0] = 1e-5
+        norm = np.sqrt(direction.dot(direction))
+        sampled_point = np.append(list(map(lambda x: magnitude * x / norm, direction)), 0)
+
+        return sampled_point
+
+    def _is_clockwise(self, rel_point, sampled_point):
+        """ Checks if the sampled_point is in the clockwise direction in relation to the rel_point.
+
+        :param rel_point: Point relative to which the test is performed. Type: 2d vector.
+        :param sampled_point: Point for which test is performed. Type: 2d vector.
+        :return: True if the sampled_point lies in the clockwise direction in relation to the rel_point, False if not.
+        """
+        return (-rel_point[0] * sampled_point[1] + rel_point[1] * sampled_point[0]) > 0
