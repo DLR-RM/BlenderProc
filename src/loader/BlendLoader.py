0c6393e4f5b56dd153dcd06990fce65b6c1d2d72
Dmitry Olefir <dmitry.olefir@dlr.de>
2020-03-29 22:49:42 +0200

    doc(loader): update documentation

--- src/loader/BlendLoader.py
+++ src/loader/BlendLoader.py
@@ -1,12 +1,13 @@
 import bpy
 import os
+import re
 
 from src.loader.Loader import Loader
 from src.utility.Utility import Utility
 
 
-class BlendCollectionLoader(Loader):
-    """ Loads all collections from the .blend's file Collection section.
+class BlendLoader(Loader):
+    """ Loads entities from a specified .blend file's section/datablock.
 
     **Configuration**:
 
@@ -14,16 +15,51 @@ class BlendCollectionLoader(Loader):
        :header: "Parameter", "Description"
 
        "path", "Path to a .blend file. Type: string."
+       "load_from", "Name of the datablock/folder inside .blend file. Type: string. See known_datablock_names for supported folder names/type IDs."
+       "entities", "Regular expression representing a name pattern of entities' names. Optional. Type: string."
     """
+
     def __init__(self, config):
         Loader.__init__(self, config)
+        # supported pairs of {datablock parameter names: ID type/section names}
+        self.known_datablock_names = {"cameras": "/Camera",
+                                      "collections": "/Collection",
+                                      "images": "/Image",
+                                      "lights": "/Light",
+                                      "materials": "/Material",
+                                      "meshes": "/Mesh",
+                                      "objects": "/Object",
+                                      "textures": "/Texture"}
 
     def run(self):
-        # get a path
+        # get a path to a .blend file
         path = Utility.resolve_path(self.config.get_string("path"))
-        # load all collections from a .blend file
-        with bpy.data.libraries.load(path) as (data_from, data_to):
-            for collection in data_from.collections:
+        # get section name/Blend ID
+        load_from = self.config.get_string("load_from")
+        # get a entities' name regex if present, set to None if not
+        if self.config.has_param("entities"):
+            entities = self.config.get_string("entities")
+        else:
+            entities = None
 
-                bpy.ops.wm.append(filepath=os.path.join(path, "/Collection", collection), filename=collection,
-                                  directory=os.path.join(path + "/Collection"))
+        with bpy.data.libraries.load(path) as (data_from, data_to):
+            # check if defined ID is supported
+            if load_from in self.known_datablock_names.keys():
+                # if some regex was specified, get corresponding matching entity's names
+                if entities is not None:
+                    entities_to_load = [item for item in getattr(data_from, load_from)
+                                        if re.fullmatch(entities, item) is not None]
+                # get all entity's names if not
+                else:
+                    entities_to_load = getattr(data_from, load_from)
+                # load entities
+                for entity_to_load in entities_to_load:
+                    bpy.ops.wm.append(filepath=os.path.join(path, self.known_datablock_names[load_from], entity_to_load),
+                                      filename=entity_to_load,
+                                      directory=os.path.join(path + self.known_datablock_names[load_from]))
+            else:
+                raise Exception("Unsupported datablock parameter name: " + load_from +
+                                ".\nSupported names: " + str(self.known_datablock_names.keys()) +
+                                ".\n All possible names: " + str(dir(data_from)) +
+                                "\n. If your ID exists, but not supported, please append a new pair of"
+                                "{parameter name: type ID(folder name)} to the 'known_datablock_names' dict.")
