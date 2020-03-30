b1bad32cbf1eeca5368f3e2cdfa7993e2403dd66
Dmitry Olefir <dmitry.olefir@dlr.de>
2020-03-30 14:27:36 +0200

    refactor(loader): inprove docu, switched key and values

--- src/loader/BlendLoader.py
+++ src/loader/BlendLoader.py
@@ -15,21 +15,21 @@ class BlendLoader(Loader):
        :header: "Parameter", "Description"
 
        "path", "Path to a .blend file. Type: string."
-       "load_from", "Name of the datablock/folder inside .blend file. Type: string. See known_datablock_names for supported folder names/type IDs."
+       "load_from", "Name of the datablock/folder inside .blend file. Always start with '/' Type: string. See known_datablock_names for supported folder names/type IDs."
        "entities", "Regular expression representing a name pattern of entities' names. Optional. Type: string."
     """
 
     def __init__(self, config):
         Loader.__init__(self, config)
-        # supported pairs of {datablock parameter names: ID type/section names}
-        self.known_datablock_names = {"cameras": "/Camera",
-                                      "collections": "/Collection",
-                                      "images": "/Image",
-                                      "lights": "/Light",
-                                      "materials": "/Material",
-                                      "meshes": "/Mesh",
-                                      "objects": "/Object",
-                                      "textures": "/Texture"}
+        # supported pairs of {ID type/section names: datablock parameter names}
+        self.known_datablock_names = {"/Camera": "cameras",
+                                      "/Collection": "collections",
+                                      "/Image": "images",
+                                      "/Light": "lights",
+                                      "/Material": "materials",
+                                      "/Mesh": "meshes",
+                                      "/Object": "objects",
+                                      "/Texture": "textures"}
 
     def run(self):
         # get a path to a .blend file
@@ -47,19 +47,19 @@ class BlendLoader(Loader):
             if load_from in self.known_datablock_names.keys():
                 # if some regex was specified, get corresponding matching entity's names
                 if entities is not None:
-                    entities_to_load = [item for item in getattr(data_from, load_from)
+                    entities_to_load = [item for item in getattr(data_from, self.known_datablock_names[load_from])
                                         if re.fullmatch(entities, item) is not None]
                 # get all entity's names if not
                 else:
-                    entities_to_load = getattr(data_from, load_from)
+                    entities_to_load = getattr(data_from, self.known_datablock_names[load_from])
                 # load entities
                 for entity_to_load in entities_to_load:
-                    bpy.ops.wm.append(filepath=os.path.join(path, self.known_datablock_names[load_from], entity_to_load),
+                    bpy.ops.wm.append(filepath=os.path.join(path, load_from, entity_to_load),
                                       filename=entity_to_load,
-                                      directory=os.path.join(path + self.known_datablock_names[load_from]))
+                                      directory=os.path.join(path + load_from))
             else:
-                raise Exception("Unsupported datablock parameter name: " + load_from +
-                                ".\nSupported names: " + str(self.known_datablock_names.keys()) +
-                                ".\n All possible names: " + str(dir(data_from)) +
-                                "\n. If your ID exists, but not supported, please append a new pair of"
-                                "{parameter name: type ID(folder name)} to the 'known_datablock_names' dict.")
+                raise Exception("Unsupported datablock/folder name: " + load_from +
+                                "\nSupported names: " + str(self.known_datablock_names.keys()) +
+                                "\nIf your ID exists, but not supported, please append a new pair of "
+                                "{type ID(folder name): parameter name} to the 'known_datablock_names' dict. Use this "
+                                "for finding your parameter name: " + str(dir(data_from)))
