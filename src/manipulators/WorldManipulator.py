cc0701341edda951c3575c9fb00f68d02eb6554d
Dmitry Olefir <dmitry.olefir@dlr.de>
2020-04-01 06:25:52 +0200

    doc(manipulator): add documentation and examples

--- src/manipulators/WorldManipulator.py
+++ src/manipulators/WorldManipulator.py
@@ -5,17 +5,58 @@ from src.main.Module import Module
 
 
 class WorldManipulator(Module):
-    """ Allows basic manipulation of the blender world. Specify any desired {key: value} pairs.
-    Each pair is treated like a {attribute_name:attribute_value} where attr_name is a custom property or a name of a
-     custom property to create, while the attr_value is its new value.
+    """ Allows manipulation of the current World in the scene via specifying one or more {attr name/custom prop. name/
+        custom name: value} pairs.
 
+        Example 1: Sets the World's custom property `category_id` to 123
+
+        {
+          "module": "manipulators.WorldManipulator",
+          "config": {
+            "cp_category_id": 123
+          }
+        }
+
+        Example 2: Sets the color and the strength of the light emitted by the background surface.
+
+        {
+          "module": "manipulators.WorldManipulator",
+          "config": {
+            "cn_bg_surface_color": [1, 1, 1, 1],
+            "cn_bg_surface_strength": 100
+          }
+        }
+
+        Example 3: Disables shader node tree of the background surface and sets a solid color to the background surface
+
+        {
+          "module": "manipulators.WorldManipulator",
+          "config": {
+            "use_nodes": False,
+            "color": [0.5, 0.5, 0.5]
+          }
+        }
 
     **Configuration**:
 
     .. csv-table::
         :header: "Parameter", "Description"
 
-        "custom_property_name": "Value that custom_property should be set to."
+        "key": "Name of the attribute/custom prop. to change as a key in {name of an attr: value to set}. Type: string."
+               "In order to specify, what exactly one want to modify (e.g. attribute, custom property, etc.):"
+               "For attribute: key of the pair must be a valid attribute name of the world."
+               "For custom property: key of the pair must start with `cp_` prefix."
+               "For calling custom method: key of the pair must start with `cn_` prefix. See table below for supported"
+               "cn names."
+        "value": "Value of the attribute/custom prop. to set as a value in {name of an attr: value to set}."
+
+    **Custom names**:
+
+    .. csv-table::
+        :header: "Parameter", "Description"
+
+        "bg_surface_color", "Sets the color of the light emitted by the background. Input value type: list of RGBA values."
+        "bg_surface_strength", "Sets the strength of the light emitted by the background. Input value type: float."
     """
 
     def __init__(self, config):
@@ -24,9 +65,10 @@ class WorldManipulator(Module):
     def run(self):
         world = bpy.context.scene.world
         for key in self.config.data.keys():
-
             requested_cp = False
             requested_cn = False
+
+            value = self.config.get_raw_value(key)
 
             if key.startswith('cp_'):
                 requested_cp = True
@@ -34,86 +76,34 @@ class WorldManipulator(Module):
             elif key.startswith('cn_'):
                 requested_cn = True
                 key = key[3:]
-
             if hasattr(world, key) and all([not requested_cp, not requested_cn]):
-                setattr(world, key, self.config.get_raw_value(key))
+                setattr(world, key, value)
             elif requested_cp:
-                world[key] = self.config.get_raw_value(key)
+                world[key] = value
             elif requested_cn:
+
                 if key == "bg_surface_color":
-                    self._set_bg_surface_color(world, self.config.get_raw_value(key))
+                    self._set_bg_surface_color(world, value)
                 elif key == "bg_surface_strength":
-                    self._set_bg_surface_strength(world, self.config.get_raw_value(key))
-                elif key == "bg_volume_color":
-                    if not self._bg_volume_is_enabled(world):
-                        self._enable_bg_volume(world)
-                    self._set_bg_volume_color(world, self.config.get_raw_value(key))
-                elif key == "bg_volume_strength":
-                    if not self._bg_volume_is_enabled(world):
-                        self._enable_bg_volume(world)
-                    self._set_bg_volume_strength(world, self.config.get_raw_value(key))
+                    self._set_bg_surface_strength(world, value)
                 else:
                     raise RuntimeError('Unknown cn_ parameter: ' + key)
+
             else:
                 raise RuntimeError('Unknown parameter: ' + key)
 
     def _set_bg_surface_color(self, world, color):
-        """
+        """ Sets the color of the emitted light by the background surface.
 
-        :param world:
-        :param color:
+        :param world: World to modify. Type: World.
+        :param color: Color of the emitted light. Type: RGBA vector.
         """
-        world.use_nodes = True
         world.node_tree.nodes["Background"].inputs[0].default_value = color
 
     def _set_bg_surface_strength(self, world, strength):
-        """
+        """ Sets the strength of the emitted light by the background surface.
 
-        :param world:
-        :param strength:
+        :param world: World to modify. Type: World.
+        :param strength: Strength of the emitted light. Type: float.
         """
-        world.use_nodes = True
         world.node_tree.nodes["Background"].inputs[1].default_value = strength
-
-    def _set_bg_volume_color(self, world, color):
-        """
-
-        :param world:
-        :param color:
-        """
-        world.node_tree.nodes["Background.001"].inputs[1].default_value = color
-
-    def _set_bg_volume_strength(self, world, strength):
-        """
-
-        :param world:
-        :param strength:
-        """
-        world.node_tree.nodes["Background.001"].inputs[1].default_value = strength
-
-    def _bg_volume_is_enabled(self, world):
-        """
-
-        :param world:
-        :return:
-        """
-        if len(Utility.get_nodes_with_type(world, "ShaderNodeBackground")) > 1:
-            is_enabled = True
-        else:
-            is_enabled = False
-
-        return is_enabled
-
-
-    def _enable_bg_volume(self, world):
-        """
-
-        :param world:
-        """
-        world.use_nodes = True
-        nodes = bpy.context.scene.world.node_tree.nodes
-        links = bpy.context.scene.world.node_tree.links
-        nodes.new("ShaderNodeBackground")
-        node_output = nodes['Background.001'].outputs['Background']
-        node_input = nodes['World Output'].inputs['Volume']
-        links.new(node_output, node_input)
