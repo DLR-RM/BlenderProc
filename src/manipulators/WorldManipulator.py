a0065439fe8faa037b26f9d193deefd1c67b2235
Dmitry Olefir <dmitry.olefir@dlr.de>
2020-04-01 05:01:35 +0200

    feat(manipulator): add setting of attributes

--- src/manipulators/WorldManipulator.py
+++ src/manipulators/WorldManipulator.py
@@ -1,4 +1,6 @@
 import bpy
+
+from src.utility.Utility import Utility
 from src.main.Module import Module
 
 
@@ -20,5 +22,98 @@ class WorldManipulator(Module):
         Module.__init__(self, config)
 
     def run(self):
+        world = bpy.context.scene.world
         for key in self.config.data.keys():
-            bpy.context.scene.world[key] = self.config.get_raw_value(key)
+
+            requested_cp = False
+            requested_cn = False
+
+            if key.startswith('cp_'):
+                requested_cp = True
+                key = key[3:]
+            elif key.startswith('cn_'):
+                requested_cn = True
+                key = key[3:]
+
+            if hasattr(world, key) and all([not requested_cp, not requested_cn]):
+                setattr(world, key, self.config.get_raw_value(key))
+            elif requested_cp:
+                world[key] = self.config.get_raw_value(key)
+            elif requested_cn:
+                if key == "bg_surface_color":
+                    self._set_bg_surface_color(world, self.config.get_raw_value(key))
+                elif key == "bg_surface_strength":
+                    self._set_bg_surface_strength(world, self.config.get_raw_value(key))
+                elif key == "bg_volume_color":
+                    if not self._bg_volume_is_enabled(world):
+                        self._enable_bg_volume(world)
+                    self._set_bg_volume_color(world, self.config.get_raw_value(key))
+                elif key == "bg_volume_strength":
+                    if not self._bg_volume_is_enabled(world):
+                        self._enable_bg_volume(world)
+                    self._set_bg_volume_strength(world, self.config.get_raw_value(key))
+                else:
+                    raise RuntimeError('Unknown cn_ parameter: ' + key)
+            else:
+                raise RuntimeError('Unknown parameter: ' + key)
+
+    def _set_bg_surface_color(self, world, color):
+        """
+
+        :param world:
+        :param color:
+        """
+        world.use_nodes = True
+        world.node_tree.nodes["Background"].inputs[0].default_value = color
+
+    def _set_bg_surface_strength(self, world, strength):
+        """
+
+        :param world:
+        :param strength:
+        """
+        world.use_nodes = True
+        world.node_tree.nodes["Background"].inputs[1].default_value = strength
+
+    def _set_bg_volume_color(self, world, color):
+        """
+
+        :param world:
+        :param color:
+        """
+        world.node_tree.nodes["Background.001"].inputs[1].default_value = color
+
+    def _set_bg_volume_strength(self, world, strength):
+        """
+
+        :param world:
+        :param strength:
+        """
+        world.node_tree.nodes["Background.001"].inputs[1].default_value = strength
+
+    def _bg_volume_is_enabled(self, world):
+        """
+
+        :param world:
+        :return:
+        """
+        if len(Utility.get_nodes_with_type(world, "ShaderNodeBackground")) > 1:
+            is_enabled = True
+        else:
+            is_enabled = False
+
+        return is_enabled
+
+
+    def _enable_bg_volume(self, world):
+        """
+
+        :param world:
+        """
+        world.use_nodes = True
+        nodes = bpy.context.scene.world.node_tree.nodes
+        links = bpy.context.scene.world.node_tree.links
+        nodes.new("ShaderNodeBackground")
+        node_output = nodes['Background.001'].outputs['Background']
+        node_input = nodes['World Output'].inputs['Volume']
+        links.new(node_output, node_input)
