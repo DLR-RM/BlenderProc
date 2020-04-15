ab0ba972f3336c11dc2d555abd711362b07b1743
Dmitry Olefir <dmitry.olefir@dlr.de>
2020-04-15 17:04:44 +0200

    fix(script): fix condition

--- scripts/format_coco_annotations.py
+++ scripts/format_coco_annotations.py
@@ -13,7 +13,7 @@ with open(args.path) as file:
     data = json.load(file)
 
 for i in range(len(data['annotations']) - 1, -1, -1):
-    if len(data['annotations'][i]):
+    if len(data['annotations'][i]['segmentation']) == 0:
         del data['annotations'][i]
 
 new_path = os.path.join(os.path.dirname(path), os.path.basename(path).split('.')[0] + '_formatted.json')
