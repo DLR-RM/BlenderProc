eb5465093e36c96a95bf1b0f833ad3b029fdbb1a
Dmitry Olefir <dmitry.olefir@dlr.de>
2020-04-15 16:15:39 +0200

    feat(script): add coco annotations formatting script

---
+++ scripts/format_coco_annotations.py
@@ -0,0 +1,21 @@
+import argparse
+import json
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--path', dest="path", type=str)
+
+args = parser.parse_args()
+
+path = args.path
+
+with open(args.path) as file:
+    data = json.load(file)
+
+for i in range(len(data['annotations']) - 1, -1, -1):
+    if len(data['annotations'][i]):
+        del data['annotations'][i]
+
+new_path = os.path.join(os.path.dirname(path), os.path.basename(path).split('.')[0] + '_formatted.json')
+with open(new_path, 'w') as f:
+    json.dump(data, f)
