ab1e6f115405d5d17b8b3ce50afaf8231a582432
Dmitry Olefir <dmitry.olefir@dlr.de>
2020-04-16 10:20:44 +0200

    doc(script): add extensive description

--- scripts/format_coco_annotations.py
+++ scripts/format_coco_annotations.py
@@ -1,9 +1,23 @@
+""" Formats the coco annotations .json file.
+
+When generating coco annotations for the rendered pipeline's images, some annotations may be corrupted due to objects
+being barely visible in the image: only 1px of the object being visible in the camera's fov leads to the empty
+segmentation mask and an annotation of wrong width/height. Those faulty annotations bring nothing useful to the table
+during training.
+
+When supplied with a path to a .json file, this script iterates through all annotations of this file, deletes ones with
+an empty segmentation masks, and then saves the formatted result a a new .json file.
+
+Input parameters:
+    * -p, --path: path to a .json coco annotations file.
+"""
+
 import argparse
 import json
 import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-p', '--path', dest="path", type=str)
+parser.add_argument('-p', '--path', dest="path", type=str, help='path to a coco annotations .json file')
 
 args = parser.parse_args()
 
