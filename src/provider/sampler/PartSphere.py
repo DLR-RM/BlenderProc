001167c20e8727ad888d68483dd087196a9fc6d1
Maximilian Denninger <maximilian.denninger@dlr.de>
2020-04-22 16:43:41 +0200

    feat(PartSphere): add a vector to specify the direction of the split

--- src/provider/sampler/PartSphere.py
+++ src/provider/sampler/PartSphere.py
@@ -3,11 +3,18 @@ import numpy as np
 from src.main.Provider import Provider
 from src.provider.sampler.Sphere import Sphere
 
-class PartZSphere(Provider):
+class PartSphere(Provider):
     """ Samples a point from the surface or from the interior of solid sphere
 
     Gaussian is spherically symmetric. Sample from three independent Gaussian distributions
     the direction of the vector inside the sphere. Then calculate magnitude based on the operation mode.
+
+    The Sphere is split along the part_sphere_vector, this gives two sphere parts. The distance is used to define
+    the half, which is used.
+
+    For example:
+
+    If the vector is parallel to the z-axis, the distance check is: sampled_location[2] > distance_above_center.
 
     **Configuration**:
 
@@ -17,6 +24,10 @@ class PartZSphere(Provider):
        "center", "A list of three values, describing the x, y and z coordinate of the center of the sphere."
        "radius", "The radius of the sphere."
        "mode", "Mode of sampling. SURFACE - sampling from the 2-sphere, INTERIOR - sampling from the 3-ball."
+       "distance_above_center", "The distance above the center, which should be used, Type: float, default: 0.0 (half of the sphere)"
+       "part_sphere_vector", "The direction in which the sphere should be split, the end point of the vector, will be"
+                             "in the middle of the sphere pointing towards the middle of the resulting surface.
+                             Type: mathutils.Vector, Default: [0,0,1]"
     """
 
     def __init__(self, config):
@@ -35,11 +46,16 @@ class PartZSphere(Provider):
         radius = self.config.get_float("radius")
         # Mode of operation.
         mode = self.config.get_string("mode")
-        z_above_center = self.config.get_float("z_above_center")
+        dist_above_center = self.config.get_float("distance_above_center", 0.0)
+        part_sphere_dir_vector = self.config.get_vector3d("part_sphere_vector", [0, 0, 1])
+        part_sphere_dir_vector.normalize()
 
-        if z_above_center >= radius:
-            raise Exception("The z_above_center value is bigger or as big as the radius!")
+        if dist_above_center >= radius:
+            raise Exception("The dist_above_center value is bigger or as big as the radius!")
+
         while True:
             location = Sphere.sample(center, radius, mode)
-            if location[2] > center[2] + z_above_center:
+            # project the location onto the part_sphere_dir_vector and get the length
+            length = location.dot(part_sphere_dir_vector)
+            if length > dist_above_center:
                 return location
