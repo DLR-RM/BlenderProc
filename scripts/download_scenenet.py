d2f5f099c185b4be9b909a9fddf805e2916fadfd
Maximilian Denninger <maximilian.denninger@dlr.de>
2020-04-22 11:33:39 +0200

    feat(download_scenenet): add script to download scenenet, texture is missing, path is printed though

---
+++ scripts/download_scenenet.py
@@ -0,0 +1,34 @@
+
+
+import os
+import csv
+from urllib.request import urlopen, Request, urlretrieve, build_opener, install_opener
+import subprocess
+
+
+if __name__ == "__main__":
+    # setting the default header, else the server does not allow the download
+    opener = build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    install_opener(opener)
+
+    # set the download directory relative to this one
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    scenenet_dir= os.path.join(current_dir, "..", "resources", "scenenet")
+
+    if not os.path.exists(scenenet_dir):
+        os.makedirs(scenenet_dir)
+
+    # download the zip file, which contains all the obj files
+    print("Download the zip file, may take a while:")
+    scenenet_url = "https://bitbucket.org/robotvault/downloadscenenet/get/cfe5ab85ddcc.zip"
+    zip_file_path = os.path.join(scenenet_dir, "scene_net.zip")
+    urlretrieve(scenenet_url, zip_file_path)
+
+    # unzip the zip file
+    print("Unzip the zip file.")
+    cmd = "unzip {}".format(zip_file_path)
+    subprocess.call(cmd, shell=True, cwd=os.path.dirname(zip_file_path))
+
+    print("Please also download the texture library from here: {}".fromat("http://tinyurl.com/zpc9ppb"))
+    print("This is a google drive folder downloading via script is tedious.")
