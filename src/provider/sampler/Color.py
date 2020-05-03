c9853589cb25e01921f7465e67ccf5f6ff94ba9f
Martin Sundermeyer <martin.sundermeyer@dlr.de>
2020-05-03 09:11:00 +0200

    flag for sampling only grey colors

--- src/provider/sampler/Color.py
+++ src/provider/sampler/Color.py
@@ -14,6 +14,7 @@ class Color(Provider):
 
        "min", "A list of four values, describing the minimum values for R, G, B and A components. Range: [0; 1]. Type: list."
        "max", "A list of four values, describing the maximum values for R, G, B and A components. Range: [0; 1]. Type: list."
+       "grey", "Sample grey values only. Type: bool. Default: False"
 
     """
 
@@ -29,11 +30,16 @@ class Color(Provider):
         min = self.config.get_vector4d("min")
         # maximum values vector
         max = self.config.get_vector4d("max")
+        # sample only grey values
+        grey = self.config.get_bool("grey", False)
 
         color = mathutils.Vector([0, 0, 0, 0])
         for i in range(4):
             if all([0 <= min[i] <= 1, 0 <= max[i] <= 1]):
-                color[i] = random.uniform(min[i], max[i])
+                if grey and 0 < i < 3:
+                    color[i] = color[i-1]
+                else:
+                    color[i] = random.uniform(min[i], max[i])
             else:
                 raise RuntimeError("min and max vectors must be composed of values in [0, 1] range!")
 
