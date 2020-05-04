b551f9684a6ed272efcc03ccac86c132d201b5f5
Dmitry Olefir <dmitry.olefir@dlr.de>
2020-05-04 15:19:55 +0200

    doc(manipulator): docu clean-up

--- src/manipulators/WorldManipulator.py
+++ src/manipulators/WorldManipulator.py
@@ -7,7 +7,7 @@ class WorldManipulator(Module):
     """ Allows manipulation of the current World in the scene via specifying one or more {attr name/custom prop. name/
         custom function name: value} pairs.
 
-        Example 1: Sets the World's custom property `category_id` to 123
+        Example 1: Sets the World's custom property `category_id` to 123.
 
         {
           "module": "manipulators.WorldManipulator",
@@ -26,7 +26,7 @@ class WorldManipulator(Module):
           }
         }
 
-        Example 3: Disables shader node tree of the background surface and sets a solid color to the background surface
+        Example 3: Disables shader node tree of the background surface and sets a solid color to the background surface.
 
         {
           "module": "manipulators.WorldManipulator",
@@ -42,9 +42,9 @@ class WorldManipulator(Module):
         :header: "Parameter", "Description"
 
         "key": "Name of the attribute/custom prop. to change as a key in {name of an attr: value to set}. Type: string."
-               "In order to specify, what exactly one wants to modify (e.g. attribute, custom property, etc.):"
-               "For attribute: key of the pair must be a valid attribute name of the world."
-               "For custom property: key of the pair must start with `cp_` prefix."
+               "In order to specify, what exactly one wants to modify (e.g. attribute, custom property, etc.): "
+               "For attribute: key of the pair must be a valid attribute name of the world. "
+               "For custom property: key of the pair must start with `cp_` prefix. "
                "For calling custom function: key of the pair must start with `cf_` prefix. See table below for "
                "supported custom function names."
         "value": "Value of the attribute/custom prop, etc. to set."
@@ -63,9 +63,9 @@ class WorldManipulator(Module):
         Module.__init__(self, config)
 
     def run(self):
-        """ XXX
+        """ Assigns user-defined values to World's attributes, custom properties, or manipulates the state of the world.
             1. Selects current active World.
-            2. Set defined attribute/custom property/custom function values.
+            2. Change World's state via setting user-defined values to it's attributes, custom properties, etc.
         """
         world = bpy.context.scene.world
         for key in self.config.data.keys():
