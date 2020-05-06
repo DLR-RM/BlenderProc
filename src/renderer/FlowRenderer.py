ffae95aa30439f8266617b0a010f5daaeae719f7
Maximilian Denninger <maximilian.denninger@dlr.de>
2020-05-06 12:06:24 +0200

    fix(flowrenderer): fix backward flow correction from blender to common style

--- src/renderer/FlowRenderer.py
+++ src/renderer/FlowRenderer.py
@@ -108,7 +108,7 @@ class FlowRenderer(Renderer):
                         file_path = temporary_bwd_flow_file_path + "%04d" % frame + ".exr"
                         bwd_flow_field = load_image(file_path, num_channels=4).astype(np.float32)
 
-                        if not self.config.get_bool('y_origin_bot', False):
+                        if not self.config.get_bool('blender_image_coordinate_style', False):
                             bwd_flow_field[:, :, 1] = bwd_flow_field[:, :, 1] * -1
 
                         fname = os.path.join(self._determine_output_dir(),
