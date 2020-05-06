9ac243e41dc27364c49e5586cd631e33d6f0299f
Maximilian Denninger <maximilian.denninger@dlr.de>
2020-05-06 15:49:54 +0200

    feat(find_missing_docu): improved to include type and default value into the output, to give better feedback

--- scripts/find_missing_docu.py
+++ scripts/find_missing_docu.py
@@ -1,6 +1,4 @@
 
-
-import glob
 import os
 
 def find_all_py_files(folder_path):
@@ -13,23 +11,84 @@ def find_all_py_files(folder_path):
             res.extend(find_all_py_files(current_file))
     return res
 
-def get_config_value_from_config_line(line):
+def get_config_element_from_line(line, line_nr):
     line = line.strip()
-    if "\"" in line:
+    config_ele = line[line.find("config.get") + len("config.get"):]
+    ele_type = config_ele[1:config_ele.find("(")]
+    if not ele_type:
+        return None
+    between_parenthesis = config_ele[config_ele.find("(")+1: config_ele.find(")")]
+    default_val = None
+    if "," in between_parenthesis:
+        # has a default value
+        split_pos = between_parenthesis.find(",")
+        key_word, default_val = between_parenthesis[:split_pos], between_parenthesis[split_pos+1:]
+        default_val = default_val.strip()
+    else:
+        key_word = between_parenthesis
+    key_word = key_word.strip()
+    if "\"" in key_word:
         sep = "\""
     else:
         sep = "'"
-    line = line[line.find("config.get") + len("config.get"):]
-    line = line[line.find(sep) + 1:]
-    line = line[:line.find(sep)].strip()
+    key_word = key_word.replace(sep, "")
+    if key_word:
+        return ConfigElement(key_word, ele_type, line, line_nr, default_val)
+    else:
+        return None
 
-    return line
-
-def get_config_value_from_csv_line(line):
+def get_config_value_from_csv_line(line, line_nr):
     line = line.strip()
-    line = line[line.find("\"") + 1:]
-    line = line[:line.find("\"")].strip()
-    return line
+    key_word = line[line.find("\"") + 1:]
+    key_word = key_word[:key_word.find("\"")].strip()
+    if key_word:
+        return ConfigElement(key_word, None, line, line_nr, None)
+    else:
+        return None
+
+class ConfigElement(object):
+
+    def __init__(self, key_word, ele_type, line, line_nr, default_val):
+        self.key_word = key_word.strip()
+        self.ele_type = ele_type
+        self.default_value = default_val
+        self.line = line
+        self.line_nr = line_nr
+        self.found_usage = None
+
+    def __repr__(self):
+        if self.default_value:
+            return str("{}({}): {}".format(self.key_word, self.ele_type, self.default_value))
+        else:
+            return str("{}({})".format(self.key_word, self.ele_type))
+
+    def set_type(self, line):
+        if "Type:" in line:
+            ele_type = line[line.find("Type:") + len("Type:"):]
+            ele_type = ele_type.strip()
+            if "Default" in ele_type:
+                poses = [ele_type.find("Default"), ele_type.find(".Default"), ele_type.find(". Default"),
+                         ele_type.find(",Default"), ele_type.find(", Default")]
+            else:
+                poses = [ele_type.find("."), ele_type.find(" "), ele_type.find(", ")]
+            poses = [ele for ele in poses if ele > 0]
+            if poses:
+                end_pos = min(poses)
+                ele_type = ele_type[:end_pos]
+            if ele_type:
+                self.ele_type = ele_type
+
+    def set_default(self, line):
+        if "Default:" in line:
+            default_val = line[line.find("Default:") + len("Default:"):]
+            default_val = default_val.strip()
+            poses = [default_val.find("."), default_val.find(" "), default_val.find(", ")]
+            if poses:
+                end_pos = min(poses)
+                default_val = default_val[:end_pos]
+            if default_val:
+                self.default_value = default_val
+
 
 
 if __name__ == "__main__":
@@ -38,6 +97,8 @@ if __name__ == "__main__":
     for py_file in all_py_files:
         if "scripts" not in os.path.abspath(py_file):
             with open(py_file, "r") as file:
+                errors = []
+                base_name = os.path.basename(py_file)
                 lines = file.read().split("\n")
                 found_config_values = []
                 start_csv_table = False
@@ -53,6 +114,7 @@ if __name__ == "__main__":
                             line = line[:line.find("\"")].strip()
                             if line:
                                 found_config_values.append(line)
+                list_of_used_config_get = []
                 if found_config_values:
                     # checks if there are config values not defined at the top
                     for line_nr, line in enumerate(lines):
@@ -60,38 +122,66 @@ if __name__ == "__main__":
                         if "config.get" in line:
                             org_line = org_line[org_line.find("config.get"):]
                             org_line = org_line[:org_line.find(")")+1]
-                            line = get_config_value_from_config_line(line)
-                            if " " not in line and line != "key":
-                                if line not in found_config_values:
-                                    print("In {}: Not found at the top: {} in L:{} '{}'".format(os.path.basename(py_file), line, line_nr, org_line.strip()))
+                            config_element = get_config_element_from_line(line, line_nr)
+                            if config_element:
+                                key_word = config_element.key_word
+                                if " " not in key_word and key_word != "key":
+                                    list_of_used_config_get.append(config_element)
+                                    if key_word not in found_config_values:
+                                        errors.append("Not found at the top: '{}' " \
+                                                      "in L:{} '{}'".format(config_element.key_word,
+                                                                        line_nr, org_line.strip()))
                 start_csv_table = False
                 start_new_config_value = False
-                current_keyword = None
-                for line in lines:
+                current_element = None
+                # check if each key_word has a Type:
+                for line_nr, line in enumerate(lines):
                     if ":header:" in line:
                         continue
                     if "csv-table" in line:
                         start_csv_table = True
-                    elif start_csv_table and "__init__" in line:
-                        if current_keyword:
-                            print("In {}: This key '{}' does not have a Type".format(
-                                os.path.basename(py_file), current_keyword))
+                    elif start_csv_table and ("__init__" in line or line.strip() == '"""'):
+                        if current_element and current_element.ele_type is None:
+                            errors.append("This key '{}' does not have a Type".format(
+                                current_element.key_word))
                         break
                     if start_csv_table:
-                        keyword = get_config_value_from_csv_line(line)
-                        if keyword and " " not in keyword and keyword != "key":
-                            # found a new keyword
-                            if current_keyword:
-                                print("In {}: This key '{}' does not have a Type".format(os.path.basename(py_file), current_keyword))
-                                current_keyword = None
-                            if "Type:" not in line:
-                                current_keyword = keyword
-                        elif current_keyword:
-                            # there is no new key found, and there was an old key found
+                        config_element = get_config_value_from_csv_line(line, line_nr)
+                        if config_element and " " not in config_element.key_word and config_element.key_word != "key":
+                            found_values = [ele for ele in list_of_used_config_get if
+                                            ele.key_word == config_element.key_word]
+                            if found_values:
+                                config_element.found_usage = found_values
+                            if current_element:
+                                # found a new key_word, check the last one
+                                if current_element.ele_type is None:
+                                    if current_element.found_usage:
+                                        errors.append(
+                                            "This key '{}' does not have a Type, used type in code: {}".format(current_element.key_word, [ele.ele_type for ele in current_element.found_usage]))
+                                    else:
+                                        errors.append("This key '{}' does not have a Type".format(current_element.key_word))
+                                if current_element.default_value:
+                                    if current_element.found_usage:
+                                        for found_value in current_element.found_usage:
+                                            f_default_v = found_value.default_value
+                                            if f_default_v != current_element.default_value:
+                                                ele_type = current_element.ele_type.lower()
+                                                if ele_type == "int" or ele_type == "float":
+                                                    if f_default_v.isnumeric() and current_element.default_value.isnumeric():
+                                                        errors.append("The default value does not match the value in the " \
+                                                                      "docu for key: {} ({}!={})".format(
+                                                                                                 current_element.key_word,
+                                                                                                 current_element.default_value,
+                                                                                             f_default_v))
+                            current_element = config_element
+                        if current_element:
+                            # there is no new key found, and there was an old key there
                             if "Type:" in line:
-                                # check if the old key is ended here
-                                current_keyword = None
-                                continue
+                                current_element.set_type(line)
+                            if "Default:" in line:
+                                current_element.set_default(line)
+                if errors:
+                    print("In {}: \n{}".format(base_name, "\n".join(["   "+ele for ele in errors])))
 
 
 
