f5ffd3d7242b077074983df082ef59f7a9a19e83
Youssef Zidan <youssef.zidan@dlr.de>
2020-05-07 10:08:09 +0200

    doc(labelidmapping): added some documentation

--- src/utility/LabelIdMapping.py
+++ src/utility/LabelIdMapping.py
@@ -1,7 +1,14 @@
 import csv
 
 class LabelIdMapping(object):
+	""" Handles category id mapping for semantic segmentation maps. It's used as a static Singleton class """
 
+	"""
+	Attributes:
+	id_label_map: maps from an id to its name. E.g. id_label_map[0] = "void".
+	label_id_map: maps a class/category name to its id. E.g. label_id_map["void"] = 0.
+	num_labels: the number of classes for the currently loaded mapping. E.g. 40 for NYU.
+	"""
 	id_label_map = []
 	label_id_map = {}
 	num_labels   = 0
@@ -11,6 +18,11 @@ class LabelIdMapping(object):
 
 	@staticmethod
 	def read_csv_mapping(path):
+		""" Loads an idset mapping from a csv file, assuming the rows are sorted by their ids.
+		
+		:param path: Path to csv file
+		"""
+
 		with open(path, 'r') as csvfile:
 				reader = csv.DictReader(csvfile)
 				new_id_label_map = []
@@ -24,6 +36,12 @@ class LabelIdMapping(object):
 
 	@staticmethod
 	def assign_mapping(mapping):
+		""" Assign a mapping based on a given id-set. This mapping could be a path to a csv file holding the
+		id-set, or a tuple contaning the attributes that should be assigned to this class
+
+		:param mapping: If string then it's assumed it's a csv file holding the mapping, otherwise a tuple 
+		holding the values that should be assigned to the attributes id_label_map, label_id_map, respectively.
+		"""
 		if isinstance(mapping, str):
 			mapping = LabelIdMapping.read_csv_mapping(mapping)
 
