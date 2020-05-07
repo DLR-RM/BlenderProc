d7fb7053e1cf81f1bc6e47685168fe1fcd2ef8e8
Mohamad Elbadrawy <mohamad.elbadrawy@dlr.de>
2020-05-07 07:07:15 +0200

    doc(ReplicaLoader): better doc

--- src/loader/ReplicaLoader.py
+++ src/loader/ReplicaLoader.py
@@ -17,8 +17,8 @@ class ReplicaLoader(Loader):
 
        "data_path", "The path to the data folder, where all rooms are saved. Type: string."
        "data_set_name", "Name of the room (for example: apartment_0). Type: string."
-       "use_ambient_occlusion", "Use ambient occlusion to lighten up the scene, if the RgbRenderer is used. Type: bool."
-       "use_smooth_shading", "Enable smooth shading on all surfaces, instead of flat shading. Type: bool."
+       "use_ambient_occlusion", "Use ambient occlusion to lighten up the scene, if the RgbRenderer is used. Type: bool. Default: False."
+       "use_smooth_shading", "Enable smooth shading on all surfaces, instead of flat shading. Type: bool. Default: False."
     """
     def __init__(self, config):
         Loader.__init__(self, config)
