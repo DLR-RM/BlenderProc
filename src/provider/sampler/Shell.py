50535950b2062b7338e397e6c878bf6b8c197f17
Martin Sundermeyer <martin.sundermeyer@dlr.de>
2020-05-25 18:09:25 +0200

    started elevation uniform

--- src/provider/sampler/Shell.py
+++ src/provider/sampler/Shell.py
@@ -52,36 +52,47 @@ class Shell(Provider):
         radius_max = self.config.get_float("radius_max")
         # Elevation angles
         elevation_min = self.config.get_float("elevation_min")
-        if elevation_min == 0:
-            # here comes the magic number
-            elevation_min = 0.001
         elevation_max = self.config.get_float("elevation_max")
-        if elevation_max == 90:
-            # behold! magic number
-            elevation_max = 0.001
-        # Height of a sampling cone
-        H = 1
-        
-        # Sampling and rejection radius
-        R_sampling = H / np.tan(np.deg2rad(elevation_min))
-        R_rejection = H / np.tan(np.deg2rad(elevation_max))
-        # Init sampled point at the center of a sampling disk
-        sampled_2d = [center[0], center[1]]
-        
-        # Sampling a point from a 2-ball (disk) i.e. from the base of the right subsampling
-        # cone using Polar + Radial CDF method + rejection for 2-ball base of the rejection cone.
-        while (sampled_2d[0] - center[0])**2 + (sampled_2d[1] - center[1])**2 <= R_rejection**2:
-        # http://extremelearning.com.au/how-to-generate-uniformly-random-points-on-n-spheres-and-n-balls/
-            r = R_sampling * np.sqrt(np.random.uniform())
-            theta = np.random.uniform() * 2 * np.pi
-            sampled_2d[0] = center[0] + r * np.cos(theta)
-            sampled_2d[1] = center[1] + r * np.sin(theta)
 
-        # Sampled point in 3d
-        direction_point = np.array([center[0] + sampled_2d[0], center[1] + sampled_2d[1], center[2] + H])
-        # Getting vector, then unit vector that defines the direction
-        full_vector = direction_point - center
-        direction_vector = full_vector/np.linalg.norm(full_vector)
+        if self.config.get_bool("uniform_elevation"):
+            elevation_sampled = elevation_min + (elevation_max-elevation_min) * np.random.rand()
+            azimuth_sampled = 2 * np.pi * np.random.rand()
+            cart_point = 1
+
+        else:
+
+            if elevation_min == 0:
+                # here comes the magic number
+                elevation_min = 0.001
+            if elevation_max == 90:
+                # behold! magic number
+                elevation_max = 0.001
+
+            # Height of a sampling cone
+            H = 1
+            
+            # Sampling and rejection radius
+            R_sampling = H / np.tan(np.deg2rad(elevation_min))
+            R_rejection = H / np.tan(np.deg2rad(elevation_max))
+            # Init sampled point at the center of a sampling disk
+            sampled_2d = [center[0], center[1]]
+            
+            # Sampling a point from a 2-ball (disk) i.e. from the base of the right subsampling
+            # cone using Polar + Radial CDF method + rejection for 2-ball base of the rejection cone.
+            while (sampled_2d[0] - center[0])**2 + (sampled_2d[1] - center[1])**2 <= R_rejection**2:
+            # http://extremelearning.com.au/how-to-generate-uniformly-random-points-on-n-spheres-and-n-balls/
+                r = R_sampling * np.sqrt(np.random.uniform())
+                theta = np.random.uniform() * 2 * np.pi
+                sampled_2d[0] = center[0] + r * np.cos(theta)
+                sampled_2d[1] = center[1] + r * np.sin(theta)
+
+            # Sampled point in 3d
+            direction_point = np.array([center[0] + sampled_2d[0], center[1] + sampled_2d[1], center[2] + H])
+            # Getting vector, then unit vector that defines the direction
+            full_vector = direction_point - center
+            direction_vector = full_vector/np.linalg.norm(full_vector)
+
+
         # Calculate the factor for the unit vector
         factor = np.random.uniform(radius_min, radius_max)
         # Get the coordinates of a sampled point inside the shell
