20728e8133d21161077f9f933201e18606ed81a2
Markus Knauer <markus.knauer@dlr.de>
2020-06-08 12:55:04 +0200

    feat(Provider/ValueSampler): add normal gaussian distribution

--- src/provider/sampler/Value.py
+++ src/provider/sampler/Value.py
@@ -22,14 +22,25 @@ class Value(Provider):
           "type": "bool"
         }
 
+        Example 3: Sample a float value from a normal (Gaussian) distribution.
+
+        {
+          "provider": "sampler.Value",
+          "type": "dist",
+          "mean": 0.0,
+          "std_dev": 0.7
+        }
+
     **Configuration**:
 
     .. csv-table::
         :header: "Parameter", "Description"
 
-        "type", "The type of a value to sample. Type: string. Available: 'float', 'int', 'boolean'."
+        "type", "The type of a value to sample. Type: string. Available: 'float', 'int', 'boolean', 'dist'."
         "min", "The minimum value. Optional. Type: float/int."
         "max", "The maximum value (excluded frm the defined range of values). Type: float/int."
+        "mean", "Mean (“centre”) of the normal (Gaussian) distribution. Type: float."
+        "std_dev", "Standard deviation (spread or “width”) of the normal (Gaussian) distribution. Type: float."
     """
 
     def __init__(self, config):
@@ -54,6 +65,10 @@ class Value(Provider):
             val_min = self.config.get_float('min')
             val_max = self.config.get_float('max')
             val = np.random.uniform(val_min, val_max)
+        elif val_type.lower() == 'dist':
+            mean = self.config.get_float('mean')
+            std_dev = self.config.get_float('std_dev')
+            val = np.random.normal(loc=mean, scale=std_dev)
         else:
             raise Exception("Cannot sample this type: " + val_type)
 
