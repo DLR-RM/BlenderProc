c1ee9989402f32c8a23428aca6d4c4b9bff4b3eb
Dmitry Olefir <Dmitry.Olefir@dlr.de>
2020-06-30 21:24:27 +0200

    doc(TextureLoader): add documentation

--- src/loader/TextureLoader.py
+++ src/loader/TextureLoader.py
@@ -7,28 +7,54 @@ from src.utility.Utility import Utility
 
 
 class TextureLoader(Loader):
-    """
+    """ Loads an image, creates an image texture, and assigns the loaded image to the texture, when a path to an image
+        is provided. Loads images and for each creates a texture, and assing an image to this texture, if a path to a
+        folder with images is provided.
 
+        Example 1: Load all images in the folder and create appropriate textures.
+
+        {
+          "module": "loader.TextureLoader",
+          "config": {
+            "path": "absolute/path/to/folder/with/assets/"
+          }
+        }
+
+        Example 2: Load a random image from the path and create an appropriate texture.
+
+        {
+          "module": "loader.TextureLoader",
+          "config": {
+            "path": {
+              "provider": "sampler.Path",
+              "path": "path/to/folder/*.png"
+            }
+          }
+        }
+
+    **Configuration**:
+
+    .. csv-table::
+       :header: "Parameter", "Description"
+
+       "path", "The path to the folder with assets/to the asset. Type: string."
     """
 
     def __init__(self, config):
         Loader.__init__(self, config)
 
     def run(self):
-        """
-
-        """
+        """ Loads images and creates image textures. """
         path = Utility.resolve_path(self.config.get_string("path"))
-
         image_paths = self._resolve_paths(path)
         self._load_and_create(image_paths)
 
     @staticmethod
     def _resolve_paths(path):
-        """
+        """ Resolves absolute paths to assets in the provided folder, or to an asset if an appropriate path is provided.
 
-        :param path:
-        :return:
+        :param path: Path to folder containing assets or path to an asset. Type: string.
+        :return: List of absolute paths to assets. Type: list.
         """
         image_paths = []
         if os.path.exists(path):
@@ -45,9 +71,9 @@ class TextureLoader(Loader):
 
     @staticmethod
     def _load_and_create(image_paths):
-        """
+        """ Loads an image, creates an image texture and assigns an image to this texture per each provided path.
 
-        :return:
+        :param image_paths: List of absolute paths to assets. Type: list.
         """
         for image_path in image_paths:
             dir_path = os.path.dirname(image_path)
