e4d0bc51da789f801755b11e3503a42db6660803
Martin Sundermeyer <martin.sundermeyer@dlr.de>
2020-07-06 13:29:15 +0200

    also move resolution settings to camera to avoid conflicts

--- src/renderer/RendererInterface.py
+++ src/renderer/RendererInterface.py
@@ -20,12 +20,7 @@ class RendererInterface(Module):
 
         "output_file_prefix", "The file prefix that should be used when writing the rendering to file. Type: String."
         "output_key", "The key which should be used for storing the rendering in a merged file. Type: String"
-
-        "resolution_x", "The render image width. "
-                        "Type: int. Default: 512, except the CameraInterface loaded something else."
-        "resolution_y", "The render image height. "
-                        "Type: int. Default: 512, except the CameraInterface loaded something else."
-
+        
         "samples", "Number of samples to render for each pixel. Higher numbers take longer but remove noise in dark "
                    "areas. Type: int. Default: 256, (not true for all Renderes)."
         "use_adaptive_sampling", "Combined with the maximum sample amount, it is also possible to set the amount of "
@@ -122,13 +117,6 @@ class RendererInterface(Module):
             bpy.context.scene.render.threads_mode = "FIXED"
             bpy.context.scene.render.threads = number_of_threads
         
-        # Collect camera and camera object
-        cam_ob = bpy.context.scene.camera
-        cam = cam_ob.data
-        
-        if not 'loaded_resolution' in cam:
-            bpy.context.scene.render.resolution_x = self.config.get_int("resolution_x", 512)
-            bpy.context.scene.render.resolution_y = self.config.get_int("resolution_y", 512)
         print('Resolution: {}, {}'.format(bpy.context.scene.render.resolution_x, bpy.context.scene.render.resolution_y))
 
         bpy.context.scene.render.resolution_percentage = 100
