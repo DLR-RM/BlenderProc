da1db6f675fd9323dbf7f710cf8ac4e1c0e1770c
Martin Sundermeyer <martin.sundermeyer@dlr.de>
2020-07-09 10:42:25 +0200

    load all camera parameters from config

--- src/camera/CameraInterface.py
+++ src/camera/CameraInterface.py
@@ -23,7 +23,7 @@ class CameraInterface(Module):
               "rotation": {
                 "format": "forward_vec"
               },
-            "fov_is_half": true
+              "fov_is_half": true
             }
           }
         }
@@ -95,33 +95,33 @@ class CameraInterface(Module):
         """ Sets camera intrinsics from a source with following priority
 
            1. from config function parameter if defined
-           2. from module config (self.config) if defined
-           3. custom property cam['loaded_intrinsics'] if set in Loader
-           4. default config
+           2. from custom properties of cam if set in Loader
+           3. default config
                 resolution_x/y: 512 
                 pixel_aspect_x: 1
                 clip_start:   : 0.1
                 clip_end      : 1000
                 fov           : 0.691111
+        :param cam: The camera which contains only camera specific attributes.
         :param config: A configuration object with cam intrinsics.
         """
-        width, height = self.config.get_int("resolution_x", 512), self.config.get_int("resolution_y", 512)
-        if 'loaded_resolution' in cam and not self.config.has_param('resolution_x'):
+        width, height = config.get_int("resolution_x", 512), config.get_int("resolution_y", 512)
+        if 'loaded_resolution' in cam and not config.has_param('resolution_x'):
             width, height = cam['loaded_resolution']
         bpy.context.scene.render.resolution_x = width
         bpy.context.scene.render.resolution_y = height
         
-        # If defined, get cam_K from config else from self.config
-        cam_K = config.get_list("cam_K", self.config.get_list("cam_K", []))
+        # If defined, get cam_K from config
+        cam_K = config.get_list("cam_K", [])
         if cam_K:
             cam['loaded_intrinsics'] = cam_K        
 
         # Convert intrinsics from loader/config to Blender format
         cam.lens_unit = 'FOV'
         if 'loaded_intrinsics' in cam:
-            if self.config.has_param("fov"):
+            if config.has_param("fov"):
                 print('WARNING: FOV defined in config is ignored. Mutually exclusive with cam_K')
-            if self.config.has_param("pixel_aspect_x"):
+            if config.has_param("pixel_aspect_x"):
                 print('WARNING: pixel_aspect_x defined in config is ignored. Mutually exclusive with cam_K')
             
             cam_K = np.array(cam['loaded_intrinsics']).reshape(3, 3).astype(np.float32)
@@ -144,21 +144,21 @@ class CameraInterface(Module):
             cam.shift_y = (cy - height / 2.0) / max_resolution
         else:
             # Set FOV (Default value is the same as the default blender value)
-            cam.angle = self.config.get_float("fov", 0.691111)
+            cam.angle = config.get_float("fov", 0.691111)
 
             # Set Pixel Aspect Ratio
-            bpy.context.scene.render.pixel_aspect_x = self.config.get_float("pixel_aspect_x", 1)
+            bpy.context.scene.render.pixel_aspect_x = config.get_float("pixel_aspect_x", 1.)
             
             if bpy.context.scene.render.pixel_aspect_x != 1:
                 print('WARNING: Using non-square pixel aspect ratio. Can influence intrinsics.')
 
             # FOV is sometimes also given as the angle between forward vector and one side of the frustum
-            if self.config.get_bool("fov_is_half", False):
+            if config.get_bool("fov_is_half", False):
                 cam.angle *= 2
 
         # Clipping (Default values are the same as default blender values)
-        cam.clip_start = config.get_float("clip_start", self.config.get_float("clip_start", 0.1))
-        cam.clip_end = config.get_float("clip_end", self.config.get_float("clip_end", 1000))
+        cam.clip_start = config.get_float("clip_start", 0.1)
+        cam.clip_end = config.get_float("clip_end", 1000)
 
         # How the two cameras converge (e.g. Off-Axis where both cameras are shifted inwards to converge in the
         # convergence plane, or parallel where they do not converge and are parallel)
