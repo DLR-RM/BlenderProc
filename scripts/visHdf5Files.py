28609888156ccf845a106d4c0f3705186a6b7780
Dominik Winkelbauer <dominik.winkelbauer@dlr.de>
2020-07-15 16:58:38 +0200

    fix(scripts): Visualizes normals in rgb-like fashion

--- scripts/visHdf5Files.py
+++ scripts/visHdf5Files.py
@@ -13,11 +13,11 @@ parser = argparse.ArgumentParser("Script to visualize hdf5 files")
 
 parser.add_argument('hdf5_paths', nargs='+', help='Path to hdf5 file/s')
 parser.add_argument('--keys', nargs='+', help='Keys that should be visualized. If none is given, all keys are visualized.', default=None)
-parser.add_argument('--rgb_keys', nargs='+', help='Keys that should be interpreted as rgb data.', default=["colors"])
+parser.add_argument('--rgb_keys', nargs='+', help='Keys that should be interpreted as rgb data.', default=["colors", "normals"])
 parser.add_argument('--flow_keys', nargs='+', help='Keys that should be interpreted as optical flow data.', default=["forward_flow", "backward_flow"])
 parser.add_argument('--segmap_keys', nargs='+', help='Keys that should be interpreted as segmentation data.', default=["segmap"])
 parser.add_argument('--segcolormap_keys', nargs='+', help='Keys that point to the segmentation color maps corresponding to the configured segmap_keys.', default=["segcolormap"])
-parser.add_argument('--other_non_rgb_keys', nargs='+', help='Keys that contain additional non-RGB data which should be visualized using a jet color map.', default=["normals", "distance", "depth"])
+parser.add_argument('--other_non_rgb_keys', nargs='+', help='Keys that contain additional non-RGB data which should be visualized using a jet color map.', default=["distance", "depth"])
 
 args = parser.parse_args()
 
