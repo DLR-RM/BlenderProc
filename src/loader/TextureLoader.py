4de7c87331783fcbf43f51254d32c42560e9be93
Dmitry Olefir <Dmitry.Olefir@dlr.de>
2020-07-19 08:51:14 +0200

    feat(TextureLoader): add colorspace options

--- src/loader/TextureLoader.py
+++ src/loader/TextureLoader.py
@@ -11,7 +11,7 @@ class TextureLoader(Loader):
         is provided. Loads images and for each creates a texture, and assing an image to this texture, if a path to a
         folder with images is provided.
 
-        Example 1: Load all images in the folder and create appropriate textures.
+        Example 1: Load all images in the folder in sRGB colorspace and create appropriate textures.
 
         {
           "module": "loader.TextureLoader",
@@ -20,7 +20,7 @@ class TextureLoader(Loader):
           }
         }
 
-        Example 2: Load a random image from the path and create an appropriate texture.
+        Example 2: Load a random image from the path in Non-color colorscpace and create an appropriate texture.
 
         {
           "module": "loader.TextureLoader",
@@ -29,7 +29,8 @@ class TextureLoader(Loader):
               "provider": "sampler.Path",
               "path": "path/to/folder/*.png"
             }
-          }
+          },
+          "colorspace": "Raw"
         }
 
     **Configuration**:
@@ -38,6 +39,8 @@ class TextureLoader(Loader):
        :header: "Parameter", "Description"
 
        "path", "The path to the folder with assets/to the asset. Type: string."
+       "colorspace", "Colorspace type to assign to loaded assets. Type: string. Available: 'Filmic Log', 'Linear',
+                     'Linear ACES', 'Non-Color', 'Raw', 'sRGB', 'XYZ'. Default: 'sRGB'."
     """
 
     def __init__(self, config):
@@ -46,8 +49,9 @@ class TextureLoader(Loader):
     def run(self):
         """ Loads images and creates image textures. """
         path = Utility.resolve_path(self.config.get_string("path"))
+        colorspace = self.config.get_string("colorspace", "sRGB")
         image_paths = self._resolve_paths(path)
-        self._load_and_create(image_paths)
+        self._load_and_create(image_paths, colorspace)
 
     @staticmethod
     def _resolve_paths(path):
@@ -70,15 +74,17 @@ class TextureLoader(Loader):
         return image_paths
 
     @staticmethod
-    def _load_and_create(image_paths):
+    def _load_and_create(image_paths, colorspace):
         """ Loads an image, creates an image texture and assigns an image to this texture per each provided path.
 
         :param image_paths: List of absolute paths to assets. Type: list.
+        :param colorspace: Colorspace type of the assets. Type: string.
         """
         for image_path in image_paths:
             dir_path = os.path.dirname(image_path)
             file_name = os.path.basename(image_path)
             bpy.ops.image.open(filepath=image_path, directory=dir_path)
+            bpy.data.images[file_name].colorspace_settings.name = colorspace
             texture_name = "ct_{}".format(os.path.splitext(file_name)[0])
             tex = bpy.data.textures.new(name=texture_name, type="IMAGE")
             bpy.data.textures[texture_name].image = bpy.data.images.get(file_name)
