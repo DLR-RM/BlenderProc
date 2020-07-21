6d2e1657fb7e9f94dcaf9dd42447ab819aa4df58
Dmitry Olefir <Dmitry.Olefir@dlr.de>
2020-07-21 09:28:37 +0200

    feat(script): draft for RLE support

--- scripts/vis_coco_annotation.py
+++ scripts/vis_coco_annotation.py
@@ -1,7 +1,9 @@
-from PIL import Image, ImageFont, ImageDraw
-import json
 import argparse
+import json
 import os
+
+import numpy as np
+from PIL import Image, ImageFont, ImageDraw
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--conf', dest='conf', default='coco_annotations.json', help='coco annotation json file')
@@ -26,9 +28,10 @@ im_path = os.path.join(base_path, "rgb_{:04d}.png".format(image_idx))
 if os.path.exists(im_path):
     im = Image.open(im_path)
 else:
-    im = Image.open(im_path.replace('png','jpg'))
+    im = Image.open(im_path.replace('png', 'jpg'))
 
 draw = ImageDraw.Draw(im)
+
 
 def get_category(_id):
     category = [category["name"] for category in categories if category["id"] == _id]
@@ -37,6 +40,21 @@ def get_category(_id):
     else:
         raise Exception("Category {} is not defined in {}".format(_id, os.path.join(base_path, conf)))
 
+
+def rle2mask(rle, width, height):
+    mask = np.zeros(width * height)
+    array = np.asarray([int(x) for x in rle])
+    starts = array[0::2]
+    lengths = array[1::2]
+    current_position = 0
+    for index, start in enumerate(starts):
+        current_position += start
+        print(index)
+        mask[current_position:current_position+lengths[index]] = 1
+        current_position += lengths[index]
+    return mask.reshape(width, height)
+
+
 font = ImageFont.load_default()
 # Add bounding boxes and masks
 for idx, annotation in enumerate(annotations):
@@ -44,10 +62,14 @@ for idx, annotation in enumerate(annotations):
         bb = annotation['bbox']
         draw.rectangle(((bb[0], bb[1]), (bb[0] + bb[2], bb[1] + bb[3])), fill=None, outline="red")
         draw.text((bb[0] + 2, bb[1] + 2), get_category(annotation["category_id"]), font=font)
+        if annotation["iscrowd"]:
+            item = rle2mask(annotation["segmentation"]["counts"], annotation["segmentation"]["size"][0], annotation["segmentation"]["size"][1])
+        else:
+            item = annotation["segmentation"][0]
         poly = Image.new('RGBA', im.size)
         pdraw = ImageDraw.Draw(poly)
-        pdraw.polygon(annotation["segmentation"][0], fill=(255, 255, 255, 127), outline=(255, 255, 255, 255))
+        pdraw.polygon(item, fill=(255, 255, 255, 127), outline=(255, 255, 255, 255))
         im.paste(poly, mask=poly)
 if save:
-    im.save(os.path.join(base_path,'coco_annotated_{}.png'.format(image_idx)), "PNG")
+    im.save(os.path.join(base_path, 'coco_annotated_{}.png'.format(image_idx)), "PNG")
 im.show()
