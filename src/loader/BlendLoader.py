df3093675249be7abde979f6a2fab248b9f5af7f
Maximilian Denninger <Maximilian.Denninger@dlr.de>
2020-07-29 15:53:59 +0200

    fix(BlenderLoader): change set properties to use scene objects not selection

--- src/loader/BlendLoader.py
+++ src/loader/BlendLoader.py
@@ -61,8 +61,7 @@ class BlendLoader(LoaderInterface):
         else:
             entities = None
 
-        bpy.ops.object.select_all(action='SELECT')
-        previously_loaded_objects = set(bpy.context.selected_objects)
+        previously_loaded_objects = set(bpy.context.scene.objects)
 
         with bpy.data.libraries.load(path) as (data_from, data_to):
             # check if defined ID is supported
@@ -85,7 +84,5 @@ class BlendLoader(LoaderInterface):
                                 "\nIf your ID exists, but not supported, please append a new pair of "
                                 "{type ID(folder name): parameter name} to the 'known_datablock_names' dict. Use this "
                                 "for finding your parameter name: " + str(dir(data_from)))
-        bpy.ops.object.select_all(action='SELECT')
-        newly_loaded_objects = list(set(bpy.context.selected_objects) - previously_loaded_objects)
+        newly_loaded_objects = list(set(bpy.context.scene.objects) - previously_loaded_objects)
         self._set_properties(newly_loaded_objects)
-        bpy.ops.object.select_all(action='DESELECT')
