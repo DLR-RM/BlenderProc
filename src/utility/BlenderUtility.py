1e56eacd60bf651200fb09c9c2687d535f9649b8
Dominik Winkelbauer <dominik.winkelbauer@dlr.de>
2020-11-20 11:16:38 +0100

    chore(collision): Adds warning if one object is detected to be completely inside the other

--- src/utility/BlenderUtility.py
+++ src/utility/BlenderUtility.py
@@ -100,10 +100,12 @@ def check_intersection(obj1, obj2, skip_inside_check=False, bvh_cache=None):
     # Optionally check whether obj2 is contained in obj1
     if not inter and not skip_inside_check:
         inter = is_point_inside_object(obj1, obj1_BVHtree, obj2.matrix_world @ obj2.data.vertices[0].co)
+        print("Warning: Detected that " + obj2.name + " is completely inside " + obj1.name + ". This might be wrong, if " + obj1.name + " is not water tight or has incorrect normals. If that is the case, consider setting skip_inside_check to True.")
 
     # Optionally check whether obj1 is contained in obj2
     if not inter and not skip_inside_check:
         inter = is_point_inside_object(obj2, obj2_BVHtree, obj1.matrix_world @ obj1.data.vertices[0].co)
+        print("Warning: Detected that " + obj1.name + " is completely inside " + obj2.name + ". This might be wrong, if " + obj2.name + " is not water tight or has incorrect normals. If that is the case, consider setting skip_inside_check to True.")
 
     return inter, bvh_cache
 
@@ -135,7 +137,7 @@ def is_point_inside_object(obj, obj_BVHtree, point):
     nearest, normal, _, _ = obj_BVHtree.find_nearest(point)
     # Compute direction
     p2 = nearest - point
-    # Compute dot production between direction and normal vector
+    # Compute dot product between direction and normal vector
     a = p2.normalized().dot((obj.rotation_euler.to_matrix() @ normal).normalized())
     return a >= 0.0
 
