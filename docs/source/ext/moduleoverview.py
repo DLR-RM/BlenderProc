8167e7f517ee7d3708487d5c34ee9fd3f827a930
Dominik Winkelbauer <dominik.winkelbauer@dlr.de>
2020-12-03 16:58:54 +0100

    chore(docs): Improves documentation sidebar

--- docs/source/ext/moduleoverview.py
+++ docs/source/ext/moduleoverview.py
@@ -13,42 +13,63 @@ class ClasslistDirective(Directive):
     def run(self):
         return [classlist('')]
 
-def generate_classlist(app, fromdocname):
+def generate_collapsible_classlist(app, fromdocname, classes, container, caption, module_index):
+
+    entries = defaultdict(list)
+    for e in classes:
+        module = e[0].split(".")[module_index]
+        entries[module].append(e)
+
+    toc = nodes.bullet_list()
+    toc += nodes.caption(caption, '', *[nodes.Text(caption)])
+    for module, class_list in entries.items():
+        ref = nodes.reference('', '')
+        ref['refuri'] = app.builder.get_relative_uri(fromdocname, "src." + module)
+        ref.append(nodes.Text(module.capitalize()))
+        module_item = nodes.list_item('', addnodes.compact_paragraph('', '', ref), classes=["toctree-l1"])
+        if fromdocname.startswith("src." + module):
+            module_item["classes"].append('current')
+        toc += module_item
+
+        subtree = nodes.bullet_list()
+        module_item += subtree
+
+        for e in class_list:
+            ref = nodes.reference('', '')
+            ref['refdocname'] = e[3]
+            ref['refuri'] = app.builder.get_relative_uri(fromdocname, e[3])
+            ref['refuri'] += '#' + e[4]
+            ref.append(nodes.Text(e[0].split(".")[-1]))
+            class_item = nodes.list_item('', addnodes.compact_paragraph('', '', ref), classes=["toctree-l2"])
+            if fromdocname.startswith(e[3]):
+                class_item['classes'].append('current')
+            subtree += class_item
+
+    container += toc
+
+def generate_sidebar(app, fromdocname):
     env = app.builder.env
     container = nodes.compound(classes=['toctree-wrapper'])#addnodes.compact_paragraph('', '', classes=['toctree-wrapper'])
     py = env.get_domain('py')
     classes = [_ for _ in py.get_objects() if _[2] == 'class']
 
-
-    n = defaultdict(list)
+    classes_per_group = {"modules": ([], 1), "provider": ([], 2), "core": ([], 1)}
     for e in classes:
-        newnode = nodes.reference('', '')
-        name = e[0].split(".")[-1]
-        #print(e)
-        innernode = nodes.Text(name)
-        newnode['refdocname'] = e[3]
-        newnode['refuri'] = app.builder.get_relative_uri(
-            fromdocname, e[3])
-        newnode['refuri'] += '#' + e[4]
-        newnode.append(innernode)
+        if e[0].startswith("src.provider."):
+            group = "provider"
+        elif e[0].startswith("src.main.") or e[0].startswith("src.utility."):
+            group = "core"
+        else:
+            group = "modules"
+        classes_per_group[group][0].append(e)
 
-        para = addnodes.compact_paragraph('', '', newnode)
-        item = nodes.list_item('', para)
+    for key, items in classes_per_group.items():
+        generate_collapsible_classlist(app, fromdocname, items[0], container, key.capitalize(), items[1])
 
-        module = e[0].split(".")[1]
-        n[module].append(item)
-
-    for module in sorted(n.keys()):
-        container += nodes.caption(module, '', *[nodes.Text(module.capitalize())])
-
-        toc = nodes.bullet_list()
-        for class_obj in n[module]:
-            toc += class_obj
-        container += toc
     return container
 
 def process_classlist(app, doctree, fromdocname):
-    container = generate_classlist(app, fromdocname)
+    container = generate_sidebar(app, fromdocname)
 
     ctx = app.env.config['html_context']
     ctx['classlist'] = container
@@ -68,7 +89,7 @@ def add_classlist_handler(app):
 
 def html_page_context(app, pagename, templatename, context, doctree):
     def make_toctree(collapse=True, maxdepth=-1, includehidden=True, titles_only=False):
-        fulltoc = generate_classlist(app, "")
+        fulltoc = generate_sidebar(app, "" if "title" not in context else context["title"])
         rendered_toc = app.builder.render_partial(fulltoc)['fragment']
         return rendered_toc
 
