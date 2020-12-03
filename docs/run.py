2a9d814aabe4164edc39c8bfc8cba50c34052d20
Dominik Winkelbauer <dominik.winkelbauer@dlr.de>
2020-12-03 11:59:51 +0100

    chore(docs): Automatically find abs path to sphinx-build in run.py

--- docs/run.py
+++ docs/run.py
@@ -1,12 +1,13 @@
 # blender --background --python run.py  -- <config> [<args>]
 import sys
 import os
+from shutil import which
 
 # Add path to custom packages inside the blender main directory
 sys.path.append(os.path.join(os.path.dirname(sys.executable), "..", "..", "..", "custom-python-packages"))
 
-# this path might need to be changed for you
-sphinx_build_bin_path = "/usr/local/bin/sphinx-build"
+# Determine abs path to sphinx-build
+sphinx_build_bin_path = which('sphinx-build')
 
 # Read args
 sys.argv = [sphinx_build_bin_path] + sys.argv[sys.argv.index("--") + 1:]
