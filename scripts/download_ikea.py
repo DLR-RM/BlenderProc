2721685b34f3fa9961bd4c6d4f4ca16c66c45d58
Maximilian Denninger <Maximilian.Denninger@dlr.de>
2020-12-07 19:00:02 +0100

    feat(download_ikea): clean up the process and remove unneeded files.

--- scripts/download_ikea.py
+++ scripts/download_ikea.py
@@ -5,8 +5,9 @@ if version_info.major == 2:
 
 import os
 from urllib.request import urlretrieve, build_opener, install_opener
+import glob
 import subprocess
-
+import shutil
 
 if __name__ == "__main__":
     # setting the default header, else the server does not allow the download
@@ -30,7 +31,78 @@ if __name__ == "__main__":
 
     # unzip the zip file
     print("Unzipping the zip file...")
-    cmd = "unzip {}".format(zip_file_path)
+    cmd = "unzip {} > /dev/null".format(zip_file_path)
     subprocess.call(cmd, shell=True, cwd=os.path.dirname(zip_file_path))
 
     os.remove(zip_file_path)
+    ikea_dir = os.path.join(ikea_dir, "IKEA")
+
+    subprocess.call("chmod -R a+rw *", shell=True, cwd=ikea_dir)
+
+    print("The IKEA dataset has some weird bugs, these are fixed now.")
+    if os.path.exists(os.path.join(ikea_dir, "IKEA_bed_BEDDINGE")):
+        shutil.rmtree(os.path.join(ikea_dir, "IKEA_bed_BEDDINGE"))
+
+    nils_folder = os.path.join(ikea_dir, "IKEA_chair_NILS")
+    if os.path.exists(nils_folder):
+        print(glob.glob(os.path.join(nils_folder, "*")))
+
+    # delete all no double .obj
+    for folder in glob.glob(os.path.join(ikea_dir, "*")):
+        no_jitter_folders = glob.glob(os.path.join(folder, "nojitter*"))
+        org_obj_files = glob.glob(os.path.join(folder, ".obj"))
+        for org_obj_file in org_obj_files:
+            os.remove(org_obj_file)
+        if no_jitter_folders:
+            for no_jitter_folder in no_jitter_folders:
+                obj_files = glob.glob(os.path.join(no_jitter_folder, "*.obj"))
+                # first remove all the ones without mtl
+                for obj_file in obj_files:
+                    new_name = obj_file.replace(os.path.basename(os.path.dirname(obj_file)), "")
+                    os.rename(obj_file, new_name)
+                    os.rename(obj_file.replace(".obj", ".mtl"), new_name.replace(".obj", ".mtl"))
+                jpg_files = glob.glob(os.path.join(no_jitter_folder, "*.jpg"))
+                for jpg_file in jpg_files:
+                    new_name = jpg_file.replace(os.path.basename(os.path.dirname(jpg_file)), "")
+                    os.rename(jpg_file, new_name)
+                folders_in_no_jitter = [f for f in glob.glob(os.path.join(no_jitter_folder, "*")) if os.path.isdir(f)]
+                for mv_folder in folders_in_no_jitter:
+                    new_name = mv_folder.replace(os.path.basename(os.path.dirname(mv_folder)), "")
+                    os.rename(mv_folder, new_name)
+
+    # delete no jitter
+    for folder in glob.glob(os.path.join(ikea_dir, "*", "nojitter*")):
+        shutil.rmtree(folder)
+
+    # delete all skp files
+    skp_files = glob.glob(os.path.join(ikea_dir, "*", "*.skp"))
+    for skp_file in skp_files:
+        os.remove(skp_file)
+
+    # delete all json files
+    js_files = glob.glob(os.path.join(ikea_dir, "*", "*.js"))
+    for js_file in js_files:
+        os.remove(js_file)
+
+    # delete specific files which need to broken up:
+    def delete_obj_file(path):
+        os.remove(path)
+        if os.path.exists(path.replace(".obj", ".mtl")):
+            os.remove(path.replace(".obj", ".mtl"))
+
+    # this are several couches in one object file
+    first_path = os.path.join(ikea_dir, "IKEA_sofa_VRETA", "3b58f55ed32ceef86315023d0bef39b6_obj0_object.obj")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
