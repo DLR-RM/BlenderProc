806b1dbdcfc2240962c93506e6cd29e91e928a91
Matthias Humt <matthias.humt@dlr.de>
2021-03-18 14:04:32 +0100

    Google code style formatting

--- scripts/paste_images_on_backgrounds.py
+++ scripts/paste_images_on_backgrounds.py
@@ -10,13 +10,32 @@ from PIL import Image
 def main():
     # Get and parse all given arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--images", type=str, help="Path to object images to paste.")
-    parser.add_argument("-b", "--backgrounds", type=str, help="Path to background images to paste on.")
-    parser.add_argument("-t", "--types", default=('jpg', 'jpeg', 'png'), type=str, nargs='+',
+    parser.add_argument("-i",
+                        "--images",
+                        type=str,
+                        help="Path to object images to paste.")
+    parser.add_argument("-b",
+                        "--backgrounds",
+                        type=str,
+                        help="Path to background images to paste on.")
+    parser.add_argument("-t",
+                        "--types",
+                        default=('jpg', 'jpeg', 'png'),
+                        type=str,
+                        nargs='+',
                         help="File types to consider. Default: jp[e]g, png.")
-    parser.add_argument("-w", "--overwrite", action="store_true",
-                        help="Merges images and backgrounds, overwriting original files. Default: False.")
-    parser.add_argument("-o", "--output", default="output", type=str, help="Output directory. Default: 'output'.")
+    parser.add_argument(
+        "-w",
+        "--overwrite",
+        action="store_true",
+        help=
+        "Merges images and backgrounds, overwriting original files. Default: False."
+    )
+    parser.add_argument("-o",
+                        "--output",
+                        default="output",
+                        type=str,
+                        help="Output directory. Default: 'output'.")
     args = parser.parse_args()
 
     # Create an output directory if `overwrite` is not selected
@@ -35,19 +54,23 @@ def main():
             img_w, img_h = img.size
 
             # Selecting and opening a random image file from given `backgrounds` directory to use as background
-            background_path = random.choice(
-                [os.path.join(args.backgrounds, p) for p in os.listdir(args.backgrounds) if p.lower().endswith(args.types)])
+            background_path = random.choice([
+                os.path.join(args.backgrounds, p)
+                for p in os.listdir(args.backgrounds)
+                if p.lower().endswith(args.types)
+            ])
             background = Image.open(background_path).resize([img_w, img_h])
             # Pasting the current image on the selected background
             background.paste(img, mask=img.convert('RGBA'))
 
-            # Overwrites original image with merged one if `overwrite` is selected 
+            # Overwrites original image with merged one if `overwrite` is selected
             if args.overwrite:
                 background.save(img_path)
             # Else store merged image in default or provided `output` directory
             else:
                 if args.output == "output":
-                    background.save(os.path.join(args.images, "output", file_name))
+                    background.save(
+                        os.path.join(args.images, "output", file_name))
                 else:
                     background.save(args.output)
 
