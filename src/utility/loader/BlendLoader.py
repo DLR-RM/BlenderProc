27bf53da75498da6f5d763535fe9c65df9d49f93
wboerdijk <wout.boerdijk@tum.de>
2021-03-30 10:36:41 +0200

    chore(blenderkit): add option for a name of the merged object

--- src/utility/loader/BlendLoader.py
+++ src/utility/loader/BlendLoader.py
@@ -43,7 +43,7 @@ class BlendLoader:
         return config_value
 
     @staticmethod
-    def load(path: str, obj_types: Union[list, str] = ["mesh", "empty"], name_regrex: str = None, data_blocks: Union[list, str] = "objects", merge_objects: bool = False) -> List[MeshObject]:
+    def load(path: str, obj_types: Union[list, str] = ["mesh", "empty"], name_regrex: str = None, data_blocks: Union[list, str] = "objects", merge_objects: bool = False, merged_object_name: str = 'parent_name') -> List[MeshObject]:
         """
         Loads entities (everything that can be stored in a .blend file's folders, see Blender's documentation for
         bpy.types.ID for more info) that match a name pattern from a specified .blend file's section/datablock.
@@ -56,6 +56,8 @@ class BlendLoader:
         :param data_blocks: The datablock or a list of datablocks which should be loaded from the given .blend file.
                             Available options are: ['armatures', 'cameras', 'curves', 'hairs', 'images', 'lights', 'materials', 'meshes', 'objects', 'textures']
         :param merge_objects: Whether to merge all loaded mesh objects into a single object or not.
+        :param merged_object_name: The name of the merged object in case of merge_objects=True. If None of ['filename', 'parent_object'], uses the provided string as filename.
+                                   Available options are: ['filename', 'parent_object', '<custom_name>']
         :return: The list of loaded mesh objects.
         """
         # get a path to a .blend file
@@ -81,6 +83,9 @@ class BlendLoader:
                     print("Imported " + str(len(data_to_entities)) + " " + data_block)
                 else:
                     raise Exception("No such data block: " + data_block)
+
+        # list for potential parent names - this is required for merging objects
+        potential_parent_names = []
 
         # Go over all imported objects again
         loaded_objects = []
@@ -110,7 +115,15 @@ class BlendLoader:
 
                             # Set frame_end to the next free keyframe
                             bpy.context.scene.frame_end = max_keyframe + 1
+
+                        # in case of merging: append parent object names
+                        if merge_objects is True and obj.parent is None and merged_object_name == 'parent_name':
+                            potential_parent_names.append(obj.name)
                     else:
+                        # Before removing check if its name is relevant in case of merging
+                        if merge_objects and obj.parent is None and merged_object_name == 'parent_name':
+                            # we insert at 0 index because sometimes the parent is e.g. a curve
+                            potential_parent_names.insert(0, obj.name)
                         # Remove object again if its type is not desired
                         bpy.data.objects.remove(obj, do_unlink=True)
                 print("Selected " + str(len(loaded_objects)) + " of the loaded objects by type")
@@ -118,28 +131,39 @@ class BlendLoader:
                 loaded_objects.extend(getattr(data_to, data_block))
 
         # merge objects into one, if desired
-        if merge_objects:
-            if len(loaded_objects) > 1:
-                # create new empty object which acts as parent, and link it to the collection
+        if merge_objects and len(loaded_objects) > 1:
+            # retrieve the name of the new object
+            if merged_object_name == 'filename':
                 parent_name = path.split('/')[-1][:-6]
-                parent_obj = bpy.data.objects.new(parent_name, None)
-                col = bpy.data.collections.get('Collection')
-                col.objects.link(parent_obj)
-                bpy.ops.object.select_all(action='DESELECT')
+            elif merged_object_name == 'parent_name':
+                if len(potential_parent_names) >= 1:
+                    parent_name = potential_parent_names[0]
+                else:
+                    parent_name = path.split('/')[-1][:-6]
+                # sometimes the parent name contains a .001 (or any other int) at the end
+                if parent_name[-4] == '.' and parent_name[-3:].isdigit():
+                    parent_name = parent_name[:-4]
+            else:
+                parent_name = merged_object_name
+            # create new empty object which acts as parent, and link it to the collection
+            parent_obj = bpy.data.objects.new(parent_name, None)
+            col = bpy.data.collections.get('Collection')
+            col.objects.link(parent_obj)
+            bpy.ops.object.select_all(action='DESELECT')
 
-                # select all relevant objects
-                for loaded_object in loaded_objects:
-                    # objects with a parent will be skipped, as this relationship will otherwise be broken
-                    # if a parent exists this object should be grandchild of parent_obj (or grandgrand...)
-                    if loaded_object.parent is not None:
-                        continue
-                    # if the object doesn't have a parent we can set its parent
-                    loaded_object.parent = parent_obj
+            # select all relevant objects
+            for loaded_object in loaded_objects:
+                # objects with a parent will be skipped, as this relationship will otherwise be broken
+                # if a parent exists this object should be grandchild of parent_obj (or grandgrand...)
+                if loaded_object.parent is not None:
+                    continue
+                # if the object doesn't have a parent we can set its parent
+                loaded_object.parent = parent_obj
 
         # As some loaded objects were deleted again due to their type, we need also to remove the dependent datablocks that were also loaded and are now orphans
         BlendLoader._purge_added_orphans(orphans_before, data_to)
 
-        if merge_objects:
+        if merge_objects and len(loaded_objects) > 1:
             return [bpy.data.objects[parent_name]]
 
         return loaded_objects
