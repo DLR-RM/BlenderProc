2b1ebaa08af3438c0c736993db2d8f0adf464d2b
Dominik Winkelbauer <dominik.winkelbauer@dlr.de>
2021-04-22 10:37:57 +0200

    chore(loader): Removes support for blender objects from LoaderInterface.set_properties

--- src/loader/LoaderInterface.py
+++ src/loader/LoaderInterface.py
@@ -49,7 +49,7 @@ class LoaderInterface(Module):
     def __init__(self, config):
         Module.__init__(self, config)
 
-    def _set_properties(self, objects: Union[bpy.types.Object, Entity]):
+    def _set_properties(self, objects: [Entity]):
         """ Sets all custom properties of all given objects according to the configuration.
 
         Also runs all custom property functions.
@@ -70,16 +70,13 @@ class LoaderInterface(Module):
             for key, value in properties.items():
                 if key.startswith("cp_"):
                     key = key[3:]
-                    if isinstance(obj, Entity):
-                        obj.set_cp(key, value)
-                    else:
-                        obj[key] = value
+                    obj.set_cp(key, value)
                 else:
                     raise RuntimeError(
                         "Loader modules support setting only custom properties. Use 'cp_' prefix for keys. "
                         "Use manipulators.Entity for setting object's attribute values.")
-            if material_properties and hasattr(obj, "material_slots"):
-                for material in (obj.get_materials() if isinstance(obj, MeshObject) else obj.data.materials):
+            if material_properties and isinstance(obj, MeshObject):
+                for material in obj.get_materials():
                     if material is None:
                         continue
                     for key, value in material_properties.items():
@@ -91,62 +88,11 @@ class LoaderInterface(Module):
                                                "Use 'cp_' prefix for keys.")
 
             # only meshes have polygons/faces
-            if hasattr(obj, 'type') and obj.type == 'MESH':
+            if isinstance(obj, MeshObject):
                 if self.config.has_param("cf_set_shading"):
                     mode = self.config.get_string("cf_set_shading")
                     angle_value = self.config.get_float("cf_shading_auto_smooth_angle_in_deg", 30)
-                    LoaderInterface.change_shading_mode([obj], mode, angle_value)
+                    obj.set_shading_mode(mode, angle_value)
 
-        apply_transformation = self.config.get_bool("cf_apply_transformation", False)
-        if apply_transformation:
-            LoaderInterface.apply_transformation_to_objects(objects)
-
-    @staticmethod
-    def apply_transformation_to_objects(objects: [bpy.types.Object]):
-        """
-        Apply the current transformation of the object, which are saved in the location, scale or rotation attributes
-        to the mesh and sets them to their init values.
-
-        :param objects: List of objects, which should be changed
-        """
-        bpy.ops.object.select_all(action='DESELECT')
-        for obj in objects:
-            # only bpy.types.Object (subclass of bpy.types.ID) have transformation
-            if isinstance(obj, bpy.types.Object):
-                obj.select_set(True)
-                bpy.context.view_layer.objects.active = obj
-                bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
-                obj.select_set(False)
-        bpy.ops.object.select_all(action='DESELECT')
-
-    @staticmethod
-    def change_shading_mode(objects: [bpy.types.Object], mode: str, angle_value: float):
-        """
-        Changes the shading mode of all objects to either flat or smooth. All surfaces of that object are changed.
-
-        :param objects: A list of objects which should receive the custom properties. Type: [bpy.types.Object]
-        :param mode: Desired mode of the shading. Available: ["FLAT", "SMOOTH", "AUTO"]. Type: str
-        :param angle_value: Angle in degrees at which flat shading is activated in `AUTO` mode. Type: float
-        """
-        if mode.lower() == "flat":
-            is_smooth = False
-            for obj in objects:
-                obj.data.use_auto_smooth = False
-        elif mode.lower() == "smooth":
-            is_smooth = True
-            for obj in objects:
-                obj.data.use_auto_smooth = False
-        elif mode.lower() == "auto":
-            is_smooth = True
-            for obj in objects:
-                obj.data.use_auto_smooth = True
-                obj.data.auto_smooth_angle = np.deg2rad(angle_value)
-        else:
-            raise Exception("This shading mode is unknown: {}".format(mode))
-
-        for obj in objects:
-            if isinstance(obj, MeshObject):
-                obj.set_shading_mode(is_smooth)
-            else:
-                for face in obj.data.polygons:
-                    face.use_smooth = is_smooth
+                if self.config.get_bool("cf_apply_transformation", False):
+                    obj.persist_transformation_into_mesh()
