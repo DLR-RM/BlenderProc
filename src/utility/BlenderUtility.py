6315614debd457d25c363dfd733d7d1a5122b73c
Maximilian Denninger <Maximilian.Denninger@dlr.de>
2021-05-10 16:23:57 +0200

    doc(BlenderUtility): add type hints

--- src/utility/BlenderUtility.py
+++ src/utility/BlenderUtility.py
@@ -322,7 +322,7 @@ def get_all_textures():
     return list(bpy.data.textures)
 
 
-def load_image(file_path, num_channels=3):
+def load_image(file_path: str, num_channels: int = 3) -> np.ndarray:
     """ Load the image at the given path returns its pixels as a numpy array.
 
     The alpha channel is neglected.
@@ -452,7 +452,7 @@ def get_node_attributes(node: bpy.types.Node) -> list:
 
     attributes = []
     for attr in node.bl_rna.properties:
-        #check if the attribute should be copied and add it to the list of attributes to copy
+        # check if the attribute should be copied and add it to the list of attributes to copy
         if not attr.identifier in ignore_attributes and not attr.identifier.split("_")[0] == "bl":
             attributes.append(attr.identifier)
 
