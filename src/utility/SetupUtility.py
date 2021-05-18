4ca9ec03f3025a1967d795c628dbea410f7fe941
Maximilian Denninger <Maximilian.Denninger@dlr.de>
2021-05-18 16:55:26 +0200

    fix(SetupUtility): fix bug in pip install on mac os, blender 2.92 changed the python path

--- src/utility/SetupUtility.py
+++ src/utility/SetupUtility.py
@@ -91,10 +91,10 @@ class SetupUtility:
             packages_path = os.path.abspath(os.path.join(blender_path, "custom-python-packages"))
             pre_python_package_path = os.path.join(blender_path, major_version, "python", "lib", "python3.7", "site-packages")
         elif platform == "darwin":
-            python_bin_folder = os.path.join(blender_path, "Contents", "Resources", major_version, "python", "bin")
+            python_bin_folder = os.path.join(blender_path, major_version, "python", "bin")
             python_bin = os.path.join(python_bin_folder, "python3.7m")
-            packages_path = os.path.abspath(os.path.join(blender_path, "Contents", "Resources", "custom-python-packages"))
-            pre_python_package_path = os.path.join(blender_path, "Contents", "Resources", major_version, "python", "lib", "python3.7", "site-packages")
+            packages_path = os.path.abspath(os.path.join(blender_path, "custom-python-packages"))
+            pre_python_package_path = os.path.join(blender_path, major_version, "python", "lib", "python3.7", "site-packages")
         elif platform == "win32":
             python_bin_folder = os.path.join(blender_path, major_version, "python", "bin")
             python_bin = os.path.join(python_bin_folder, "python")
