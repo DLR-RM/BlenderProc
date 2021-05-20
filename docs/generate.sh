f1ac07473c1d19c04b61b1e660be168b612d3cb8
Moiz Sajid <Moiz.Sajid@dlr.de>
2021-05-20 13:13:32 +0200

    fix(docs): Moved images and updated all the image paths

--- docs/generate.sh
+++ docs/generate.sh
@@ -3,8 +3,7 @@
 sphinx-apidoc -e -P -f -o source/ ../src/ ../src/run.py ../src/debug.py ../src/debug_startup.py
 make html
 
-cp source/readme.jpg build/html
-cp source/BlenderProcVideoImg.jpg build/html
 cd source
+cp images/* ../build/html/ --parents
 cp examples/**/*.jpg ../build/html/ --parents
 cp examples/**/*.png ../build/html/ --parents
