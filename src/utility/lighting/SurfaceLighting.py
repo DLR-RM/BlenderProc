8253d57ee4965ae490d95b9399afef016f218d03
Dominik Winkelbauer <dominik.winkelbauer@dlr.de>
2021-05-21 10:45:29 +0200

    chore(surfacelight): Moves SurfaceLighting to API

--- src/utility/lighting/SurfaceLighting.py
+++ src/utility/lighting/SurfaceLighting.py
@@ -1,64 +1,15 @@
-import bpy
-import mathutils
-
-from src.main.Module import Module
-from src.utility.Config import Config
 from src.utility.MeshObjectUtility import MeshObject
-from src.utility.Utility import Utility
 
 
-class SurfaceLighting(Module):
-    """
-    Adds lighting to the scene, by adding emission shader nodes to surfaces of specified objects.
-    The speciality here is that the material can still look like before and now also emit light, this can be done
-    by setting `keep_using_base_color` to `True`. If the material should not be kept this can be set to `False` and
-    with the key `emission_color` a new color can be set, if none is given it will assume `[1, 1, 1]`, which is white.
+class SurfaceLighting:
+    @staticmethod
+    def run(objects: [MeshObject], emission_strength: float = 10.0, keep_using_base_color: bool = False, emission_color: list = None):
+        """ Add emission shader to the materials of the given objects.
 
-    **Configuration**:
-
-    .. list-table:: 
-        :widths: 25 100 10
-        :header-rows: 1
-
-        * - Parameter
-          - Description
-          - Type
-        * - selection
-          - Selection of objects, via the `getter.Entity`.
-          - Provider
-        * - emission_strength
-          - The strength of the emission shader. Default: 10.0.
-          - float
-        * - keep_using_base_color
-          - If this is True, the Base Color of the material is kept, this means if the material was yellow before. \
-            The light now is also yellow. This is also true, if a texture was used before. Default: False.
-          - bool
-        * - emission_color
-          - If `keep_using_case_color` is False it is possible to set the color of the light with an RGB value. All \
-            values have to be in the range from [0, 1]. Default: None.
-          - mathutils.Vector
-    """
-
-    def __init__(self, config: Config):
-        Module.__init__(self, config)
-        self.emission_strength = self.config.get_float("emission_strength", 10.0)
-        self.keep_using_base_color = self.config.get_bool("keep_using_base_color", False)
-        self.emission_color = self.config.get_vector3d("emission_color", None)
-
-    def run(self):
-        """
-        Run this current module.
-        """
-        # get all objects which material should be changed
-        objects = MeshObject.convert_to_meshes(self.config.get_list("selector"))
-
-        self.add_emission_to_materials(objects)
-
-    def add_emission_to_materials(self, objects):
-        """
-        Add emission shader to the materials of the objects which are selected via the `selector`
-
-        :param objects: to change the materials of
+        :param objects: A list of mesh objects whose materials should emit light.
+        :param emission_strength: The strength of the emitted light.
+        :param keep_using_base_color: If True, the base color of the material will be used as emission color.
+        :param emission_color: The color of the light to emit. Is ignored if keep_using_base_color is set to True.
         """
         empty_material = None
 
@@ -85,7 +36,7 @@ class SurfaceLighting(Module):
                 # add a custom property to later identify these materials
                 material.set_cp("is_lamp", True)
 
-                material.make_emissive(emission_strength=self.emission_strength, emission_color=self.emission_color, keep_using_base_color=self.keep_using_base_color)
+                material.make_emissive(emission_strength=emission_strength, emission_color=emission_color, keep_using_base_color=keep_using_base_color)
 
 
 
