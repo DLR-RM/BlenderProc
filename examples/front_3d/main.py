55a785ef1974ab148283d4699d01dde0476fcaf3
Maximilian Denninger <Maximilian.Denninger@dlr.de>
2021-06-17 08:51:16 +0200

    chore(front_3D_main): adapt the example to more closely match the config example

--- examples/front_3d/main.py
+++ examples/front_3d/main.py
@@ -1,4 +1,5 @@
 from src.utility.SetupUtility import SetupUtility
+
 SetupUtility.setup([])
 
 import argparse
@@ -7,7 +8,6 @@ import os
 import bpy
 import numpy as np
 from mathutils import Matrix, Euler
-
 
 from src.utility.Initializer import Initializer
 from src.utility.LabelIdMapping import LabelIdMapping
@@ -22,8 +22,6 @@ from src.utility.Utility import Utility
 
 from src.utility.RendererUtility import RendererUtility
 from src.utility.SegMapRendererUtility import SegMapRendererUtility
-
-
 
 parser = argparse.ArgumentParser()
 parser.add_argument("front", help="Path to the 3D front file")
@@ -45,13 +43,13 @@ RendererUtility.set_light_bounces(diffuse_bounces=200, glossy_bounces=200, ao_bo
 
 # load the front 3D objects
 loaded_objects = Front3DLoader.load(
-            json_path=args.front,
-            future_model_path=args.future_folder,
-            front_3D_texture_path=args.front_3D_texture_path,
-            mapping=mapping,
-            ceiling_light_strength=0.8,
-            lamp_light_strength=0.8
-        )
+    json_path=args.front,
+    future_model_path=args.future_folder,
+    front_3D_texture_path=args.front_3D_texture_path,
+    mapping=mapping,
+    ceiling_light_strength=0.8,
+    lamp_light_strength=0.8
+)
 
 # Init sampler for sampling locations inside the loaded front3D house
 point_sampler = Front3DPointInRoomSampler(loaded_objects)
@@ -61,22 +59,38 @@ bvh_tree = MeshObject.create_bvh_tree_multi_objects([o for o in loaded_objects i
 
 poses = 0
 tries = 0
-while tries < 10000 and poses < 5:
+
+
+def check_name(name):
+    for category_name in ["chair", "sofa", "table", "bed"]:
+        if category_name in name.lower():
+            return True
+    return False
+
+
+# filter some objects from the loaded objects, which are later used in calculating an interesting score
+special_objects = [obj.get_cp("category_id") for obj in loaded_objects if check_name(obj.get_name())]
+
+proximity_checks = {"min": 1.0, "avg": {"min": 2.5, "max": 3.5}, "no_background": True}
+while tries < 10000 and poses < 10:
     # Sample point inside house
-    height = np.random.uniform(0.5, 2)
+    height = np.random.uniform(1.4, 1.8)
     location = point_sampler.sample(height)
     # Sample rotation (fix around X and Y axis)
-    rotation = np.random.uniform([1.2217, 0, 0], [1.2217, 0, 6.283185307])
+    rotation = np.random.uniform([1.2217, 0, 0], [1.338, 0, 6.283185307])
     cam2world_matrix = MathUtility.build_transformation_mat(location, Euler(rotation).to_matrix())
 
-    # Check that obstacles are at least 1 meter away from the camera and make sure the view interesting enough
-    if CameraValidation.perform_obstacle_in_view_check(cam2world_matrix, {"min": 1.0}, bvh_tree) and CameraValidation.scene_coverage_score(cam2world_matrix) > 0.4:
+    # Check that obstacles are at least 1 meter away from the camera and have an average distance between 2.5 and 3.5
+    # meters and make sure that no background is visible, finally make sure the view is interesting enough
+    if CameraValidation.scene_coverage_score(cam2world_matrix, special_objects, special_objects_weight=10.0) > 0.8 \
+            and CameraValidation.perform_obstacle_in_view_check(cam2world_matrix, proximity_checks, bvh_tree):
         CameraUtility.add_camera_pose(cam2world_matrix)
         poses += 1
     tries += 1
 
 # set the category_id of the world to 0
 bpy.context.scene.world["category_id"] = 0
+RendererUtility.set_samples(350)
 
 # render the whole pipeline
 data = RendererUtility.render()
@@ -84,4 +98,3 @@ data.update(SegMapRendererUtility.render(Utility.get_temporary_directory(), Util
 
 # write the data to a .hdf5 container
 WriterUtility.save_to_hdf5(args.output_dir, data)
-
