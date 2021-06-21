fe540650897b255d3b4808bca8a0e506333f7fd3
Winkelbauer, Dominik <Dominik.Winkelbauer@dlr.de>
2021-06-21 17:04:24 +0200

    Update examples/coco_annotations/main.py
    Co-authored-by: Sundermeyer, Martin <Martin.Sundermeyer@dlr.de>

--- examples/coco_annotations/main.py
+++ examples/coco_annotations/main.py
@@ -16,7 +16,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('camera', nargs='?', default="examples/coco_annotations/camera_positions", help="Path to the camera file")
-parser.add_argument('scene', nargs='?', default="examples/coco_annotations/scene.blend", help="Path to the scene.obj file")
+parser.add_argument('scene', nargs='?', default="examples/coco_annotations/scene.blend", help="Path to the scene.blend file")
 parser.add_argument('output_dir', nargs='?', default="examples/coco_annotations/output", help="Path to where the final files will be saved ")
 args = parser.parse_args()
 
