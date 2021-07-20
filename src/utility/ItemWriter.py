a5a491d9b1d68586800a5536338b40eeb207b19d
Dominik Winkelbauer <dominik.winkelbauer@dlr.de>
2021-07-20 14:43:35 +0200

    feat(writer): Adds possibility back to change local frame of matrix_world matrices

--- src/utility/ItemWriter.py
+++ src/utility/ItemWriter.py
@@ -11,26 +11,28 @@ class ItemWriter:
     def __init__(self, get_item_attribute_func):
         self.get_item_attribute_func = get_item_attribute_func
 
-    def write_items_to_file(self, path_prefix, items, attributes, destination_frame):
+    def write_items_to_file(self, path_prefix, items, attributes, local_frame_change, world_frame_change):
         """ Writes the state of the given items to one numpy file per frame.
 
         :param path_prefix: The prefix path to write the files to.
         :param items: A list of items.
         :param attributes: A list of attributes to write per item.
-        :param destination_frame: Used to transform points to the blender coordinate frame.
+        :param local_frame_change: Can be used to change the local coordinate frame of matrices. Default: ["X", "Y", "Z"]
+        :param world_frame_change: Can be used to change the world coordinate frame of points and matrices. Default: ["X", "Y", "Z"]
         """
         for frame in range(bpy.context.scene.frame_start, bpy.context.scene.frame_end):
             bpy.context.scene.frame_set(frame)
-            self._write_items_to_file_for_current_frame(path_prefix, items, attributes, frame, destination_frame)
+            self._write_items_to_file_for_current_frame(path_prefix, items, attributes, frame, local_frame_change, world_frame_change)
 
-    def _write_items_to_file_for_current_frame(self, path_prefix, items, attributes, frame, destination_frame):
+    def _write_items_to_file_for_current_frame(self, path_prefix, items, attributes, frame, local_frame_change, world_frame_change):
         """ Writes the state of the given items to one numpy file for the given frame.
 
         :param path_prefix: The prefix path to write the files to.
         :param items: A list of items.
         :param attributes: A list of attributes to write per item.
         :param frame: The frame number.
-        :param destination_frame: Used to transform points to the blender coordinate frame.
+        :param local_frame_change: Can be used to change the local coordinate frame of matrices. Default: ["X", "Y", "Z"]
+        :param world_frame_change: Can be used to change the world coordinate frame of points and matrices. Default: ["X", "Y", "Z"]
         """
         value_list = []
         # Go over all items
@@ -39,16 +41,13 @@ class ItemWriter:
             # Go through all attributes
             for attribute in attributes:
                 # Get the attribute value
-                value = self.get_item_attribute_func(item, attribute, destination_frame)
+                value = self.get_item_attribute_func(item, attribute, local_frame_change, world_frame_change)
 
                 # If its a list of numbers, just add to the array, else just add one value
                 if isinstance(value, (Vector, Euler, Color, Quaternion)):
                     value = list(value)  
                 elif isinstance(value, (Matrix, np.ndarray)):
                     value = np.array(value).tolist()
-
-                if isinstance(value, list) and len(value) == 3:
-                    MathUtility.change_coordinate_frame_of_point(value, destination_frame)
 
                 value_list_per_item[attribute] = value
 
