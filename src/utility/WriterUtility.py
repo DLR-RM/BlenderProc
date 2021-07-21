8f00ef03e8ac0637f20614d307da472b196a9f7f
Winkelbauer, Dominik <Dominik.Winkelbauer@dlr.de>
2021-07-21 14:37:19 +0200

    Update src/utility/WriterUtility.py
    Co-authored-by: Sundermeyer, Martin <Martin.Sundermeyer@dlr.de>

--- src/utility/WriterUtility.py
+++ src/utility/WriterUtility.py
@@ -120,8 +120,8 @@ class WriterUtility:
 
         :param item: The item. Type: blender object.
         :param attribute_name: The attribute name. Type: string.
-        :param local_frame_change: Used to transform item to blender coordinates. Default: ["X", "Y", "Z"]
-        :param world_frame_change: Used to transform item to blender coordinates. Default: ["X", "Y", "Z"]
+     :param local_frame_change: Can be used to change the local coordinate frame of matrices. Default: ["X", "Y", "Z"]
+     :param world_frame_change: Can be used to change the world coordinate frame of points and matrices. Default: ["X", "Y", "Z"]
         :return: The attribute value.
         """
 
