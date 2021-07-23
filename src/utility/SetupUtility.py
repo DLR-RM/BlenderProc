a5eef5a1ca8bba497140d72f8b0b5f9be223f582
Maximilian Denninger <Maximilian.Denninger@dlr.de>
2021-07-23 15:39:06 +0200

    fix(SetupUtility): remove python3 syntax

--- src/utility/SetupUtility.py
+++ src/utility/SetupUtility.py
@@ -6,8 +6,6 @@ import importlib
 from io import BytesIO
 import zipfile
 
-from requests.models import Response
-
 class SetupUtility:
 
     # Remember already installed packages, so we do not have to call pip freeze multiple times
@@ -198,7 +196,7 @@ class SetupUtility:
             SetupUtility.installed_packages = dict(zip(installed_packages_name, installed_packages_versions))
 
     @staticmethod
-    def extract_file(output_dir: str, file: bytes = None):
+    def extract_file(output_dir, file):
         """ Extract all members from the archive to output_dir
 
         :param output_dir: the dir to zip file extract to
@@ -212,7 +210,7 @@ class SetupUtility:
             raise e
 
     @staticmethod
-    def extract_from_response(output_dir: str, response: Response = None):
+    def extract_from_response(output_dir, response):
         """ Extract all members from the archive to output_dir
 
         :param output_dir: the dir to zip file extract to
