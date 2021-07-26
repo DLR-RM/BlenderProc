e2d2463b8c9498f61c9f6b93981ffa2eb1ccd2a7
Dominik Winkelbauer <dominik.winkelbauer@dlr.de>
2021-07-26 14:52:44 +0200

    chore(materialmanip): Moves leftover functionality of material manipulator to utility class

---
+++ examples/advanced/dust/main.py
@@ -0,0 +1,62 @@
+from src.utility.SetupUtility import SetupUtility
+SetupUtility.setup([])
+
+from src.utility.loader.BlendLoader import BlendLoader
+from src.utility.loader.HavenEnvironmentLoader import HavenEnvironmentLoader
+from src.utility.materials.Dust import Dust
+from src.utility.sampler.PartSphere import PartSphere
+
+from src.utility.MathUtility import MathUtility
+from src.utility.CameraUtility import CameraUtility
+from src.utility.WriterUtility import WriterUtility
+from src.utility.Initializer import Initializer
+from src.utility.LightUtility import Light
+
+from src.utility.RendererUtility import RendererUtility
+
+import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('model', nargs='?', default="resources/haven/models/ArmChair_01/ArmChair_01_2k.blend", help="ath to the blend file, from the haven dataset, browse the model folder, for all possible options")
+parser.add_argument('hdri_path', nargs='?', default="resources/haven", help="The folder where the `hdri` folder can be found, to load an world environment")
+parser.add_argument('output_dir', nargs='?', default="examples/datasets/haven/output", help="Path to where the final files, will be saved")
+args = parser.parse_args()
+
+Initializer.init()
+
+# load the objects into the scene
+obj = BlendLoader.load(args.model)[0]
+
+HavenEnvironmentLoader.set_random_world_background_hdr_img(args.hdri_path)
+
+# define a light and set its location and energy level
+light = Light()
+light.set_type("POINT")
+light.set_location([5, -5, 5])
+light.set_energy(1000)
+
+# Sample five camera poses
+for i in range(5):
+    # Sample random camera location above objects
+    location = PartSphere.sample(center=np.array([0, 0, 0]), mode="SURFACE", radius=3, part_sphere_dir_vector=np.array([1, 0, 0]), dist_above_center=0.5)
+    # Compute rotation based on vector going from location towards poi
+    rotation_matrix = CameraUtility.rotation_from_forward_vec(obj.get_location() - location)
+    # Add homog cam pose based on location an rotation
+    cam2world_matrix = MathUtility.build_transformation_mat(location, rotation_matrix)
+    CameraUtility.add_camera_pose(cam2world_matrix)
+
+for material in obj.get_materials():
+    Dust.add_to_material(material, strength=0.8, texture_scale=0.05)
+
+# activate normal and distance rendering
+RendererUtility.enable_normals_output()
+RendererUtility.enable_distance_output()
+# set the amount of samples, which should be used for the color rendering
+RendererUtility.set_samples(350)
+
+# render the whole pipeline
+data = RendererUtility.render()
+
+# write the data to a .hdf5 container
+WriterUtility.save_to_hdf5(args.output_dir, data)
--- src/manipulators/MaterialManipulator.py
+++ src/manipulators/MaterialManipulator.py
@@ -11,6 +11,7 @@ from src.utility import BlenderUtility
 from src.utility.Config import Config
 from src.utility.MaterialUtility import Material
 from src.utility.Utility import Utility
+from src.utility.materials.Dust import Dust
 
 
 class MaterialManipulator(Module):
@@ -532,19 +533,7 @@ class MaterialManipulator(Module):
         :param material: Material to be modified.
         :param multiply_factor: Multiplication factor of the displacement.
         """
-        output = material.get_the_one_node_with_type("OutputMaterial")
-        texture = material.get_nodes_with_type("ShaderNodeTexImage")
-        if texture is not None:
-            if len(texture) == 1:
-                texture = texture[0]
-                math_node = material.new_node('ShaderNodeMath')
-                math_node.operation = "MULTIPLY"
-                math_node.inputs[1].default_value = multiply_factor
-                material.link(texture.outputs["Color"], math_node.inputs[0])
-                material.link(math_node.outputs["Value"], output.inputs["Displacement"])
-            else:
-                raise Exception("The amount of output and texture nodes of the material '{}' is not supported by "
-                                "this custom function.".format(material.get_name()))
+        material.set_displacement_from_principled_shader_value("Base Color", multiply_factor)
 
     @staticmethod
     def _map_vertex_color(material: Material, layer_name: str):
@@ -573,67 +562,29 @@ class MaterialManipulator(Module):
         :param material: Material, which will be changed
         :param config: containing the config information
         """
-        used_mode = config.get_string("mode", "overlay").lower()
-        if used_mode not in ["overlay", "mix", "set"]:
-            raise Exception(f'This mode is unknown here: {used_mode}, only ["overlay", "mix", "set"]!')
-
+        used_mode = config.get_string("mode", "overlay")
         used_textures = config.get_list("used_texture")
-        if len(used_textures) == 0:
-            raise Exception(f"You have to select a texture, which is {used_mode} over the material!")
-
         invert_texture = config.get_bool("invert_texture", False)
-
-        used_texture = random.choice(used_textures)
-        used_connector = config.get_string("connection", "Base Color").title()
+        used_connector = config.get_string("connection", "Base Color")
         texture_scale = config.get_float("texture_scale", 0.05)
+        strength = config.get_float("strength", 0.5)
 
         if config.has_param("strength") and used_mode == "set":
             raise Exception("The strength can only be used if the mode is not \"set\"!")
-        strength = config.get_float("strength", 0.5)
 
-        principled_bsdfs = material.get_nodes_with_type("BsdfPrincipled")
-        if len(principled_bsdfs) != 1:
-            raise Exception("This only works with materials, which have exactly one Prinicpled BSDF, "
-                            "use a different selector!")
-        principled_bsdf = principled_bsdfs[0]
-        if used_connector not in principled_bsdf.inputs:
-            raise Exception(f"The {used_connector} not an input to Principled BSDF!")
+        if len(used_textures) == 0:
+            raise Exception(f"You have to select a texture, which is {used_mode} over the material!")
+        texture = random.choice(used_textures)
 
-        node_connected_to_the_connector = None
-        for link in material.links:
-            if link.to_socket == principled_bsdf.inputs[used_connector]:
-                node_connected_to_the_connector = link.from_node
-                # remove this connection
-                material.links.remove(link)
-        if node_connected_to_the_connector is not None or used_mode == "set":
-            texture_node = material.new_node("ShaderNodeTexImage")
-            texture_node.image = used_texture.image
-            # add texture coords to make the scaling of the dust texture possible
-            texture_coords = material.new_node("ShaderNodeTexCoord")
-            mapping_node = material.new_node("ShaderNodeMapping")
-            mapping_node.vector_type = "TEXTURE"
-            mapping_node.inputs["Scale"].default_value = [texture_scale] * 3
-            material.link(texture_coords.outputs["UV"], mapping_node.inputs["Vector"])
-            material.link(mapping_node.outputs["Vector"], texture_node.inputs["Vector"])
-            texture_node_output = texture_node.outputs["Color"]
-            if invert_texture:
-                invert_node = material.new_node("ShaderNodeInvert")
-                invert_node.inputs["Fac"].default_value = 1.0
-                material.link(texture_node_output, invert_node.inputs["Color"])
-                texture_node_output = invert_node.outputs["Color"]
-            if node_connected_to_the_connector is not None and used_mode != "set":
-                mix_node = material.new_node("ShaderNodeMixRGB")
-                if used_mode in "mix_node":
-                    mix_node.blend_type = "OVERLAY"
-                elif used_mode in "mix":
-                    mix_node.blend_type = "MIX"
-                mix_node.inputs["Fac"].default_value = strength
-                material.link(texture_node_output, mix_node.inputs["Color2"])
-                # hopefully 0 is the color node!
-                material.link(node_connected_to_the_connector.outputs[0], mix_node.inputs["Color1"])
-                material.link(mix_node.outputs["Color"], principled_bsdf.inputs[used_connector])
-            elif used_mode == "set":
-                material.link(texture_node_output, principled_bsdf.inputs[used_connector])
+        material.infuse_texture(
+            texture=texture,
+            mode=used_mode,
+            invert_texture=invert_texture,
+            connection=used_connector,
+            texture_scale=texture_scale,
+            strength=strength
+        )
+
 
     @staticmethod
     def _infuse_material(material: Material, config: Config):
@@ -646,9 +597,7 @@ class MaterialManipulator(Module):
         :param config: Used config
         """
         # determine the mode
-        used_mode = config.get_string("mode", "mix").lower()
-        if used_mode not in ["add", "mix"]:
-            raise Exception(f'This mode is unknown here: {used_mode}, only ["mix", "add"]!')
+        used_mode = config.get_string("mode", "mix")
 
         mix_strength = 0.0
         if used_mode == "mix":
@@ -658,52 +607,13 @@ class MaterialManipulator(Module):
 
         # get the material, which will be used to infuse the given material
         used_materials = config.get_list("used_material")
-        if len(used_materials) == 0:
-            raise Exception(f"You have to select a material, which is {used_mode}ed over the material!")
-
         used_material = random.choice(used_materials)
 
-        # move the copied material inside of a group
-        group_node = material.new_node("ShaderNodeGroup")
-        group = BlenderUtility.add_nodes_to_group(used_material.node_tree.nodes,
-                                                  f"{used_mode.title()}_{used_material.name}")
-        group_node.node_tree = group
-        # get the current material output and put the used material in between the last node and the material output
-        material_output = material.get_the_one_node_with_type("OutputMaterial")
-        for mat_output_input in material_output.inputs:
-            if len(mat_output_input.links) > 0:
-                if "Float" in mat_output_input.bl_idname or "Vector" in mat_output_input.bl_idname:
-                    # For displacement
-                    infuse_node = material.new_node("ShaderNodeMixRGB")
-                    if used_mode == "mix":
-                        # as there is no mix mode, we use multiply here, which is similar
-                        infuse_node.blend_type = "MULTIPLY"
-                        infuse_node.inputs["Fac"].default_value = mix_strength
-                        input_offset = 1
-                    elif used_mode == "add":
-                        infuse_node.blend_type = "ADD"
-                        input_offset = 0
-                    else:
-                        raise Exception(f"This mode is not supported here: {used_mode}!")
-                    infuse_output = infuse_node.outputs["Color"]
-                else:
-                    # for the normal surface output (Color)
-                    if used_mode == "mix":
-                        infuse_node = material.new_node('ShaderNodeMixShader')
-                        infuse_node.inputs[0].default_value = mix_strength
-                        input_offset = 1
-                    elif used_mode == "add":
-                        infuse_node = material.new_node('ShaderNodeMixShader')
-                        input_offset = 0
-                    else:
-                        raise Exception(f"This mode is not supported here: {used_mode}!")
-                    infuse_output = infuse_node.outputs["Shader"]
-
-                # link the infuse node with the correct group node and the material output
-                for link in mat_output_input.links:
-                    material.link(link.from_socket, infuse_node.inputs[input_offset])
-                material.link(group_node.outputs[mat_output_input.name], infuse_node.inputs[input_offset + 1])
-                material.link(infuse_output, mat_output_input)
+        material.infuse_material(
+            material=used_material,
+            mode=used_mode,
+            mix_strength=mix_strength
+        )
 
     def _add_dust_to_material(self, material: Material, value: dict):
         """
@@ -722,159 +632,9 @@ class MaterialManipulator(Module):
         # if no texture is used, a random noise texture is generated
         texture_nodes = config.get_list("used_dust_texture", None)
 
-        group_node = material.new_node("ShaderNodeGroup")
-        group_node.width = 250
-        group = bpy.data.node_groups.new(name="Dust Material", type="ShaderNodeTree")
-        group_node.node_tree = group
-        nodes, links = group.nodes, group.links
-
-        # define start locations and differences, to make the debugging easier
-        x_pos, x_diff = -(250 * 4), 250
-        y_pos, y_diff = (x_diff * 1), x_diff
-
-        # Extract the normal for the current material location
-        geometry_node = nodes.new('ShaderNodeNewGeometry')
-        geometry_node.location.x = x_pos + x_diff * 0
-        geometry_node.location.y = y_pos
-        # this node clips the values, to avoid negative values in the usage below
-        clip_mix_node = nodes.new("ShaderNodeMixRGB")
-        clip_mix_node.inputs["Fac"].default_value = 1.0
-        clip_mix_node.use_clamp = True
-        clip_mix_node.location.x = x_pos + x_diff * 1
-        clip_mix_node.location.y = y_pos
-        links.new(geometry_node.outputs["Normal"], clip_mix_node.inputs["Color2"])
-
-        # use only the z component
-        separate_z_normal = nodes.new("ShaderNodeSeparateRGB")
-        separate_z_normal.location.x = x_pos + x_diff * 2
-        separate_z_normal.location.y = y_pos
-        links.new(clip_mix_node.outputs["Color"], separate_z_normal.inputs["Image"])
-
-        # this layer weight adds a small fresnel around the object, which makes it more realistic
-        layer_weight = nodes.new("ShaderNodeLayerWeight")
-        layer_weight.location.x = x_pos + x_diff * 2
-        layer_weight.location.y = y_pos - y_diff * 1
-        layer_weight.inputs["Blend"].default_value = 0.5
-        # combine it with the z component
-        mix_with_layer_weight = nodes.new("ShaderNodeMixRGB")
-        mix_with_layer_weight.location.x = x_pos + x_diff * 3
-        mix_with_layer_weight.location.y = y_pos
-        mix_with_layer_weight.inputs["Fac"].default_value = 0.2
-        links.new(separate_z_normal.outputs["B"], mix_with_layer_weight.inputs["Color1"])
-        links.new(layer_weight.outputs["Facing"], mix_with_layer_weight.inputs["Color2"])
-        # add a gamma node, to scale the colors correctly
-        gamma_node = nodes.new("ShaderNodeGamma")
-        gamma_node.location.x = x_pos + x_diff * 4
-        gamma_node.location.y = y_pos
-        gamma_node.inputs["Gamma"].default_value = 2.2
-        links.new(mix_with_layer_weight.outputs["Color"], gamma_node.inputs["Color"])
-
-        # use an overlay node to combine it with the texture result
-        overlay = nodes.new("ShaderNodeMixRGB")
-        overlay.location.x = x_pos + x_diff * 5
-        overlay.location.y = y_pos
-        overlay.blend_type = "OVERLAY"
-        overlay.inputs["Fac"].default_value = 1.0
-        links.new(gamma_node.outputs["Color"], overlay.inputs["Color1"])
-
-        # add a multiply node to scale down or up the strength of the dust
-        multiply_node = nodes.new("ShaderNodeMath")
-        multiply_node.location.x = x_pos + x_diff * 6
-        multiply_node.location.y = y_pos
-        multiply_node.inputs[1].default_value = strength
-        multiply_node.operation = "MULTIPLY"
-        links.new(overlay.outputs["Color"], multiply_node.inputs[0])
-
-        # add texture coords to make the scaling of the dust texture possible
-        texture_coords = nodes.new("ShaderNodeTexCoord")
-        texture_coords.location.x = x_pos + x_diff * 0
-        texture_coords.location.y = y_pos - y_diff * 2
-        mapping_node = nodes.new("ShaderNodeMapping")
-        mapping_node.location.x = x_pos + x_diff * 1
-        mapping_node.location.y = y_pos - y_diff * 2
-        mapping_node.vector_type = "TEXTURE"
-        scale_value = texture_scale
-        mapping_node.inputs["Scale"].default_value = [scale_value] * 3
-        links.new(texture_coords.outputs["UV"], mapping_node.inputs["Vector"])
-        # check if a texture should be used
-        if texture_nodes is not None and texture_nodes:
-            texture_node = nodes.new("ShaderNodeTexImage")
-            texture_node.location.x = x_pos + x_diff * 2
-            texture_node.location.y = y_pos - y_diff * 2
-            texture_node.image = random.choice(texture_nodes).image
-            links.new(mapping_node.outputs["Vector"], texture_node.inputs["Vector"])
-            links.new(texture_node.outputs["Color"], overlay.inputs["Color2"])
-        else:
-            if not texture_nodes:
-                warnings.warn("No texture was found, check the config. Random generated texture is used.")
-            # if no texture is used, we great a random noise pattern, which is used instead
-            noise_node = nodes.new("ShaderNodeTexNoise")
-            noise_node.location.x = x_pos + x_diff * 2
-            noise_node.location.y = y_pos - y_diff * 2
-            # this determines the pattern of the dust flakes, a high scale makes them small enough to look like dust
-            noise_node.inputs["Scale"].default_value = 250.0
-            noise_node.inputs["Detail"].default_value = 0.0
-            noise_node.inputs["Roughness"].default_value = 0.0
-            noise_node.inputs["Distortion"].default_value = 1.9
-            links.new(mapping_node.outputs["Vector"], noise_node.inputs["Vector"])
-            # this noise_node produces RGB colors, we only need one value in this instance red
-            separate_r_channel = nodes.new("ShaderNodeSeparateRGB")
-            separate_r_channel.location.x = x_pos + x_diff * 3
-            separate_r_channel.location.y = y_pos - y_diff * 2
-            links.new(noise_node.outputs["Color"], separate_r_channel.inputs["Image"])
-            # as the produced noise image has a nice fading to it, we use a color ramp to create dust flakes
-            color_ramp = nodes.new("ShaderNodeValToRGB")
-            color_ramp.location.x = x_pos + x_diff * 4
-            color_ramp.location.y = y_pos - y_diff * 2
-            color_ramp.color_ramp.elements[0].position = 0.4
-            color_ramp.color_ramp.elements[0].color = [1, 1, 1, 1]
-            color_ramp.color_ramp.elements[1].position = 0.46
-            color_ramp.color_ramp.elements[1].color = [0, 0, 0, 1]
-            links.new(separate_r_channel.outputs["R"], color_ramp.inputs["Fac"])
-            links.new(color_ramp.outputs["Color"], overlay.inputs["Color2"])
-
-        # combine the previous color with the new dust mode
-        mix_shader = nodes.new("ShaderNodeMixShader")
-        mix_shader.location = (x_pos + x_diff * 8, y_pos)
-        links.new(multiply_node.outputs["Value"], mix_shader.inputs["Fac"])
-
-        # add a bsdf node for the dust, this will be used to actually give the dust a color
-        dust_color = nodes.new("ShaderNodeBsdfPrincipled")
-        dust_color.location = (x_pos + x_diff * 6, y_pos - y_diff)
-        # the used dust color is a grey with a tint in orange
-        dust_color.inputs["Base Color"].default_value = [0.8, 0.773, 0.7, 1.0]
-        dust_color.inputs["Roughness"].default_value = 1.0
-        dust_color.inputs["Specular"].default_value = 0.0
-        links.new(dust_color.outputs["BSDF"], mix_shader.inputs[2])
-
-        # create the input and output nodes inside of the group
-        group_output = nodes.new("NodeGroupOutput")
-        group_output.location = (x_pos + x_diff * 9, y_pos)
-        group_input = nodes.new("NodeGroupInput")
-        group_input.location = (x_pos + x_diff * 7, y_pos - y_diff * 0.5)
-
-        # create sockets for the outside of the group match them to the mix shader
-        group.outputs.new(mix_shader.outputs[0].bl_idname, mix_shader.outputs[0].name)
-        group.inputs.new(mix_shader.inputs[1].bl_idname, mix_shader.inputs[1].name)
-        group.inputs.new(multiply_node.inputs[1].bl_idname, "Dust strength")
-        group.inputs.new(mapping_node.inputs["Scale"].bl_idname, "Texture scale")
-
-        # link the input and output to the mix shader
-        links.new(group_input.outputs[0], mix_shader.inputs[1])
-        links.new(mix_shader.outputs[0], group_output.inputs[0])
-        links.new(group_input.outputs["Dust strength"], multiply_node.inputs[1])
-        links.new(group_input.outputs["Texture scale"], mapping_node.inputs["Scale"])
-
-        # remove the connection between the output and the last node and put the mix shader in between
-        node_connected_to_the_output, material_output = material.get_node_connected_to_the_output_and_unlink_it()
-
-        # place the group node above the material output
-        group_node.location = (material_output.location.x - x_diff, material_output.location.y + y_diff)
-
-        # connect the dust group
-        material.link(node_connected_to_the_output.outputs[0], group_node.inputs[0])
-        material.link(group_node.outputs[0], material_output.inputs["Surface"])
-
-        # set the default values
-        group_node.inputs["Dust strength"].default_value = strength
-        group_node.inputs["Texture scale"].default_value = [texture_scale] * 3
+        Dust.add_to_material(
+            material=material,
+            strength=strength,
+            texture_nodes=texture_nodes,
+            texture_scale=texture_scale
+        )
