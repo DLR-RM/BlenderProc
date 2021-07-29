c0a0912d7d23d2b456af66d8cb92897401231a5d
Dominik Winkelbauer <dominik.winkelbauer@dlr.de>
2021-07-29 11:50:45 +0200

    chore(stereo): Migrates the stereo matching writer and example

---
+++ examples/advanced/stereo_matching/main.py
@@ -0,0 +1,64 @@
+from src.utility.SetupUtility import SetupUtility
+SetupUtility.setup([])
+
+from src.utility.writer.StereoGlobalMatchingWriter import StereoGlobalMatchingWriter
+from src.utility.Utility import Utility
+from src.utility.MathUtility import MathUtility
+from src.utility.CameraUtility import CameraUtility
+from src.utility.LabelIdMapping import LabelIdMapping
+from src.utility.MaterialLoaderUtility import MaterialLoaderUtility
+from src.utility.loader.SuncgLoader import SuncgLoader
+from src.utility.lighting.SuncgLighting import SuncgLighting
+from src.utility.WriterUtility import WriterUtility
+from src.utility.Initializer import Initializer
+from src.utility.RendererUtility import RendererUtility
+
+import argparse
+import os
+import numpy as np
+
+parser = argparse.ArgumentParser()
+parser.add_argument('camera', help="Path to the camera file which describes one camera pose per line, here the output of scn2cam from the SUNCGToolbox can be used")
+parser.add_argument('house', help="Path to the house.json file of the SUNCG scene to load")
+parser.add_argument('output_dir', nargs='?', default="examples/datasets/suncg_basic/output", help="Path to where the final files, will be saved")
+args = parser.parse_args()
+
+Initializer.init()
+
+# load the objects into the scene
+label_mapping = LabelIdMapping.from_csv(Utility.resolve_path(os.path.join('resources', 'id_mappings', 'nyu_idset.csv')))
+objs = SuncgLoader.load(args.house, label_mapping=label_mapping)
+
+# define the camera intrinsics
+K = np.array([
+    [650.018, 0, 637.962],
+    [0, 650.018, 355.984],
+    [0, 0, 1]
+])
+CameraUtility.set_intrinsics_from_K_matrix(K, 1280, 720)
+CameraUtility.set_stereo_parameters(interocular_distance=0.05, convergence_mode="PARALLEL", convergence_distance=0.00001)
+
+# read the camera positions file and convert into homogeneous camera-world transformation
+with open(args.camera, "r") as f:
+    for line in f.readlines():
+        line = [float(x) for x in line.split()]
+        position = MathUtility.change_coordinate_frame_of_point(line[:3], ["X", "-Z", "Y"])
+        rotation = MathUtility.change_coordinate_frame_of_point(line[3:6], ["X", "-Z", "Y"])
+        matrix_world = MathUtility.build_transformation_mat(position, CameraUtility.rotation_from_forward_vec(rotation))
+        CameraUtility.add_camera_pose(matrix_world)
+
+# makes Suncg objects emit light
+SuncgLighting.light()
+
+# activate normal and distance rendering
+RendererUtility.enable_distance_output()
+MaterialLoaderUtility.add_alpha_channel_to_textures(blurry_edges=True)
+RendererUtility.toggle_stereo(True)
+
+# render the whole pipeline
+data = RendererUtility.render()
+
+data["stereo-depth"], data["disparity"] = StereoGlobalMatchingWriter.match(data["colors"], disparity_filter=False)
+
+# write the data to a .hdf5 container
+WriterUtility.save_to_hdf5(args.output_dir, data)
--- src/utility/writer/StereoGlobalMatchingWriter.py
+++ src/utility/writer/StereoGlobalMatchingWriter.py
@@ -1,95 +1,43 @@
 from src.utility.SetupUtility import SetupUtility
 SetupUtility.setup_pip(["Pillow", "opencv-contrib-python"])
 
-import os
-from math import tan
-
+from typing import Tuple, List
 import bpy
 import cv2
 import numpy as np
 
-from src.main.GlobalStorage import GlobalStorage
-from src.renderer.RendererInterface import RendererInterface
-from src.utility.BlenderUtility import load_image
+from src.utility.CameraUtility import CameraUtility
 from src.utility.SGMUtility import fill_in_fast
 from src.utility.SGMUtility import resize
-from src.utility.Utility import Utility
 
+class StereoGlobalMatchingWriter:
 
-class StereoGlobalMatchingWriter(RendererInterface):
-    """ Writes depth image generated from the stereo global matching algorithm to file
-
-    **Configuration**:
-
-    .. list-table:: 
-        :widths: 25 100 10
-        :header-rows: 1
-
-        * - Parameter
-          - Description
-          - Type
-        * - infer_focal_length_from_fov
-          - If true, then focal length would be calculated from the field of view angle, otherwise the value of the
-            focal length would be read from the config parameter: 'focal_length'. Default: False
-          - bool
-        * - disparity_filter
-          - Applies post-processing of the generated disparity map using WLS filter. Default: True
-          - bool
-        * - depth_completion
-          - Applies basic depth completion using image processing techniques. Default: True
-          - bool
-        * - focal_length
-          - Focal length used in the depth calculation step, should be set if 'infer_focal_length_from_fov' is set
-            to false. Default: 0.0
-          - float
-        * - window_size
-          - Semi-global matching kernel size. Should be an odd number. Optional. Default: 7
-          - int
-        * - num_disparities
-          - Semi-global matching number of disparities. Should be > 0 and divisible by 16. Default: 32
-          - int
-        * - min_disparity
-          - Semi-global matching minimum disparity. Optional. Default: 0
-          - int
-        * - output_disparity
-          - Additionally outputs the disparity map. Default: False
-          - bool
-        * - rgb_output_key
-          - The key for the rgb data in the output. Optional. default: colors.
-          - string
-    """
-
-    def __init__(self, config):
-        RendererInterface.__init__(self, config)
-
-        self.rgb_output_key = self.config.get_string("rgb_output_key", "colors")
-        if self.rgb_output_key is None:
-            raise Exception("RGB output is not registered, please register the RGB renderer before this module.")
-
-        self.output_dir = self._determine_output_dir()
-        if not os.path.exists(self.output_dir):
-            os.makedirs(self.output_dir)
-
- 
-    def sgm(self, imgL, imgR):
+    @staticmethod
+    def _sgm(left_color_image: np.ndarray, right_color_image: np.ndarray, baseline: float, depth_max: float, focal_length: float, window_size: int = 7, num_disparities: int = 32, min_disparity: int = 0, disparity_filter: bool = True, depth_completion: bool = True) -> Tuple[np.ndarray, np.ndarray]:
         """ Semi global matching funciton, for more details on what this function does check the original paper
         https://elib.dlr.de/73119/1/180Hirschmueller.pdf
-        
-        :param imgL: Left image. Type: blender image type object.
-        :param imgR: Right image. Type: blender image type object.
+
+        :param left_color_image: The left color image.
+        :param right_color_image: The right color image.
+        :param baseline: The baseline that was used for rendering the two images.
+        :param depth_max: The maximum depth value for clipping the resulting depth values.
+        :param focal_length: The focal length that was used for rendering the two images.
+        :param window_size: Semi-global matching kernel size. Should be an odd number.
+        :param num_disparities: Semi-global matching number of disparities. Should be > 0 and divisible by 16.
+        :param min_disparity: Semi-global matching minimum disparity.
+        :param disparity_filter: Applies post-processing of the generated disparity map using WLS filter.
+        :param depth_completion: Applies basic depth completion using image processing techniques.
         :return: depth, disparity
          """
-        window_size = self.config.get_int("window_size", 7)
         if window_size % 2 == 0:
             raise Exception("Window size must be an odd number")
 
-        numDisparities = self.config.get_int("num_disparities", 32)
-        if not (numDisparities > 0 and numDisparities % 16 == 0):
+        if not (num_disparities > 0 and num_disparities % 16 == 0):
             raise Exception("Number of disparities must be > 0 and divisible by 16")
 
         left_matcher = cv2.StereoSGBM_create(
-            minDisparity=self.config.get_int("min_disparity", 0),
-            numDisparities=numDisparities,
+            minDisparity=min_disparity,
+            numDisparities=num_disparities,
             blockSize=5,
             P1=8 * 3 * window_size ** 2,
             P2=32 * 3 * window_size ** 2,
@@ -102,7 +50,7 @@ class StereoGlobalMatchingWriter(RendererInterface):
             mode=cv2.StereoSGBM_MODE_HH
         )
 
-        if self.config.get_bool("disparity_filter", True):
+        if disparity_filter:
             right_matcher = cv2.ximgproc.createRightMatcher(left_matcher)
 
             lmbda = 80000
@@ -112,90 +60,73 @@ class StereoGlobalMatchingWriter(RendererInterface):
             wls_filter.setLambda(lmbda)
             wls_filter.setSigmaColor(sigma)
 
-            dispr = right_matcher.compute(imgR, imgL)
+            dispr = right_matcher.compute(right_color_image, left_color_image)
 
-        displ = left_matcher.compute(imgL, imgR)
+        displ = left_matcher.compute(left_color_image, right_color_image)
 
         filteredImg = None
-        if self.config.get_bool("disparity_filter", True):
-            filteredImg = wls_filter.filter(displ, imgL, None, dispr).astype(np.float32)
+        if disparity_filter:
+            filteredImg = wls_filter.filter(displ, left_color_image, None, dispr).astype(np.float32)
             filteredImg = cv2.normalize(src=filteredImg, dst=filteredImg, beta=0, alpha=255, norm_type=cv2.NORM_MINMAX)
 
-        disparity_to_be_written = filteredImg if self.config.get_bool("disparity_filter", True) else displ
+        disparity_to_be_written = filteredImg if disparity_filter else displ
         disparity = np.float64(np.copy(disparity_to_be_written)) / 16.0
 
         # Crop and resize, due to baseline, a part of the image on the left can't be matched with the one on the right
-        disparity = resize(disparity[:, numDisparities:], (self.width, self.height))
+        print(left_color_image.shape[1], left_color_image.shape[0])
+        disparity = resize(disparity[:, num_disparities:], (left_color_image.shape[1], left_color_image.shape[0]))
 
         # Triangulation
-        depth = (1.0 / disparity) * self.baseline * self.focal_length
+        depth = (1.0 / disparity) * baseline * focal_length
 
         # Clip from depth map to 25 meters
-        depth[depth > self.depth_max] = self.depth_max
+        depth[depth > depth_max] = depth_max
         depth[depth < 0] = 0.0
 
-        if self.config.get_bool("depth_completion", True):
-            depth = fill_in_fast(depth, self.depth_max)
+        if depth_completion:
+            depth = fill_in_fast(depth, depth_max)
 
         return depth, disparity_to_be_written
 
-    def run(self):
+    @staticmethod
+    def match(color_images: List[np.ndarray], depth_max: float = None, window_size: int = 7, num_disparities: int = 32, min_disparity: int = 0, disparity_filter: bool = True, depth_completion: bool = True) -> Tuple[List[np.ndarray], List[np.ndarray]]:
         """ Does the stereo global matching in the following steps:
         1. Collect camera object and its state,
         2. For each frame, load left and right images and call the `sgm()` methode.
         3. Write the results to a numpy file.
+
+        :param color_images: A list of stereo images, where each entry has the shape [2, height, width, 3].
+        :param depth_max: The maximum depth value for clipping the resulting depth values. If None, distance_start + distance_range that were configured for distance rendering are used.
+        :param window_size: Semi-global matching kernel size. Should be an odd number.
+        :param num_disparities: Semi-global matching number of disparities. Should be > 0 and divisible by 16.
+        :param min_disparity: Semi-global matching minimum disparity.
+        :param disparity_filter: Applies post-processing of the generated disparity map using WLS filter.
+        :param depth_completion: Applies basic depth completion using image processing techniques.
+        :return: Returns the computed depth and disparity images for all given frames.
         """
-        if self._avoid_output:
-            print("Avoid output is on, no output produced!")
-            return
-
-        if GlobalStorage.is_in_storage("renderer_distance_end"):
-            self.depth_max = GlobalStorage.get("renderer_distance_end")
-        else:
-            raise RuntimeError("A distance rendering has to be executed before this module is executed, "
-                               "else the `renderer_distance_end` is not set!")
-
-        self.rgb_output_path = Utility.find_registered_output_by_key(self.rgb_output_key)["path"]
-
         # Collect camera and camera object
         cam_ob = bpy.context.scene.camera
         cam = cam_ob.data
 
-        self.width = bpy.context.scene.render.resolution_x
-        self.height = bpy.context.scene.render.resolution_y
-        print('Resolution: {}, {}'.format(self.width, self.height))
+        baseline = cam.stereo.interocular_distance
+        if not baseline:
+            raise Exception("Stereo parameters are not set. Make sure to enable RGB stereo rendering before this module.")
 
-        self.baseline = cam.stereo.interocular_distance
-        if not self.baseline:
-            raise Exception(
-                "Stereo parameters are not set. Make sure to enable RGB stereo rendering before this module.")
+        if depth_max is None:
+            depth_max = bpy.context.scene.world.mist_settings.start + bpy.context.scene.world.mist_settings.depth
 
-        if self.config.get_bool("infer_focal_length_from_fov", False):
-            fov = cam.angle_x if cam.angle_x else cam.angle
-            if not fov:
-                raise Exception("Could not obtain field of view angle")
-            self.focal_length = float((1.0 / tan(fov / 2.0)) * (float(self.width) / 2.0))
-        else:
-            self.focal_length = self.config.get_float("focal_length", 0.0)
-            if self.focal_length == 0.0:
-                raise Exception(
-                    "Focal length set to 0. This is either intentional or because no value was set by the user. Either way, this needs to be corrected by setting a value > 0 or enabling 'infer_focal_length_from_fov'.")
+        baseline = cam.stereo.interocular_distance
+        if not baseline:
+            raise Exception("Stereo parameters are not set. Make sure to enable RGB stereo rendering before this module.")
 
-        for frame in range(bpy.context.scene.frame_start, bpy.context.scene.frame_end):
-            path_split = self.rgb_output_path.split(".")
-            path_l = "{}_L.{}".format(path_split[0], path_split[1])
-            path_r = "{}_R.{}".format(path_split[0], path_split[1])
+        focal_length = CameraUtility.get_intrinsics_as_K_matrix()[0, 0]
 
-            imgL = load_image(path_l % frame)
-            imgR = load_image(path_r % frame)
+        depth_frames = []
+        disparity_frames = []
+        for frame, color_image in enumerate(color_images):
+            depth, disparity = StereoGlobalMatchingWriter._sgm(color_image[0], color_image[1], baseline, depth_max, focal_length, window_size, num_disparities, min_disparity, disparity_filter, depth_completion)
 
-            depth, disparity = self.sgm(imgL, imgR)
+            depth_frames.append(depth)
+            disparity_frames.append(disparity)
 
-            np.save(os.path.join(self.output_dir, "stereo-depth_%04d") % frame, depth)
-
-            if self.config.get_bool("output_disparity", False):
-                np.save(os.path.join(self.output_dir, "disparity_%04d") % frame, disparity)
-        Utility.register_output(self._determine_output_dir(), "stereo-depth_", "stereo-depth", ".npy", "1.0.0")
-        if self.config.get_bool("output_disparity", False):
-            Utility.register_output(self._determine_output_dir(), "disparity_", "disparity", ".npy", "1.0.0")
-
+        return depth_frames, disparity_frames
