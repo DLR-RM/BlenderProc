d9de54ae71f3835112ff53644051c783745fc7e9
Dominik Winkelbauer <dominik.winkelbauer@dlr.de>
2021-08-03 16:56:16 +0200

    chore(Undo): Adds unit test

---
+++ tests/testUtility.py
@@ -0,0 +1,25 @@
+
+import unittest
+import os.path
+
+from src.utility.loader.ObjectLoader import ObjectLoader
+from src.utility.Utility import Utility
+from src.utility.Initializer import Initializer
+
+resource_folder = os.path.join(os.path.dirname(__file__), "..", "examples", "resources")
+
+class UnitTestCheckUtility(unittest.TestCase):
+
+    def test_blender_reference_after_undo(self):
+        Initializer.init()
+        objs = ObjectLoader.load(os.path.join(resource_folder, "scene.obj"))
+
+        for obj in objs:
+            obj.set_cp("test", 0)
+
+        with Utility.UndoAfterExecution():
+            for obj in objs:
+                obj.set_cp("test", 1)
+
+        for obj in objs:
+            self.assertEqual(obj.get_cp("test"), 0)
