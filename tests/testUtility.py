6d7eaee25ccd629d6f25d300b420915403af7ff2
Markus Knauer <Markus.Knauer@dlr.de>
2021-08-04 14:42:52 +0200

    feat(tests): add utility test for math util transformation mat

--- tests/testUtility.py
+++ tests/testUtility.py
@@ -1,9 +1,11 @@
 
 import unittest
 import os.path
+import numpy as np
 
 from src.utility.loader.ObjectLoader import ObjectLoader
 from src.utility.Utility import Utility
+from src.utility.MathUtility import MathUtility
 from src.utility.Initializer import Initializer
 
 resource_folder = os.path.join(os.path.dirname(__file__), "..", "examples", "resources")
@@ -25,3 +27,26 @@ class UnitTestCheckUtility(unittest.TestCase):
 
         for obj in objs:
             self.assertEqual(obj.get_cp("test"), 0)
+
+
+    def test_math_util_transformation_mat(self):
+        """ Tests if the transformation matrix is calculated correctly
+        """
+
+        # poi = np.array([0, 0, 0])
+        location = np.array([1, 2, 3])
+        # rotation = 0.0
+        rotation_matrix = np.array([[-8.94427180e-01, -3.58568549e-01, 2.67261177e-01],
+                                        [ 4.47213531e-01, -7.17137218e-01, 5.34522414e-01],
+                                        [ 2.34371083e-08,  5.97614229e-01, 8.01783800e-01]])
+
+        correct_cam2world_matrix = np.array([[-8.94427180e-01, -3.58568549e-01, 2.67261177e-01, 1.00000000e+00],
+                                        [4.47213531e-01, -7.17137218e-01, 5.34522414e-01, 2.00000000e+00],
+                                        [2.34371083e-08, 5.97614229e-01, 8.01783800e-01, 3.00000000e+00],
+                                        [0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
+
+        # Add homog cam pose based on location an rotation
+        cam2world_matrix = MathUtility.build_transformation_mat(location, rotation_matrix)
+
+        for x, y in zip(np.reshape(correct_cam2world_matrix, -1).tolist(), np.reshape(cam2world_matrix, -1).tolist()):
+            self.assertAlmostEqual(x, y)
