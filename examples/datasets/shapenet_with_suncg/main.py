3e9118384da2ed64a79f16218406ecf1b94b3714
Dominik Winkelbauer <dominik.winkelbauer@dlr.de>
2021-08-20 15:18:52 +0200

    chore(examples): Migrates shapenet_with_suncg example to the new API

---
+++ examples/datasets/shapenet_with_suncg/main.py
@@ -0,0 +1,84 @@
+from src.utility.SetupUtility import SetupUtility
+SetupUtility.setup([])
+
+from src.utility.MeshObjectUtility import MeshObject
+from src.utility.filter.Filter import Filter
+from src.utility.Utility import Utility
+from src.utility.MathUtility import MathUtility
+from src.utility.CameraUtility import CameraUtility
+from src.utility.LabelIdMapping import LabelIdMapping
+from src.utility.loader.SuncgLoader import SuncgLoader
+from src.utility.loader.ShapeNetLoader import ShapeNetLoader
+from src.utility.lighting.SuncgLighting import SuncgLighting
+from src.utility.sampler.UpperRegionSampler import UpperRegionSampler
+from src.utility.object.PhysicsSimulation import PhysicsSimulation
+from src.utility.sampler.PartSphere import PartSphere
+from src.utility.WriterUtility import WriterUtility
+from src.utility.Initializer import Initializer
+from src.utility.RendererUtility import RendererUtility
+
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('shape_net', help="path to the downloaded shape net core v2 dataset, get it from (http://www.shapenet.org/)")
+parser.add_argument('house', help="Path to the house.json file of the SUNCG scene to load")
+parser.add_argument('output_dir', nargs='?', default="examples/datasets/shapenet_with_suncg/output", help="Path to where the final files, will be saved")
+args = parser.parse_args()
+
+Initializer.init()
+
+# load suncg house into the scene
+label_mapping = LabelIdMapping.from_csv(Utility.resolve_path(os.path.join('resources', 'id_mappings', 'nyu_idset.csv')))
+suncg_objs = SuncgLoader.load(args.house, label_mapping=label_mapping)
+
+# Find all bed objects, to sample the shapenet objects on
+bed_objs = Filter.by_cp(suncg_objs, "category_id", label_mapping.id_from_label("bed"))
+
+# makes Suncg objects emit light
+SuncgLighting.light()
+
+# load selected shapenet object
+shapenet_obj = ShapeNetLoader.load(args.shape_net, used_synset_id="02801938")
+
+# Sample a point above any bed
+sample_point = UpperRegionSampler.sample(
+    objects_to_sample_on=bed_objs,
+    min_height=0.75,
+)
+# move the shapenet object to the sampled position
+shapenet_obj.set_location(sample_point)
+
+# adding a modifier we avoid that the objects falls through other objects during the physics simulation
+shapenet_obj.add_modifier(name="SOLIDIFY", thickness=0.001)
+
+# enable rigid body component of the objects which makes them participate in physics simulations
+shapenet_obj.enable_rigidbody(active=True, mass_factor=2000, collision_margin=0.0001)
+for obj in Filter.all_with_type(suncg_objs, MeshObject):
+    obj.enable_rigidbody(active=False, mass_factor=2000, collision_margin=0.0001)
+
+# Run the physics simulation
+PhysicsSimulation.simulate_and_fix_final_poses(min_simulation_time=0.5, max_simulation_time=4, check_object_interval=0.25)
+
+# sample five camera poses
+for i in range(5):
+    # sample random camera location around the shapenet object
+    location = PartSphere.sample(center=shapenet_obj.get_location(), mode="SURFACE", radius=2, dist_above_center=0.5)
+    # compute rotation based on vector going from the camera location towards shapenet object
+    rotation_matrix = CameraUtility.rotation_from_forward_vec(shapenet_obj.get_location() - location)
+    # add homog cam pose based on location an rotation
+    cam2world_matrix = MathUtility.build_transformation_mat(location, rotation_matrix)
+    CameraUtility.add_camera_pose(cam2world_matrix)
+
+# set the number of samples to render for each object
+RendererUtility.set_samples(150)
+
+# activate normal and distance rendering
+RendererUtility.enable_normals_output()
+RendererUtility.enable_distance_output()
+
+# render the whole pipeline
+data = RendererUtility.render()
+
+# write the data to a .hdf5 container
+WriterUtility.save_to_hdf5(args.output_dir, data)
