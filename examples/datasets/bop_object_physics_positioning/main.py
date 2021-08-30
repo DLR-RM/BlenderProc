3e980de439db879a117e3a4439b6c5ba1ae48573
Sundermeyer, Martin <Martin.Sundermeyer@dlr.de>
2021-08-30 19:28:45 +0200

    Update examples/datasets/bop_object_physics_positioning/main.py
    Co-authored-by: Winkelbauer, Dominik <Dominik.Winkelbauer@dlr.de>

--- examples/datasets/bop_object_physics_positioning/main.py
+++ examples/datasets/bop_object_physics_positioning/main.py
@@ -66,7 +66,7 @@ for j, obj in enumerate(sampled_bop_objs + distractor_bop_objs):
         
     mat = obj.get_materials()[0]
     if obj.get_cp("bop_dataset_name") in ['itodd', 'tless']:
-        grey_col = np.random.uniform(0.3, 0.9)   
+        grey_col = np.random.uniform(0.1, 0.9)   
         mat.set_principled_shader_value("Base Color", [grey_col, grey_col, grey_col, 1])        
     mat.set_principled_shader_value("Roughness", np.random.uniform(0, 1.0))
     mat.set_principled_shader_value("Specular", np.random.uniform(0, 1.0))
