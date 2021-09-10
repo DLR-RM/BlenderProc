a592317062fd186566abb641c3ea907ff1e2f6a3
Maximilian Denninger <Maximilian.Denninger@dlr.de>
2021-09-10 16:34:34 +0200

    feat(types): add types to bproc api

---
+++ blenderproc/object/__init__.py
@@ -0,0 +1,3 @@
+
+
+from blenderproc.python.types.MeshObjectUtility import get_all_mesh_objects, convert_to_meshes, create_from_blender_mesh, create_with_empty_mesh, create_primitive, disable_all_rigid_bodies, create_bvh_tree_multi_objects, compute_poi, scene_ray_cast
