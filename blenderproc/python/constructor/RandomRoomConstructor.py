872600a4f87268799296fd4a51ac5abdccb3abc5
Dominik Winkelbauer <dominik.winkelbauer@dlr.de>
2021-09-10 17:53:05 +0200

    fix(constructor): Fixes RandomRoomConstructor

--- blenderproc/python/constructor/RandomRoomConstructor.py
+++ blenderproc/python/constructor/RandomRoomConstructor.py
@@ -129,7 +129,7 @@ def construct_random_room(used_floor_area: float, interior_objects: [MeshObject]
     # Add the loaded objects, which couldn't be placed
     list_of_deleted_objects.extend([obj for obj in interior_objects if obj not in placed_objects])
     # Delete them all
-    Entity.delete_multiple(list_of_deleted_objects)
+    delete_multiple(list_of_deleted_objects)
 
     if floor_obj is not None:
         placed_objects.append(floor_obj)
