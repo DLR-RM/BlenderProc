38ed71ff8abf7fdd163dc99883188387b9d05da1
Dominik Winkelbauer <dominik.winkelbauer@dlr.de>
2021-09-20 11:30:53 +0200

    fix(pip): Makes pip install working

--- blenderproc/__init__.py
+++ blenderproc/__init__.py
@@ -1,7 +1,11 @@
 import os
+import sys
 
 # Only import if we are in the blender environment, this environment variable is set by the run.py script
 if "INSIDE_OF_THE_INTERNAL_BLENDER_PYTHON_ENVIRONMENT" in os.environ:
+    # Remove the parent of the blender proc folder, as it might contain other packages
+    # that we do not want to import inside the blenderproc env
+    sys.path.remove(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
     from .python.utility.SetupUtility import SetupUtility
     SetupUtility.setup([])
     from . import loader
