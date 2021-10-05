06a07d5dc0d2bb8a41d756a82d641e2b76b3a2d8
Dominik Winkelbauer <dominik.winkelbauer@dlr.de>
2021-10-05 14:50:07 +0200

    feat(cli): Allows flag arguments in users python script

--- blenderproc/command_line.py
+++ blenderproc/command_line.py
@@ -42,7 +42,6 @@ def cli():
     # Setup all common arguments of run and debug mode
     for subparser in [parser_run, parser_debug]:
         subparser.add_argument('file', default=None, nargs='?', help='The path to a configuration file which describes what the pipeline should do or a python file which uses BlenderProc via the API.')
-        subparser.add_argument('args', metavar='arguments', nargs='*', help='Additional arguments which are used to replace placeholders inside the configuration. <args:i> is hereby replaced by the i-th argument.')
 
         subparser.add_argument('--reinstall-blender', dest='reinstall_blender', action='store_true', help='If given, the blender installation is deleted and reinstalled. Is ignored, if a "custom_blender_path" is configured in the configuration file.')
         subparser.add_argument('--temp-dir', dest='temp_dir', default=None, help="The path to a directory where all temporary output files should be stored. If it doesn't exist, it is created automatically. Type: string. Default: \"/dev/shm\" or \"/tmp/\" depending on which is available.")
@@ -53,10 +52,7 @@ def cli():
         subparser.add_argument('--blender-install-path', dest='blender_install_path', default=None, help="Set path where blender should be installed. If None is given, /home_local/<env:USER>/blender/ is used per default. This argument is ignored if it is specified in the given YAML config.")
         subparser.add_argument('--custom-blender-path', dest='custom_blender_path', default=None, help="Set, if you want to use a custom blender installation to run BlenderProc. If None is given, blender is installed into the configured blender_install_path. This argument is ignored if it is specified in the given YAML config.")
 
-    if len(sys.argv) >= 2 and sys.argv[1] in scripts.keys():
-        args, unknown_args = parser.parse_known_args()
-    else:
-        args = parser.parse_args()
+    args, unknown_args = parser.parse_known_args()
 
     if args.mode in ["run", "debug"]:
         # Make sure a file is given
@@ -88,9 +84,9 @@ def cli():
 
         # Run either in debug or in normal mode
         if args.mode == "debug":
-            p = subprocess.Popen([blender_run_path, "--python-use-system-env", "--python-exit-code", "0", "--python", os.path.join(repo_root_directory, "blenderproc/debug_startup.py"), "--", path_src_run if not is_config else args.file, temp_dir] + args.args, env=used_environment)
+            p = subprocess.Popen([blender_run_path, "--python-use-system-env", "--python-exit-code", "0", "--python", os.path.join(repo_root_directory, "blenderproc/debug_startup.py"), "--", path_src_run if not is_config else args.file, temp_dir] + unknown_args, env=used_environment)
         else:
-            p = subprocess.Popen([blender_run_path, "--background", "--python-use-system-env", "--python-exit-code", "2", "--python", path_src_run, "--", args.file, temp_dir] + args.args, env=used_environment)
+            p = subprocess.Popen([blender_run_path, "--background", "--python-use-system-env", "--python-exit-code", "2", "--python", path_src_run, "--", args.file, temp_dir] + unknown_args, env=used_environment)
 
         def clean_temp_dir():
             # If temp dir should not be kept and temp dir still exists => remove it
