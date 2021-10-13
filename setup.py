20add103595d6a43dd39648570c1d25ac6d95771
Dominik Winkelbauer <dominik.winkelbauer@dlr.de>
2021-10-13 11:34:41 +0200

    chore(setup): Updates version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='blenderproc',
-      version='1.12',
+      version='2.0.0a3',
       url='https://github.com/DLR-RM/BlenderProc',
       author='German Aerospace Center (DLR) - Institute of Robotics and Mechatronics (RM)',
       packages=find_packages(exclude=['docs', 'examples', 'external', 'images', 'resources', 'scripts', 'tests']),
