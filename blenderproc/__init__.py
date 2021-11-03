230f285cb52bbcc10d5ffc858d7adedff2887151
Maximilian Denninger <Maximilian.Denninger@dlr.de>
2021-11-03 14:19:18 +0100

    fix(command_line.py): fix command_line.py for blenderproc

--- blenderproc/__init__.py
+++ blenderproc/__init__.py
@@ -36,5 +36,6 @@ else:
     file_names_of_stack = [os.path.basename(file_summary.filename) for file_summary in traceback.extract_stack()]
     first_file_name = file_names_of_stack[0]
     # check if the name of this file is either blenderproc or if the "OUTSIDE_OF_THE_INTERNAL_BLENDER_PYTHON_ENVIRONMENT_BUT_IN_RUN_SCRIPT" is set, which is set in the run.py
-    if "OUTSIDE_OF_THE_INTERNAL_BLENDER_PYTHON_ENVIRONMENT_BUT_IN_RUN_SCRIPT" not in os.environ and "blenderproc" != first_file_name:
+    is_correct_startup_script = first_file_name != "blenderproc" and first_file_name != "command_line.py"
+    if "OUTSIDE_OF_THE_INTERNAL_BLENDER_PYTHON_ENVIRONMENT_BUT_IN_RUN_SCRIPT" not in os.environ and is_correct_startup_script:
         raise Exception(f"\n###############\nThis script can only be run by \"blenderproc run\", instead of calling:\n\tpython {sys.argv[0]}\ncall:\n\tblenderproc run {sys.argv[0]}\n###############")
