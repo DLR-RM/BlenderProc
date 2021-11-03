1dfa65b7566012b5ee1ec71a8b5e64de1503112a
Maximilian Denninger <Maximilian.Denninger@dlr.de>
2021-11-03 12:46:57 +0100

    feat(init): add a check to avoid that people use their own python environment

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import argparse
 import sys
+import os
+os.environ.setdefault("OUTSIDE_OF_THE_INTERNAL_BLENDER_PYTHON_ENVIRONMENT_BUT_IN_RUN_SCRIPT", "1")
 from blenderproc.command_line import cli
 
 parser = argparse.ArgumentParser(add_help=False)
