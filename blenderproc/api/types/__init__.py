8cfe4a8239581a6bb59f1551322429f1c3699e98
wboerdijk <wout.boerdijk@tum.de>
2021-12-10 15:20:49 +0100

    refactor (api): add Armature, Robot classes

--- blenderproc/api/types/__init__.py
+++ blenderproc/api/types/__init__.py
@@ -4,3 +4,5 @@ from blenderproc.python.types.EntityUtility import Entity
 from blenderproc.python.types.MeshObjectUtility import MeshObject
 from blenderproc.python.types.LightUtility import Light
 from blenderproc.python.types.MaterialUtility import Material
+from blenderproc.python.types.ArmatureUtility import Armature
+from blenderproc.python.types.RobotUtility import Robot
