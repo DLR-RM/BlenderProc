5dbc693052df9939b52c0b95c1d522893fdd936f
lab-lin.ziyao <lab-lin.ziyao>
2022-03-09 07:37:14 +0000

    download_cc_textures:将request.get变成session.get，同时设置session的重新尝试次数为3

--- blenderproc/scripts/download_cc_textures.py
+++ blenderproc/scripts/download_cc_textures.py
@@ -8,6 +8,11 @@ import requests
 import argparse
 
 from blenderproc.python.utility.SetupUtility import SetupUtility
+from requests.adapters import HTTPAdapter
+
+session = requests.Session()
+session.mount('http://', HTTPAdapter(max_retries=3))
+session.mount('https://', HTTPAdapter(max_retries=3))
 
 def cli():
     parser = argparse.ArgumentParser("Downloads textures from cc0textures.com")
@@ -20,15 +25,18 @@ def cli():
     }
 
     cc_texture_dir = args.output_dir
+    csv_file_path = os.path.join(cc_texture_dir, "full_info.csv")
     if not os.path.exists(cc_texture_dir):
         os.makedirs(cc_texture_dir)
     else:
-        raise Exception("The folder already exists!")
+        if os.path.isfile(csv_file_path):
+            print("Resume previous download")
+        else:
+            raise Exception("The folder already exists and does not contain cc_textures!")
 
     # download the csv file, which contains all the download links
     csv_url = "https://cc0textures.com/api/v1/downloads_csv"
-    csv_file_path = os.path.join(cc_texture_dir, "full_info.csv")
-    request = requests.get(csv_url, headers=headers)
+    request = session.get(csv_url, headers=headers)
     with open(csv_file_path, "wb") as file:
         file.write(request.content)
 
@@ -53,14 +61,22 @@ def cli():
                 break
         if do_not_use:
             continue
-        print("Download asset: {} of {}/{}".format(asset, index, len(data)))
         current_folder =  os.path.join(cc_texture_dir, asset)
         if not os.path.exists(current_folder):
             os.makedirs(current_folder)
-        current_file_path = os.path.join(current_folder, "{}.zip".format(asset))
-        response = requests.get(link, headers=headers)
-        SetupUtility.extract_from_response(current_folder, response)
-
+        elif os.listdir(current_folder):
+            print("Already downloaded asset: {} of {}/{}".format(asset, index + 1, len(data)))
+            continue
+        try:
+            response = session.get(link, headers=headers, timeout=20)
+        except Exception as e:
+            print(f"Get response from link:{link} failed, with error:{e}")
+            raise e
+        try:
+            SetupUtility.extract_from_response(current_folder, response)
+        except Exception as e:
+            print(f"Cannot extract texture, because:{e}")
+        print("Download and extract asset: {} of {}/{}".format(asset, index + 1, len(data)))
     print("Done downloading textures, saved in {}".format(cc_texture_dir))
 
 if __name__ == "__main__":
