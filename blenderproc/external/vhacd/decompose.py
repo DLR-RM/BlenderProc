6ef05638cbb596d97bef58dff8b6c9b82736353f
Maximilian Denninger <Maximilian.Denninger@dlr.de>
2022-07-05 14:16:53 +0200

    fix(decompose): fix the compose script for v-hacd 4, does not work yet.

--- blenderproc/external/vhacd/decompose.py
+++ blenderproc/external/vhacd/decompose.py
@@ -2,7 +2,7 @@
 # V-HACD Blender add-on
 # Copyright (c) 2014, Alain Ducharme
 # ----------------
-# This software is provided 'as-is', without any express or implied warranty.
+# This software is provided "as-is", without any express or implied warranty.
 # In no event will the authors be held liable for any damages arising from the use of this software.
 # Permission is granted to anyone to use this software for any purpose,
 # including commercial applications, and to alter it and redistribute it freely,
@@ -13,13 +13,14 @@
 # 3. This notice may not be removed or altered from any source distribution.
 
 #
-# NOTE: requires/calls Khaled Mamou's VHACD executable found here: https://github.com/kmammou/v-hacd/
+# NOTE: requires/calls Khaled Mamou"s VHACD executable found here: https://github.com/kmammou/v-hacd/
 # We specifically asked for the permission to use this inside BlenderProc. All rights are still with Khaled Mamou.
 
 import os
 from sys import platform
 
 import git
+import numpy as np
 
 import bpy
 from mathutils import Matrix
@@ -27,7 +28,7 @@ import bmesh
 from subprocess import Popen
 import shutil
 
-def convex_decomposition(obj: "MeshObject", temp_dir: str, vhacd_path: str, resolution: int = 1000000, name_template: str = "?_hull_#", remove_doubles: bool = True, apply_modifiers: bool = True, apply_transforms: str = "NONE", depth: int = 20, concavity: float = 0.0025, plane_downsampling: int = 4, convexhull_downsampling: int = 4, alpha: float = 0.05, beta: float = 0.05, gamma: float = 0.00125, pca: bool = False, mode: str = "VOXEL", max_num_vertices_per_ch: int = 32, min_volume_per_ch: float = 0.0001, cache_dir: str = None):
+def convex_decomposition(obj: "MeshObject", temp_dir: str, vhacd_path: str, resolution: int = 1000000, name_template: str = "?_hull_#", remove_doubles: bool = True, apply_modifiers: bool = True, apply_transforms: str = "NONE", depth: int = 20, concavity: float = 0.0025, plane_downsampling: int = 4, convexhull_downsampling: int = 4, alpha: float = 0.05, beta: float = 0.05, gamma: float = 0.00125, pca: bool = False, mode: str = "VOXEL", max_num_vertices_per_ch: int = 64, min_volume_per_ch: float = 0.0001, cache_dir: str = None):
     """ Uses V-HACD to decompose the given object.
 
     You can turn of the usage of OpenCL by setting the environment variable NO_OPENCL to "1".
@@ -69,12 +70,11 @@ def convex_decomposition(obj: "MeshObject", temp_dir: str, vhacd_path: str, reso
         else:
             os.system(os.path.join(os.path.dirname(__file__), "build_linux.sh") + " " + os.path.join(vhacd_path, "v-hacd") + " -DNO_OPENCL=OFF")
 
-    off_filename = os.path.join(temp_dir, 'vhacd.off')
-    outFileName = os.path.join(temp_dir, 'vhacd.wrl')
-    logFileName = os.path.join(temp_dir, 'vhacd_log.txt')
+    off_filename = os.path.join(temp_dir, "vhacd.obj")
+    logFileName = os.path.join(temp_dir, "vhacd_log.txt")
 
     # Apply modifiers
-    bpy.ops.object.select_all(action='DESELECT')
+    bpy.ops.object.select_all(action="DESELECT")
     if apply_modifiers:
         mesh = obj.blender_obj.evaluated_get(bpy.context.evaluated_depsgraph_get()).data.copy()
     else:
@@ -83,17 +83,17 @@ def convex_decomposition(obj: "MeshObject", temp_dir: str, vhacd_path: str, reso
     # Apply transforms
     translation, quaternion, scale = Matrix(obj.get_local2world_mat()).decompose()
     scale_matrix = Matrix(((scale.x, 0, 0, 0), (0, scale.y, 0, 0), (0, 0, scale.z, 0), (0, 0, 0, 1)))
-    if apply_transforms in ['S', 'RS', 'LRS']:
+    if apply_transforms in ["S", "RS", "LRS"]:
         pre_matrix = scale_matrix
         post_matrix = Matrix()
     else:
         pre_matrix = Matrix()
         post_matrix = scale_matrix
-    if apply_transforms in ['RS', 'LRS']:
+    if apply_transforms in ["RS", "LRS"]:
         pre_matrix = quaternion.to_matrix().to_4x4() @ pre_matrix
     else:
         post_matrix = quaternion.to_matrix().to_4x4() @ post_matrix
-    if apply_transforms == 'LRS':
+    if apply_transforms == "LRS":
         pre_matrix = Matrix.Translation(translation) @ pre_matrix
     else:
         post_matrix = Matrix.Translation(translation) @ post_matrix
@@ -114,26 +114,31 @@ def convex_decomposition(obj: "MeshObject", temp_dir: str, vhacd_path: str, reso
     for vert in mesh.vertices:
         # Combine the hashes of the local coordinates of all vertices
         mesh_hash = hash((mesh_hash, hash(vert.co[:])))
+    mesh_hash = abs(mesh_hash)
 
-    if cache_dir is None or not os.path.exists(os.path.join(cache_dir, str(mesh_hash) + ".wrl")):
-        vhacd_binary = os.path.join(vhacd_path, "v-hacd", 'bin', "test", "testVHACD")
+    if cache_dir is None or not os.path.exists(os.path.join(cache_dir, str(mesh_hash) + ".obj")):
+        vhacd_binary = os.path.join(vhacd_path, "v-hacd", "app", "TestVHACD")
+        if not os.path.exists(vhacd_binary):
+            raise FileNotFoundError("The vhacd binary was not found, the build script probably failed!")
 
         # Run V-HACD
-        print('\nExporting mesh for V-HACD: {}...'.format(off_filename))
-        off_export(mesh, off_filename)
+        print("\nExporting mesh for V-HACD: {}...".format(off_filename))
+        obj_export(mesh, off_filename)
         bpy.data.meshes.remove(mesh)
-        cmd_line = ('"{}" --input "{}" --resolution {} --depth {} '
-                    '--concavity {:g} --planeDownsampling {} --convexhullDownsampling {} '
-                    '--alpha {:g} --beta {:g} --gamma {:g} --pca {:b} --mode {:b} '
-                    '--maxNumVerticesPerCH {} --minVolumePerCH {:g} --output "{}" --log "{}"').format(
-            vhacd_binary, off_filename, resolution, depth,
-            concavity, plane_downsampling, convexhull_downsampling,
-            alpha, beta, gamma, pca, mode == 'TETRAHEDRON',
-            max_num_vertices_per_ch, min_volume_per_ch, outFileName, logFileName)
+        cmd_line = f'"{vhacd_binary}" {off_filename} -r {resolution} -v {max_num_vertices_per_ch} -d {depth}' #> {logFileName}'
+        #cmd_line = ('"{}" --input "{}" --resolution {} --depth {} '
+        #            "--concavity {:g} --planeDownsampling {} --convexhullDownsampling {} "
+        #            "--alpha {:g} --beta {:g} --gamma {:g} --pca {:b} --mode {:b} "
+        #            '--maxNumVerticesPerCH {} --minVolumePerCH {:g} --output "{}" --log "{}"').format(
+        #    vhacd_binary, off_filename, resolution, depth,
+        #    concavity, plane_downsampling, convexhull_downsampling,
+        #    alpha, beta, gamma, pca, mode == "TETRAHEDRON",
+        #    max_num_vertices_per_ch, min_volume_per_ch, outFileName, logFileName)
 
-        print('Running V-HACD...\n{}\n'.format(cmd_line))
-        vhacd_process = Popen(cmd_line, bufsize=-1, close_fds=True, shell=True)
+        print("Running V-HACD...\n{}\n".format(cmd_line))
+        vhacd_process = Popen(cmd_line, bufsize=-1, close_fds=True, shell=True, cwd=temp_dir)
         vhacd_process.wait()
+        outFileName = os.path.join(temp_dir, "decomp.stl")
 
         # Import convex parts
         if not os.path.exists(outFileName):
@@ -144,33 +149,33 @@ def convex_decomposition(obj: "MeshObject", temp_dir: str, vhacd_path: str, reso
             if not os.path.exists(cache_dir):
                 os.makedirs(cache_dir, exist_ok=True)
             # Copy decomposition into cache dir
-            shutil.copyfile(outFileName, os.path.join(cache_dir, str(mesh_hash) + ".wrl"))
+            shutil.copyfile(outFileName, os.path.join(cache_dir, str(mesh_hash) + ".stl"))
     else:
-        outFileName = os.path.join(cache_dir, str(mesh_hash) + ".wrl")
+        outFileName = os.path.join(cache_dir, str(mesh_hash) + ".stl")
 
-    bpy.ops.import_scene.x3d(filepath=outFileName, axis_forward='Y', axis_up='Z')
+    bpy.ops.import_scene.stl(filepath=outFileName, axis_forward="Y", axis_up="Z")
     imported = bpy.context.selected_objects
 
     # Name and transform the loaded parts
     for index, hull in enumerate(imported):
         hull.select_set(False)
         hull.matrix_basis = post_matrix
-        name = name_template.replace('?', obj.get_name(), 1)
-        name = name.replace('#', str(index + 1), 1)
+        name = name_template.replace("?", obj.get_name(), 1)
+        name = name.replace("#", str(index + 1), 1)
         if name == name_template:
             name += str(index + 1)
         hull.name = name
         hull.data.name = name
-        hull.display_type = 'WIRE'
+        hull.display_type = "WIRE"
 
     return imported
 
-def off_export(mesh, fullpath):
+def obj_export(mesh, fullpath):
     """ Export triangulated mesh to Object File Format """
-    with open(fullpath, 'wb') as off:
-        off.write(b'OFF\n')
-        off.write(str.encode('{} {} 0\n'.format(len(mesh.vertices), len(mesh.polygons))))
+    with open(fullpath, "wb") as off:
         for vert in mesh.vertices:
-            off.write(str.encode('{:g} {:g} {:g}\n'.format(*vert.co)))
+            off.write(str.encode("v {:g} {:g} {:g}\n".format(*vert.co)))
         for face in mesh.polygons:
-            off.write(str.encode('3 {} {} {}\n'.format(*face.vertices)))
+            vertices = np.array(face.vertices) + 1
+            off.write(str.encode("f {} {} {}\n".format(*vertices)))
+
