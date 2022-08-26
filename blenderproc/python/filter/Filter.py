c6420c344a09fdf3d50951af28ddbd4e382102d8
Maximilian Denninger <Maximilian.Denninger@dlr.de>
2022-08-26 10:59:34 +0200

    chore(filter): pylint for filter

--- blenderproc/python/filter/Filter.py
+++ blenderproc/python/filter/Filter.py
@@ -1,6 +1,9 @@
+"""Filter classes to filter entities based on certain attributes"""
+
 from typing import Any, Type, List
-import numpy as np
 import re
+
+import numpy as np
 
 from blenderproc.python.types.StructUtility import Struct
 
@@ -14,8 +17,7 @@ def all_with_type(elements: List[Struct], filtered_data_type: Type[Struct] = Non
     """
     if filtered_data_type is not None:
         return list(filter(lambda x: isinstance(x, filtered_data_type), elements))
-    else:
-        return elements
+    return elements
 
 
 def by_attr(elements: List[Struct], attr_name: str, value: Any, filtered_data_type: Type[Struct] = None,
@@ -30,7 +32,7 @@ def by_attr(elements: List[Struct], attr_name: str, value: Any, filtered_data_ty
     :return: The elements from the given list that match the given value at the specified attribute.
     """
     elements = all_with_type(elements, filtered_data_type)
-    return list(filter(lambda struct: Filter._check_equality(struct.get_attr(attr_name), value, regex), elements))
+    return list(filter(lambda struct: Filter.check_equality(struct.get_attr(attr_name), value, regex), elements))
 
 
 def one_by_attr(elements: List[Struct], attr_name: str, value: Any, filtered_data_type: Type[Struct] = None,
@@ -47,7 +49,7 @@ def one_by_attr(elements: List[Struct], attr_name: str, value: Any, filtered_dat
     :return: The one element from the given list that matches the given value at the specified attribute.
     """
     elements = by_attr(elements, attr_name, value, filtered_data_type, regex)
-    return Filter._check_list_has_length_one(elements)
+    return Filter.check_list_has_length_one(elements)
 
 
 def by_cp(elements: List[Struct], cp_name: str, value: Any, filtered_data_type: Type[Struct] = None,
@@ -63,7 +65,7 @@ def by_cp(elements: List[Struct], cp_name: str, value: Any, filtered_data_type: 
     """
     elements = all_with_type(elements, filtered_data_type)
     return list(
-        filter(lambda struct: struct.has_cp(cp_name) and Filter._check_equality(struct.get_cp(cp_name), value, regex),
+        filter(lambda struct: struct.has_cp(cp_name) and Filter.check_equality(struct.get_cp(cp_name), value, regex),
                elements))
 
 
@@ -81,12 +83,13 @@ def one_by_cp(elements: List[Struct], cp_name: str, value: Any, filtered_data_ty
     :return: The one element from the given list that matches the given value at the specified custom property.
     """
     elements = by_cp(elements, cp_name, value, filtered_data_type, regex)
-    return Filter._check_list_has_length_one(elements)
+    return Filter.check_list_has_length_one(elements)
 
 
 def by_attr_in_interval(elements: List[Struct], attr_name: str, min_value: Any = None, max_value: Any = None,
                         filtered_data_type: Type[Struct] = None) -> List[Struct]:
-    """ Returns all elements from the given list whose specified attribute has a value in the given interval (including the boundaries).
+    """ Returns all elements from the given list whose specified attribute has a value in the given interval
+        (including the boundaries).
 
     :param elements: A list of elements.
     :param attr_name: The name of the attribute to look for.
@@ -97,7 +100,7 @@ def by_attr_in_interval(elements: List[Struct], attr_name: str, min_value: Any =
     """
     elements = all_with_type(elements, filtered_data_type)
     return list(filter(lambda struct: (min_value is None or min_value <= struct.get_attr(attr_name)) and (
-                max_value is None or max_value >= struct.get_attr(attr_name)), elements))
+            max_value is None or max_value >= struct.get_attr(attr_name)), elements))
 
 
 def by_attr_outside_interval(elements: List[Struct], attr_name: str, min_value: Any = None, max_value: Any = None,
@@ -117,9 +120,10 @@ def by_attr_outside_interval(elements: List[Struct], attr_name: str, min_value: 
 
 
 class Filter:
+    """Static class for filtering elements based on different elements. """
 
     @staticmethod
-    def _check_list_has_length_one(elements: List[Any]) -> Any:
+    def check_list_has_length_one(elements: List[Any]) -> Any:
         """ Checks if the given list only contains one element and returns it.
 
         :param elements: The list of elements to check.
@@ -132,8 +136,9 @@ class Filter:
         return elements[0]
 
     @staticmethod
-    def _check_equality(attr_value: Any, filter_value: Any, regex: bool = False) -> bool:
-        """ Checks whether the two values are equal. If the values have multiple elements, they must all match (uses broadcasting).
+    def check_equality(attr_value: Any, filter_value: Any, regex: bool = False) -> bool:
+        """ Checks whether the two values are equal. If the values have multiple elements,
+            they must all match (uses broadcasting).
 
         :param attr_value: The first value.
         :param filter_value: The second value.
@@ -144,10 +149,8 @@ class Filter:
         # If desired do regex matching for strings
         if isinstance(attr_value, str) and regex:
             return re.fullmatch(filter_value, attr_value)
-        else:
-            try:
-                return np.all(np.array(filter_value) == np.array(attr_value))
-            except:
-                raise Exception('Could not broadcast attribute {} with shape {} '
-                                'to filter_value {} with shape {}! '.format(attr_value, np.array(attr_value).shape,
-                                                                            filter_value, np.array(filter_value).shape))
+        try:
+            return np.all(np.array(filter_value) == np.array(attr_value))
+        except Exception as e:
+            raise RuntimeError(f'Could not broadcast attribute {attr_value} with shape {np.array(attr_value).shape} '
+                               f'to filter_value {filter_value} with shape {np.array(filter_value).shape}!') from e
