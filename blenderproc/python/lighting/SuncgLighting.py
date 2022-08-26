07d18dabc1a652b55242d04d14db9f86b1bfd98e
Maximilian Denninger <Maximilian.Denninger@dlr.de>
2022-08-26 11:05:01 +0200

    chore(lighting): pylint for lighting

--- blenderproc/python/lighting/SuncgLighting.py
+++ blenderproc/python/lighting/SuncgLighting.py
@@ -1,3 +1,5 @@
+"""This module provides functions to add light to the SUNCG scenes."""
+
 from typing import Tuple, List, Dict
 
 from blenderproc.python.utility.Utility import Utility
@@ -25,29 +27,31 @@ def light_suncg_scene(lightbulb_emission_strength: float = 15, lampshade_emissio
 
             # In the case of the lamp
             if obj_id in lights:
-                SuncgLighting._make_lamp_emissive(obj, lights[obj_id], collection_of_mats, lightbulb_emission_strength,
-                                                  lampshade_emission_strength)
+                SuncgLighting.make_lamp_emissive(obj, lights[obj_id], collection_of_mats, lightbulb_emission_strength,
+                                                 lampshade_emission_strength)
 
             # Make the windows emit light
             if obj_id in windows:
-                SuncgLighting._make_window_emissive(obj, collection_of_mats)
+                SuncgLighting.make_window_emissive(obj, collection_of_mats)
 
             # Also make ceilings slightly emit light
             if obj.get_name().startswith("Ceiling#"):
-                SuncgLighting._make_ceiling_emissive(obj, collection_of_mats, ceiling_emission_strength)
+                SuncgLighting.make_ceiling_emissive(obj, collection_of_mats, ceiling_emission_strength)
 
 
 class SuncgLighting:
+    """ Adds light properties to the SUNCG scenes. """
 
     @staticmethod
-    def _make_lamp_emissive(obj: MeshObject, light: Tuple[List[str], List[str]],
-                            collection_of_mats: Dict[str, Dict[str, Material]],
-                            lightbulb_emission_strength: float = 15,
-                            lampshade_emission_strength: float = 7):
+    def make_lamp_emissive(obj: MeshObject, light: Tuple[List[str], List[str]],
+                           collection_of_mats: Dict[str, Dict[str, Material]],
+                           lightbulb_emission_strength: float = 15,
+                           lampshade_emission_strength: float = 7):
         """ Adds an emission shader to the object materials which are specified in the light dictionary
 
         :param obj: The blender object.
-        :param light: A tuple of two lists. The first list specifies all materials which should act as a lightbulb, the second one lists all materials corresponding to lampshades.
+        :param light: A tuple of two lists. The first list specifies all materials which should act as a lightbulb,
+                      the second one lists all materials corresponding to lampshades.
         :param collection_of_mats: A dictionary that contains materials for lamps, windows and ceilings.
         :param lightbulb_emission_strength: The emission strength that should be used for light bulbs. Default: 15
         :param lampshade_emission_strength: The emission strength that should be used for lamp shades. Default: 7
@@ -84,7 +88,7 @@ class SuncgLighting:
                     collection_of_mats["lamp"][old_mat_name] = m
 
     @staticmethod
-    def _make_window_emissive(obj: MeshObject, collection_of_mats: Dict[str, Dict[str, Material]]):
+    def make_window_emissive(obj: MeshObject, collection_of_mats: Dict[str, Dict[str, Material]]):
         """ Makes the given window object emissive.
 
         For each material with alpha < 1.
@@ -108,7 +112,7 @@ class SuncgLighting:
                     # this material was used as a ceiling before use that one
                     obj.set_material(i, collection_of_mats["window"][mat_name])
                     continue
-                # copy the material if more than one users is using it
+                # copy the material if more than one user is using it
                 if m.get_users() > 1:
                     m = m.duplicate()
                     obj.set_material(i, m)
@@ -124,29 +128,29 @@ class SuncgLighting:
                 collection_of_mats["window"][mat_name] = m
 
     @staticmethod
-    def _make_ceiling_emissive(obj: MeshObject, collection_of_mats: Dict[str, Dict[str, Material]],
-                               ceiling_emission_strength: float = 1.5):
+    def make_ceiling_emissive(obj: MeshObject, collection_of_mats: Dict[str, Dict[str, Material]],
+                              ceiling_emission_strength: float = 1.5):
         """ Makes the given ceiling object emissive, s.t. there is always a little bit ambient light.
 
         :param obj: The ceiling object.
         :param collection_of_mats: A dictionary that contains materials for lamps, windows and ceilings.
         :param ceiling_emission_strength: The emission strength that should be used for the ceiling. Default: 1.5
         """
-        for i, m in enumerate(obj.get_materials()):
-            if m is None:
+        for i, material in enumerate(obj.get_materials()):
+            if material is None:
                 continue
-            mat_name = m.get_name()
+            mat_name = material.get_name()
             if mat_name in collection_of_mats["ceiling"]:
                 # this material was used as a ceiling before use that one
                 obj.set_material(i, collection_of_mats["ceiling"][mat_name])
                 continue
-            # copy the material if more than one users is using it
-            if m.get_users() > 1:
-                m = m.duplicate()
-                obj.set_material(i, m)
+            # copy the material if more than one user is using it
+            if material.get_users() > 1:
+                material = material.duplicate()
+                obj.set_material(i, material)
             # rename the material
-            m.set_name(m.get_name() + "_emission")
+            material.set_name(material.get_name() + "_emission")
 
-            if not m.get_nodes_with_type("Emission") and m.get_nodes_with_type("BsdfPrincipled"):
-                m.make_emissive(emission_strength=ceiling_emission_strength, emission_color=[1, 1, 1, 1])
-                collection_of_mats["ceiling"][mat_name] = m
+            if not material.get_nodes_with_type("Emission") and material.get_nodes_with_type("BsdfPrincipled"):
+                material.make_emissive(emission_strength=ceiling_emission_strength, emission_color=[1, 1, 1, 1])
+                collection_of_mats["ceiling"][mat_name] = material
