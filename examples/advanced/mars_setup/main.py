65f374ba659d5ca60aac2f128bb18e5be8082227
Maximilian Denninger <Maximilian.Denninger@dlr.de>
2022-09-03 15:26:52 +0200

    chore(mars_setup): use new dynamic sky feature

--- examples/advanced/mars_setup/main.py
+++ examples/advanced/mars_setup/main.py
@@ -5,6 +5,10 @@ import numpy as np
 
 
 bproc.init()
+bproc.object.create_primitive("MONKEY")
+bproc.world.add_dynamic_sky_as_world_background(scene_brightness=1.0, sky_color=[0.7, 0.11, 0.019, 1.0],
+                                                horizon_color=[0.3, 0.08, 0.06, 1.0],
+                                                sun_direction=(-0.0744048, -0.741071, 0.667291))
 
 cubes = [bproc.object.create_primitive("MONKEY") for i in range(15)]
 for cube in cubes:
@@ -14,7 +18,7 @@ for cube in cubes:
     cube.add_material(mat)
 
 plane = bproc.object.generate_random_surface()
-plane.add_particle_system(cubes, amount_of_particles=100, children_clumping_factor=-0.1)
+plane.add_particle_system(cubes, amount_of_particles=4000, children_clumping_factor=-0.1)
 
 for cube in cubes:
     cube.delete(remove_all_offspring=True)
