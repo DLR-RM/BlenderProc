342c1c57371ece059339ca32a01252d9e09dcf99
Maximilian Denninger <Maximilian.Denninger@dlr.de>
2022-09-14 09:36:14 +0200

    break main example

--- examples/basics/basic/main.py
+++ examples/basics/basic/main.py
@@ -36,5 +36,7 @@ bproc.renderer.enable_depth_output(activate_antialiasing=False)
 # render the whole pipeline
 data = bproc.renderer.render()
 
+data["colors"][0] += 100
+
 # write the data to a .hdf5 container
 bproc.writer.write_hdf5(args.output_dir, data)
